6245af27c98bc61a412fed8720589e80f771508c
Pete Farmer <p@greyzone.ch>
2018-01-21 18:01:12 +0100

    fixed missing whitespace error

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 
 const logo = require('./logo.svg');

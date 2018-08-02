9ba09ccdf5b91a580f0a3274340a4a1810ca074e
Aaron Craelius <aaron@regen.network>
2018-08-02 17:35:42 -0400

    Added syncing of map polygon and list selection

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View } from 'react-native';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
 import ReactMapboxGl from "react-mapbox-gl";
 import * as mapbox from "mapbox-gl";
 import 'mapbox-gl/dist/mapbox-gl.css';
@@ -7,6 +11,7 @@ import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css';
 import * as MapboxDraw from '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw';
 import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import withTheme from '@material-ui/core/styles/withTheme';
 
 const mapboxAccessToken = "pk.eyJ1IjoiYWFyb25jLXJlZ2VuIiwiYSI6ImNqa2I4dW9sbjBob3czcHA4amJqM2NhczAifQ.4HW-QDLUBJiHxOjDakKm2w";
 
@@ -18,11 +23,22 @@ const Map = ReactMapboxGl({
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { features: {} };
+    this.state = {
+      features: {},
+      selected: {}
+   };
   }
 
   onDrawUpdated = (e) => {
     this.setState({ features: this.state.drawControl.getAll() });
+  }
+
+  onSelectionChange = (e) => {
+    var selected = {};
+    e.features.forEach((feature) => {
+      selected[feature.id] = true;
+    });
+    this.setState({selected});
   }
 
   onMapLoad = (map) => {
@@ -49,18 +65,31 @@ class App extends Component {
     map.on('draw.combine', this.onDrawUpdated);
     map.on('draw.uncombine', this.onDrawUpdated);
     map.on('draw.update', this.onDrawUpdated);
+    map.on('draw.selectionchange', this.onSelectionChange);
+  }
+
+  toggleSelectItem = (id) => {
+    // TODO I was trying to get the toggling to work, but somehow changeMode isn't working with simple_select - @aaronc
+    // either we just use direct_select or do a bit of debugging
+    const {selected, drawControl} = this.state;
+    const newSelected = {...selected};
+    newSelected[id] = !selected[id];
+    const featureIds = [];
+    Object.getOwnPropertyNames(newSelected).forEach((k) => {
+      if(newSelected[k]) featureIds.push(k);
+    });
+    drawControl.changeMode('simple_select', {featureIds});
+    this.setState({selected:newSelected});
+    //this.state.drawControl.changeMode('direct_select', {featureId: id});
   }
 
   render() {
+    const {features, selected} = this.state;
     return (
       <View style={{ flex: 1, flexDirection: 'column' }}>
         <View style={{ flex: 8, flexDirection: 'row' }}>
           <View style={{ flex: 2 }}>
-            <FlatList
-              data={this.state.features.features}
-              keyExtractor={(item) => item.id}
-              renderItem={({ item }) => <FeatureListItem item={item} />}
-            />
+            <FeatureList features={features} selected={selected} toggleSelectItem={this.toggleSelectItem} />
           </View>
           <View style={{ flex: 8 }}>
             <Map
@@ -81,12 +110,24 @@ class App extends Component {
   }
 }
 
-const FeatureListItem = ({ item }) => {
+const FeatureList = withTheme()(({features, selected, toggleSelectItem, theme}) => {
   return (
-    <View style={{ flexDirection: 'column' }}>
-      <Text style={{ fontWeight: 'bold' }}>{item.geometry.type}</Text>
-      <Text style={{ fontSize: 10 }}>{JSON.stringify(item.geometry.coordinates[0])}</Text>
-    </View>
+  <List subheader={<ListSubheader>Features</ListSubheader>}>
+    {features && features.features && features.features.map((feature) =>
+      <FeatureListItem item={feature} theme={theme} selected={selected[feature.id]}
+        toggleSelectThis={() => toggleSelectItem(feature.id)}
+      />
+    )}
+  </List>
+  );
+});
+
+const FeatureListItem = ({ item, selected, toggleSelectThis, theme }) => {
+  const style = selected ? {backgroundColor: theme.palette.primary.main} : {};
+  return (
+    <ListItem dense button style={style} key={item.id} onClick={toggleSelectThis}>
+      <ListItemText primary={"Unsaved Polygon " + item.id} />
+    </ListItem>
   );
 }
 

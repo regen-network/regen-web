4d8f05671e3313bb3c3f86329f4cd42904913bc9
Clare Politano Hutchings <cpolitano8@gmail.com>
2018-08-02 16:38:42 -0400

    attach custom mui template

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -7,13 +7,17 @@ import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css';
 import * as MapboxDraw from '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw';
 import * as MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { withTheme } from '@material-ui/core/styles';
 
 const mapboxAccessToken = "pk.eyJ1IjoiYWFyb25jLXJlZ2VuIiwiYSI6ImNqa2I4dW9sbjBob3czcHA4amJqM2NhczAifQ.4HW-QDLUBJiHxOjDakKm2w";
 
 const Map = ReactMapboxGl({
   accessToken: mapboxAccessToken
 });
-
 
 class App extends Component {
   constructor(props) {
@@ -52,8 +56,25 @@ class App extends Component {
   }
 
   render() {
+    const { theme } = this.props;
+    const styles = {
+      primaryColor: {
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.common.white,
+      },
+      fontFamily: theme.fontFamily
+    };
+
     return (
       <View style={{ flex: 1, flexDirection: 'column' }}>
+        <AppBar position="static">
+          <Toolbar style={{display: 'flex', justifyContent: 'space-between'}}>
+            <Typography variant="title" style={{color: styles.primaryColor.color, fontFamily: styles.fontFamily}}>
+              Welcome, User!
+            </Typography>
+            <Button style={{color: styles.primaryColor.color}}>Logout</Button>
+          </Toolbar>
+        </AppBar>
         <View style={{ flex: 8, flexDirection: 'row' }}>
           <View style={{ flex: 2 }}>
             <FlatList
@@ -92,4 +113,4 @@ const FeatureListItem = ({ item }) => {
 
 // <Map containerStyle={{height:"100vh", width:"100vw"}} />
 
-export default App;
+export default withTheme()(App);
--- web/src/index.js
+++ web/src/index.js
@@ -2,7 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
+import theme from './theme';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Root = () => {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
+  );
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();

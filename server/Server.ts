cfe1d293ded9d7b54601e5f29c637d0d79019148
Aaron Craelius <aaron@regen.network>
2018-08-06 15:53:21 -0400

    Move ts-node out of dev deps

--- server/Server.ts
+++ server/Server.ts
@@ -4,18 +4,18 @@ import {postgraphile} from 'postgraphile';
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '../web/build')));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../web/build', 'index.html'));
+});
+
 app.use(postgraphile(process.env.DATABASE_URL || 'postgres://postgres@localhost:5432/xrn', 'public', {
   graphiql:true,
   watchPg: true,
   pgDefaultRole: 'guest',
   dynamicJson: true
 }));
-
-app.use(express.static(path.join(__dirname, '../web/build')));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '../web/build', 'index.html'));
-});
 
 const port = process.env.PORT || 5000;
 

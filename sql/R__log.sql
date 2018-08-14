ada271a6a9269769b7a47459f6e5c762736b3be5
Aaron Craelius <aaron@regen.network>
2018-08-14 15:11:00 -0400

    Renamed create_entry -> log_entry, added comments

--- sql/R__log.sql
+++ sql/R__log.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION pblic.create_entry(type text, comment text, polygon json, point json, species text, unit text, numeric_value numeric)
+CREATE OR REPLACE FUNCTION public.log_entry(type text, comment text, polygon json, point json, species text, unit text, numeric_value numeric)
   returns public.entry as $$
 INSERT INTO entry (created_by, created_at, type, comment, polygon, point, species, unit, numeric_value)
 VALUES (current_user, current_timestamp, type, comment, st_geomfromgeojson(polygon::text), st_geomfromgeojson(point::text), species, unit, numeric_value)
@@ -6,3 +6,5 @@ VALUES (current_user, current_timestamp, type, comment, st_geomfromgeojson(polyg
 -- "security definer" implies that the function gets called with the
 -- permissions of the definer (the root user) and thus bypasseses RLS
 $$ language sql volatile security definer;
+
+COMMENT ON FUNCTION public.log_entry(type text, comment text, polygon json, point json, species text, unit text, numeric_value numeric) IS 'Inserts a log entry into the entry table';

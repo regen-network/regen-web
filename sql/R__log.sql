84b277a4b33368d99dcd5bc86c5654090f267ad5
Aaron Craelius <aaron@regen.network>
2018-08-14 14:57:28 -0400

    Initial schema for log entries

---
+++ sql/R__log.sql
@@ -0,0 +1,8 @@
+CREATE OR REPLACE FUNCTION pblic.create_entry(type text, comment text, polygon json, point json, species text, unit text, numeric_value numeric)
+  returns public.entry as $$
+INSERT INTO entry (created_by, created_at, type, comment, polygon, point, species, unit, numeric_value)
+VALUES (current_user, current_timestamp, type, comment, st_geomfromgeojson(polygon::text), st_geomfromgeojson(point::text), species, unit, numeric_value)
+    RETURNING *;
+-- "security definer" implies that the function gets called with the
+-- permissions of the definer (the root user) and thus bypasseses RLS
+$$ language sql volatile security definer;

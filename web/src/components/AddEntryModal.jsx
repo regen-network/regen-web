fba80f0fce014c0b842f4c510da39ca42dddd467
Aaron Craelius <aaron@regen.network>
2018-08-15 16:52:32 -0400

    WIP on adding controls to add entry modal

--- web/src/components/AddEntryModal.jsx
+++ web/src/components/AddEntryModal.jsx
@@ -4,30 +4,53 @@ import Modal from '@material-ui/core/Modal';
 import Select from './Select.jsx';
 
 const entryTypes = [
-    'Planting',
-    'Harvesting',
-    'Tillage'
+    {type:'Planting', category: 'PlantRelated'},
+    {type:'Harvesting', category: 'PlantRelated'},
+    {type:'Tillage'}
 ];
+
+const entryTypeCategories = new Map(entryTypes.map(({type,category}) => [type, category]));
+
+const isPlantRelated = (type) => entryTypeCategories[type] === 'PlantRelated';
+
+const plants = [
+    {name:'Wheat'},
+    {name:'Rye'},
+    {name:'Soy'},
+    {name:'Corn'},
+    {name:'Buckwheat'},
+]
 
 class AddEntryModal extends React.Component {
     state = {
-        type: null
+        type: null,
+        species: null
     }
 
-    onTypeSelected = (v) => this.setState({type:v});
+    onTypeSelected = (e) => this.setState({type:e.value});
+    onSpeciesSelected = (e) => this.setState({species:e.value});
 
     render() {
         const {open, onClose} = this.props;
-        const {type} = this.state;
+        const {type, species} = this.state;
+        console.log(this.state);
+        console.log(entryTypes);
+        console.log(entryTypeCategories);
 
         return (
             <Modal open={open} onClose={onClose}>
                 <div className="modal-add-entry">
                     <Select
-                        options={entryTypes.map(t => {return {value:t, label:t}})}
+                        options={entryTypes.map(({type}) => {return {value:type, label:type}})}
                         value={type}
                         onChange={this.onTypeSelected}
                     />
+                    {isPlantRelated(type) ?
+                       <Select
+                           options={plants.map(({name}) => {return {value:name, label:name}})}
+                           value={species}
+                           onChange={this.onSpeciesSelected} /> : null
+                    }
                 </div>
             </Modal>
         );

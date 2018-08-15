cb297edfcee0f25f93f13edc9d3188667cc81dc1
Aaron Craelius <aaron@regen.network>
2018-08-15 16:11:41 -0400

    Created Select control based off of Material UI react-select example

--- web/src/components/AddEntryModal.jsx
+++ web/src/components/AddEntryModal.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
-import { withTheme } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
 import './AddEntryModal.css';
+import Modal from '@material-ui/core/Modal';
+import Select from './Select.jsx';
+
+const entryTypes = [
+    'Planting',
+    'Harvesting',
+    'Tillage'
+];
 
 class AddEntryModal extends React.Component {
+    state = {
+        type: null
+    }
+
+    onTypeSelected = (v) => this.setState({type:v});
+
     render() {
         const {open, onClose} = this.props;
+        const {type} = this.state;
 
         return (
             <Modal open={open} onClose={onClose}>
                 <div className="modal-add-entry">
-                    Hello World!
+                    <Select
+                        options={entryTypes.map(t => {return {value:t, label:t}})}
+                        value={type}
+                        onChange={this.onTypeSelected}
+                    />
                 </div>
             </Modal>
         );

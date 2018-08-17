87e2644a3a02e85cb1ccb52ac4c66f712a75a50f
Clare Politano Hutchings <cpolitano8@gmail.com>
2018-08-17 10:37:24 -0400

    sql coalesce

--- sql/R__geo.sql
+++ sql/R__geo.sql
@@ -6,7 +6,6 @@ $$ language sql stable ;
 CREATE OR REPLACE FUNCTION public.create_polygon_by_json(name text, geojson json, owner text)
 returns public.polygon as $$
   INSERT INTO polygon (name, geom, owner)
-  VALUES (name, st_geomfromgeojson(geojson::text), owner || current_user)
+  VALUES (name, st_geomfromgeojson(geojson::text), COALESCE(owner, current_user))
   RETURNING *;
 $$ language sql volatile ;
-

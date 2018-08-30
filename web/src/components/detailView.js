11e8ed90c1427aa747462e87fee3bfeb4f23ce7e
Clare Politano Hutchings <cpolitano8@gmail.com>
2018-08-30 13:23:51 -0400

    query findEntries

--- web/src/components/detailView.js
+++ web/src/components/detailView.js
@@ -5,6 +5,26 @@ import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
 import { withTheme } from '@material-ui/core/styles';
 import PolygonIcon from './polygonIcon';
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+
+const GET_ENTRIES = gql`
+  query FindEntries($polygon: JSON!) {
+    findEntries(polygon: $polygon) {
+      nodes {
+        when
+        type
+        unit
+        point
+        comment
+        species
+        numericValue
+        id
+      }
+      totalCount
+    }
+  }
+`;
 
 const DetailView = withTheme()(({ features, selected, polygons, theme, styles }) => {
 
@@ -19,37 +39,46 @@ const DetailView = withTheme()(({ features, selected, polygons, theme, styles })
     });
   }
 
+  console.log(selectedPolygon);
+
   const fakeEntryItems = [
-    {type: 'Planting', polygon: selectedPolygon, species: 'Wheat', date: "2018-08-01" },
-    {type: 'Harvesting', polygon: selectedPolygon, species: 'Wheat', date: "2018-08-01" },
-    {type: 'Cover crop', polygon: selectedPolygon, species: 'Rye', date: "2018-08-01" },
-    {type: 'Tillage', polygon: selectedPolygon, date: "2018-08-01" }
+    {type: 'Planting', polygon: selectedPolygon, species: 'Wheat', date: "2018-08-01", id: "1" },
+    {type: 'Harvesting', polygon: selectedPolygon, species: 'Wheat', date: "2018-08-01", id: "2" },
+    {type: 'Cover crop', polygon: selectedPolygon, species: 'Rye', date: "2018-08-01", id: "3" },
+    {type: 'Tillage', polygon: selectedPolygon, date: "2018-08-01", id: "4" }
   ];
 
   return (
     <div>
       { selectedPolygon ?
-        <div>
-          <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-            <Typography variant="title" style={{fontFamily: styles.title.fontFamily, fontSize: styles.title.fontSize, marginRight: "25px"}}>
-              {"Plot Details"}
-            </Typography>
-            <Typography variant="title" style={{color: styles.accent.blue, fontFamily: styles.title.fontFamily, fontSize: styles.title.fontSize, marginRight: "16px"}}>
-              {selectedPolygon.name}
-            </Typography>
-            <PolygonIcon polygon={selectedPolygon}/>
-          </div>
-          <div style={{textAlign: "center"}}>
-            <List style={{width: "300px", margin: "0 auto"}}>
-              {fakeEntryItems.map((entry) =>
-                <EntryItem
-                  entry={entry}
-                  styles={styles}
-                  key={entry.polygon.id} />
-              )}
-            </List>
-          </div>
-        </div>
+        <Query query={GET_ENTRIES} variables={{ polygon: selectedPolygon }}>
+          {({loading, error, data}) => {
+            console.log("GET_ENTRIES", data);
+
+            return (
+              <div>
+                <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                  <Typography variant="title" style={{fontFamily: styles.title.fontFamily, fontSize: styles.title.fontSize, marginRight: "25px"}}>
+                    {"Plot Details"}
+                  </Typography>
+                  <Typography variant="title" style={{color: styles.accent.blue, fontFamily: styles.title.fontFamily, fontSize: styles.title.fontSize, marginRight: "16px"}}>
+                    {selectedPolygon.name}
+                  </Typography>
+                  <PolygonIcon polygon={selectedPolygon}/>
+                </div>
+                <div style={{textAlign: "center"}}>
+                  <List style={{width: "300px", margin: "0 auto"}}>
+                    {fakeEntryItems.map((entry) =>
+                      <EntryItem
+                        entry={entry}
+                        styles={styles}
+                        key={entry.id} />
+                    )}
+                  </List>
+                </div>
+              </div>
+          )}}
+        </Query>
         : null
       }
     </div>

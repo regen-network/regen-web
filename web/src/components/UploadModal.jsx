c493535fa7483484c090c542db02ec6350b8937c
Pete Farmer <p@greyzone.ch>
2018-12-03 18:55:20 +0100

    WIP: refresh query, menu and panel cleanup.

--- web/src/components/UploadModal.jsx
+++ web/src/components/UploadModal.jsx
@@ -3,7 +3,23 @@ import './AddEntryModal.css';
 import { withTheme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Modal from '@material-ui/core/Modal';
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
+
+const GET_POLYGONS = gql`
+{
+  getCurrentUser
+  allPolygons {
+    nodes {
+      id
+      name
+      geomJson
+      owner
+    }
+  }
+}
+`;
 
 class UploadModal extends React.Component {
     constructor(props) {
@@ -36,7 +52,7 @@ class UploadModal extends React.Component {
 
     render() {
         const {open, onClose, theme, accessToken } = this.props;
-        console.log("accessToken=",accessToken);
+
         const styles = {
           primaryColor: {
           backgroundColor: theme.palette.primary.main,
@@ -54,7 +70,9 @@ class UploadModal extends React.Component {
               <div className="modal-add-entry">
                 <div style={{margin: "25px"}}>
                   <Typography variant="title" style={{fontFamily: styles.font}}>
-                    {"Be my modal please."}
+                    {"Select a .kmz file."}
+                  </Typography>
+                  {/* <Query query={GET_POLYGONS} refetchQueries={[{query: GET_POLYGONS}]}> */}
 
                     <form encType="multipart/form-data" onSubmit={this.handleUpload} >
                       <div>
@@ -66,8 +84,8 @@ class UploadModal extends React.Component {
                         <input type="submit"/>
                       </div>
                     </form>
+                    {/* </Query> */}
 
-                  </Typography>
                 </div>
               </div>
             </Modal>

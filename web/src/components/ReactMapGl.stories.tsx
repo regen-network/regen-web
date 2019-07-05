5528045063b8ca106e6ae0c9012834485a26b714
Aaron Craelius <aaron@regen.network>
2019-07-05 07:25:14 -0400

    WIP on map stories

--- web/src/components/ReactMapGl.stories.tsx
+++ web/src/components/ReactMapGl.stories.tsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 import {storiesOf} from '@storybook/react';
 import {action} from '@storybook/addon-actions';
 
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, {Marker} from 'react-map-gl';
 
 class Map extends Component {
 
@@ -20,10 +20,44 @@ class Map extends Component {
   render() {
     return (
       <ReactMapGL
-        {...this.state.viewport} onViewportChange={(viewport) => this.setState({viewport})}/>
+        width="100%"
+        height="100%"
+        mapStyle="mapbox://styles/mapbox/satellite-streets-v10"
+        {...this.state.viewport} onViewportChange={(viewport) => this.setState({viewport})}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      >
+        {this.props.children}
+      </ReactMapGL>
     );
   }
 }
 
-storiesOf('ReactMapGL', module)
-  .add('default', () => <Map />);
+const ICON = `M20.2,15.7L20.2,15.7c1.1-1.6,1.8-3.6,1.8-5.7c0-5.6-4.5-10-10-10S2,4.5,2,10c0,2,0.6,3.9,1.6,5.4c0,0.1,0.1,0.2,0.2,0.3
+  c0,0,0.1,0.1,0.1,0.2c0.2,0.3,0.4,0.6,0.7,0.9c2.6,3.1,7.4,7.6,7.4,7.6s4.8-4.5,7.4-7.5c0.2-0.3,0.5-0.6,0.7-0.9
+  C20.1,15.8,20.2,15.8,20.2,15.7z`;
+
+const pinStyle = {
+  cursor: 'pointer',
+  fill: '#d00',
+  stroke: 'none'
+};
+
+const size = 20;
+
+storiesOf('Maps|ReactMapGL', module)
+  .addDecorator(story =>
+    <Map>{story()}</Map>
+  )
+  .add("default", () => null)
+  .add("marker", () => <Marker latitude={37.8} longitude={-122.5} key={0}>
+    <svg
+      height={size}
+      viewBox="0 0 24 24"
+      style={{
+        ...pinStyle,
+        transform: `translate(${-size / 2}px,${-size}px)`
+      }}
+    >
+      <path d={ICON} />
+    </svg>
+  </Marker>)

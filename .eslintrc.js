dec6ef7a46561905715a747260644c8256869610
aptcore <apt.devcore@gmail.com>
2019-07-19 09:31:09 +0200

    Added initial eslint config with prettier

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: [
+    'react-app',
+    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  rules: {
+    'no-empty': 'off',
+    'no-console': 'warn',
+    '@typescript-eslint/explicit-function-return-type': [
+      'error',
+      {
+        allowExpressions: true,
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+  plugins: ['react'],
+};

4c10b524b080ccd7428d035571438605db197361
aptcore <apt.devcore@gmail.com>
2019-07-19 17:34:06 +0200

    Adding stories from web-components

--- web-storybook/.storybook/config.js
+++ web-storybook/.storybook/config.js
@@ -10,9 +10,11 @@ addParameters({
 });
 
 const webStories = require.context('../../web', true, /\.stories\.((js|ts)x?)$/);
+const webComponentStories = require.context('../../web-components', true, /\.stories\.((js|ts)x?)$/);
 
 function loadStories() {
   webStories.keys().forEach(filename => webStories(filename));
+  webComponentStories.keys().forEach(filename => webComponentStories(filename));
 }
 
 configure(loadStories, module);

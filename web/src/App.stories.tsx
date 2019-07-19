844170f7109624cac8cd88fded5e40019b9ba417
aptcore <apt.devcore@gmail.com>
2019-07-19 17:43:28 +0200

    Example of using a web-component in web module

--- web/src/App.stories.tsx
+++ web/src/App.stories.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
 import { storiesOf } from '@storybook/react';
+import Table from 'web-components/lib/components/table';
 
-storiesOf('Components|Views', module).add('welcome', () => <div>Hello world!</div>);
+storiesOf('Components|Views', module).add('welcome', () => <Table rowsPage={200} totalRows={500} />);

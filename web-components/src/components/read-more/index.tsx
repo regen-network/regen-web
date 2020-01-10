6b9f7d76494f470bfb2107eaf5b3692936d45422
blushi <marie.gauthier63@gmail.com>
2020-01-10 13:17:28 +0100

    [#170594123] Implement first version of ReadMore component

---
+++ web-components/src/components/read-more/index.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { makeStyles, Theme } from '@material-ui/core';
+import OutlinedButton from '../buttons/OutlinedButton';
+import Description from '../description';
+// TODO use svg icon from figma
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import { truncate } from './truncate';
+
+interface ReadMoreProps {
+  children: string;
+  length?: number;
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  textContainer: {
+    marginBottom: theme.spacing(7),
+  },
+}));
+
+export default function ReadMore({ children, length = 700 }: ReadMoreProps): JSX.Element {
+  const classes = useStyles({});
+  const [expanded, setExpanded] = useState(false);
+  const trimText = children.substring(0, length);
+  // const trimText = truncate(children, length);
+  const expandedText = expanded ?
+    <Description>{children}</Description> :
+    <Description>{trimText}</Description>;
+
+  return (
+    <div>
+      <div className={classes.textContainer}>
+        {expandedText}
+      </div>
+      {children.length > length &&
+        <OutlinedButton
+          onClick={() => setExpanded(!expanded)}
+          startIcon={expanded ? <ArrowUpwardIcon /> : <ArrowDownwardIcon />}
+        >
+          read {expanded ? 'less' : 'more'}
+        </OutlinedButton>
+      }
+    </div>
+  )
+}
---
+++ web-components/src/components/read-more/truncate.ts
@@ -0,0 +1,12 @@
+// TODO
+export function truncate(str: string, length: number) {
+  const sentences = str.match(/[^\.!\?]+[\.!\?]+/g);
+  if (sentences) {
+    // for (let i )
+    console.log(sentences);
+    if (sentences.length > length) {
+      return sentences.slice(0, length).join(" ");
+    }
+  }
+  return str;
+}

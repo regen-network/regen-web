8ff4d456c7946b4ebbd0b8abcc0b65f00d212996
blushi <marie.gauthier63@gmail.com>
2020-01-27 12:14:41 +0100

    Update tests
    * Add matchMedia mock for tests to pass
    * Update storyshots snapshots
    
    [Story] [#170431076]
    [Test] All automatic tests pass now
    [Review] na

--- web/src/storyshots.test.ts
+++ web/src/storyshots.test.ts
@@ -1,5 +1,6 @@
 import initStoryshots from '@storybook/addon-storyshots';
 import path from 'path';
+import './matchMedia.mock';
 
 initStoryshots({
   configPath: path.resolve(__dirname, '../../web-storybook/.storybook'),

b2150bec2e3dbdef55793c47ddf0711a496fe69d
blushi <marie.gauthier63@gmail.com>
2020-02-03 16:21:56 +0100

    Add id as required project field
    * will be used for routing
    
    [Story] [#170729961]
    [Test] n
    [Review] na

--- web/src/mocks.ts
+++ web/src/mocks.ts
@@ -4,6 +4,9 @@ import { Event } from 'web-components/lib/components/timeline';
 
 import mock from './mock.json';
 
+// imgSrc should be either web url or static image filenames within web/src/assets/
+// (eg to load web/src/assets/coorong.png, use « coorong.png » as imgSrc)
+
 interface Action {
   name: string;
   description: string;
@@ -24,11 +27,12 @@ interface Metholody {
 interface CreditClass {
   name: string;
   description: string;
-  // methodology: Metholody;
+  methodology?: Metholody;
   imgSrc: string;
 }
 
 export interface Project {
+  id: string; // human-readable id for now
   name: string;
   place: string;
   type: string;

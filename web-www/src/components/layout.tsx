b9fd249b2366cded2b00aef3f6cb02af70787288
Jared <neuralsplash@gmail.com>
2020-05-26 04:50:05 -0600

    removed header children from layout component

--- web-www/src/components/layout.tsx
+++ web-www/src/components/layout.tsx
@@ -5,28 +5,17 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from 'react';
-import PropTypes, { string } from 'prop-types';
-import { useStaticQuery, graphql } from 'gatsby';
-import styled from 'styled-components';
-import Header from 'web-components/lib/components/header';
-import './layout.css';
-import MenuHover from '../components/menuHover';
-import { MenuItem, MenuList, Link } from '@material-ui/core';
+import React from "react"
+import PropTypes, { string } from "prop-types"
+import { useStaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
+import Header from "web-components/lib/components/header"
+import "./layout.css"
 
-let logo = 'images/logo.png';
+let logo = "images/logo.png"
 interface propTypes {
-  children: Array<React.ReactElement>;
+  children: Array<React.ReactElement>
 }
-
-let StyledMenuItem = styled(MenuItem)``;
-
-let StyledMenuList = styled(MenuList)`
-  display: flex;
-  position: unset;
-  width: unset;
-  z-index: 0;
-`;
 
 const Layout = ({ children }: propTypes): JSX.Element => {
   const data = useStaticQuery(graphql`
@@ -37,40 +26,23 @@ const Layout = ({ children }: propTypes): JSX.Element => {
         }
       }
     }
-  `);
+  `)
 
-  const url = typeof window !== 'undefined' ? window.location.href : '';
+  const url = typeof window !== "undefined" ? window.location.href : ""
 
-  let color = '#000';
-  if (url.includes('page')) {
-    color = '#fff';
+  let color = "#000"
+  if (url.includes("page")) {
+    color = "#fff"
   }
 
   return (
     <>
-      <Header transparent={true} color={color} logo={logo} absolute={true}>
-        <StyledMenuList>
-          <StyledMenuItem>
-            <Link href="">Buyers</Link>
-          </StyledMenuItem>
-          <StyledMenuItem>
-            <Link href="">Land Stewards</Link>
-          </StyledMenuItem>
-          <MenuItem>
-            <MenuHover text="Learn More">
-              <MenuItem>
-                <Link href="https://regen.network/#">Case Studies</Link>
-              </MenuItem>
-              <MenuItem>
-                <Link href="https://regen.network/#">FAQ</Link>
-              </MenuItem>
-              <MenuItem>
-                <Link href="https://regen.network/#">Team</Link>
-              </MenuItem>
-            </MenuHover>
-          </MenuItem>
-        </StyledMenuList>
-      </Header>
+      <Header
+        transparent={true}
+        color={color}
+        logo={logo}
+        absolute={true}
+      ></Header>
       <div
         style={{
           margin: `0 auto`,
@@ -84,11 +56,11 @@ const Layout = ({ children }: propTypes): JSX.Element => {
         </footer>
       </div>
     </>
-  );
-};
+  )
+}
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-};
+}
 
-export default Layout;
+export default Layout

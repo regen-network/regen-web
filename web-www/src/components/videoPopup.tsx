57456fedd2f1763d5cda2b650cdb0eef8447d74d
Jared <neuralsplash@gmail.com>
2020-06-10 12:58:22 -0600

    svg + play icon for video popup

--- web-www/src/components/videoPopup.tsx
+++ web-www/src/components/videoPopup.tsx
@@ -1,27 +1,33 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Popover from '@material-ui/core/Popover';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
+import Modal from '@material-ui/core/Modal';
 import { StaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
-import BackgroundImage from 'gatsby-background-image';
+import PlayIcon from 'web-components/lib/components/icons/PlayIcon';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    cursor: 'pointer',
     width: 'min-content',
+    height: 'min-content',
+    cursor: 'pointer',
     margin: '0 auto',
   },
   typography: {
     padding: theme.spacing(2),
   },
+  play: {
+    width: theme.spacing(20),
+    height: theme.spacing(20),
+    backgroundColor: theme.palette.primary.main,
+    borderRadius: '50%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   icon: {
-    position: 'absolute',
-    width: '80%',
-    height: '80%',
-    top: '50%',
-    left: '54%',
+    position: 'relative',
+    top: theme.spacing(6),
+    left: theme.spacing(10),
     transform: 'translate(-50%, -50%)',
   },
   vid: {
@@ -45,68 +51,40 @@ export default function VideoPopup() {
   const open = Boolean(anchorEl);
   const id = open ? 'simple-popover' : undefined;
   return (
-    <StaticQuery
-      query={graphql`
-        query {
-          play: file(relativePath: { eq: "play.png" }) {
-            childImageSharp {
-              fixed(quality: 90, width: 45) {
-                ...GatsbyImageSharpFixed_withWebp
-              }
-            }
-          }
-          whiteEllipse: file(relativePath: { eq: "white-ellipse.png" }) {
-            childImageSharp {
-              fixed(quality: 90, width: 100) {
-                ...GatsbyImageSharpFixed_withWebp
-              }
-            }
-          }
-        }
-      `}
-      render={data => {
-        return (
-          <div className={classes.root} onClick={handleClick}>
-            <BackgroundImage Tag="div" fixed={data.whiteEllipse.childImageSharp.fixed}>
-              <Img
-                fixed={data.play.childImageSharp.fixed}
-                style={{ position: 'absolute' }}
-                className={classes.icon}
-              ></Img>
-            </BackgroundImage>
-            <Popover
-              id={id}
-              open={open}
-              onClose={handleClose}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'center',
-              }}
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'center',
-              }}
-            >
-              <div className={classes.vid} style={{ padding: '56.25% 0 0 0', position: 'relative' }}>
-                <iframe
-                  title="Kiss the Ground Video"
-                  src="https://player.vimeo.com/video/299326236?autoplay=1"
-                  style={{
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    width: '100%',
-                    height: '100%',
-                  }}
-                  frameBorder={0}
-                  allow="autoplay; fullscreen"
-                  allowFullScreen
-                />
-              </div>
-            </Popover>
-          </div>
-        );
-      }}
-    />
+    <div className={classes.root} onClick={handleClick}>
+      <div className={classes.play}>
+        <PlayIcon width="60%" height="60%" className={classes.icon} />
+      </div>
+      <Modal
+        id={id}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+      >
+        <div className={classes.vid} style={{ padding: '56.25% 0 0 0', position: 'relative' }}>
+          <iframe
+            title="Kiss the Ground Video"
+            src="https://player.vimeo.com/video/299326236?autoplay=1"
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%',
+              height: '100%',
+            }}
+            frameBorder={0}
+            allow="autoplay; fullscreen"
+            allowFullScreen
+          />
+        </div>
+      </Modal>
+    </div>
   );
 }

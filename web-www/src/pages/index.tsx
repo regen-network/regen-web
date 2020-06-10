ae4341214d44584d4ce88b7af1c6cae9a94710e5
Jared <neuralsplash@gmail.com>
2020-06-10 09:19:36 -0600

    use theme

--- web-www/src/pages/index.tsx
+++ web-www/src/pages/index.tsx
@@ -7,16 +7,20 @@ import styled from 'styled-components';
 import StyledComponentsWrapper from '../components/styledComponentsWrapper';
 import HomeFoldSection from '../sections/home-fold-section';
 import MarketplaceSection from '../sections/marketplace-section';
+import { useTheme } from '@material-ui/core/styles';
 
-const IndexPage = (): JSX.Element => (
-  <StyledComponentsWrapper>
-    <Layout color="#fff">
-      <SEO title="Home" />
-      <HomeFoldSection></HomeFoldSection>
-      <MarketplaceSection></MarketplaceSection>
-      <Link to="/page-2/">Go to page 2</Link>
-    </Layout>
-  </StyledComponentsWrapper>
-);
+const IndexPage = (): JSX.Element => {
+  const theme = useTheme();
+  return (
+    <StyledComponentsWrapper>
+      <Layout color={theme.palette.primary.main}>
+        <SEO title="Home" />
+        <HomeFoldSection></HomeFoldSection>
+        <MarketplaceSection></MarketplaceSection>
+        <Link to="/page-2/">Go to page 2</Link>
+      </Layout>
+    </StyledComponentsWrapper>
+  );
+};
 
 export default IndexPage;

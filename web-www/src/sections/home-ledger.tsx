1e2ae4b7936ac4a2caa0efcdd40e07204b3c1c76
Jared <neuralsplash@gmail.com>
2020-06-16 08:06:06 -0600

    ledger section looking better

--- web-www/src/sections/home-ledger.tsx
+++ web-www/src/sections/home-ledger.tsx
@@ -1,21 +1,50 @@
-import React from "react"
-import BackgroundImage from "gatsby-background-image"
-import clsx from "clsx"
-import { graphql, StaticQuery, useStaticQuery } from "gatsby"
-import Grid, { GridSpacing } from "@material-ui/core/Grid"
-import { makeStyles } from "@material-ui/core"
-import Img from "gatsby-image"
+import React from 'react';
+import BackgroundImage from 'gatsby-background-image';
+import clsx from 'clsx';
+import { graphql, StaticQuery, useStaticQuery } from 'gatsby';
+import Grid, { GridSpacing } from '@material-ui/core/Grid';
+import ContainedButton from 'web-components/lib/components/buttons/ContainedButton';
+import { makeStyles, Theme } from '@material-ui/core';
+import Title from 'web-components/lib/components/Title';
+import Img from 'gatsby-image';
 
-let useStyles = makeStyles({
+let useStyles = makeStyles((theme: Theme) => ({
   section: {
-    height: "40vh",
-    "font-size": "0.75rem",
-    "padding-top": "10vh",
+    height: 'min-content',
+    'font-size': theme.typography.body2.fontSize,
+    'padding-top': theme.spacing(30),
+    'padding-bottom': theme.spacing(45),
+    'text-align': 'center',
+    '& p': {
+      'max-width': theme.spacing(100),
+      'text-align': 'left',
+    },
   },
-})
+  img: {
+    'min-width': 'fit-content',
+    'margin-right': theme.spacing(10),
+  },
+  grid: {
+    width: 'auto',
+    display: 'inline-flex',
+  },
+  button: {
+    [theme.breakpoints.down('xs')]: {
+      padding: `${theme.spacing(1)} ${theme.spacing(2)}`,
+      fontSize: theme.spacing(4.5),
+    },
+    [theme.breakpoints.up('sm')]: {
+      padding: `${theme.spacing(2)} ${theme.spacing(8.5)}`,
+      fontSize: theme.spacing(4.5),
+    },
+  },
+  green: {
+    color: theme.palette.secondary.main,
+  },
+}));
 
 interface Props {
-  className?: string
+  className?: string;
 }
 
 const HomeLedger = ({ className }: Props) => {
@@ -36,32 +65,33 @@ const HomeLedger = ({ className }: Props) => {
         }
       }
     }
-  `)
-  let classes = useStyles()
+  `);
+  let classes = useStyles();
   return (
     <BackgroundImage
       Tag="section"
       className={clsx(classes.section, className)}
       fluid={data.bg.childImageSharp.fluid}
     >
-      <Grid className={classes.grid} container spacing={3}>
-        <Grid item sm={4}>
+      <Grid className={classes.grid} wrap="nowrap" container spacing={3}>
+        <Grid className={classes.img} item sm={4}>
           <Img fixed={data.ledger.childImageSharp.fixed}></Img>
         </Grid>
-        <Grid item sm={8}>
-          <h2>The Regen Ledger powers our work</h2>
+        <Grid item sm={6} md={8}>
+          <Title align="left" variant="h2">
+            The <span className={classes.green}>Regen Ledger</span> powers our work
+          </Title>
           <p>
-            Regen Ledger is a public, proof of stake (POS) blockchain developed
-            with the Cosmos Software Development Kit (SDK) built for
-            verification of claims, agreements & data related to ecological
-            state. Regen Ledger enables multiple registries to communicate and
-            transact with each other producing a public ecological accounting
-            system. Get involved with our community of developers.
+            Regen Ledger is a public, proof of stake (POS) blockchain developed with the Cosmos Software
+            Development Kit (SDK) built for verification of claims, agreements & data related to ecological
+            state. Regen Ledger enables multiple registries to communicate and transact with each other
+            producing a public ecological accounting system. Get involved with our community of developers.
           </p>
+          <ContainedButton className={classes.button}>Learn More</ContainedButton>
         </Grid>
       </Grid>
     </BackgroundImage>
-  )
-}
+  );
+};
 
-export default HomeLedger
+export default HomeLedger;

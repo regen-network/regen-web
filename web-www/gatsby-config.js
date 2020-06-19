71fe78972ebc46dc1855df8b3c6f5ea2676e8722
Cory Levinson <cjlevinson@gmail.com>
2020-06-18 21:06:54 -0700

    refactor marketplace section to generate content from yaml file

--- web-www/gatsby-config.js
+++ web-www/gatsby-config.js
@@ -38,6 +38,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-transformer-yaml`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/content/`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,

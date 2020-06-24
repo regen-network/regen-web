fcae7bcf08fdf0ecda9c9613266b39e2862058f7
blushi <marie.gauthier63@gmail.com>
2020-06-24 13:06:45 +0200

    Use yaml for buyers top section content

--- web-www/src/sections/buyers/top-section.tsx
+++ web-www/src/sections/buyers/top-section.tsx
@@ -82,10 +82,21 @@ const TopSection = () => {
               }
             }
           }
+          text: contentYaml {
+            topSection {
+              header
+              body {
+                start
+                underlined
+                end
+              }
+            }
+          }
         }
       `}
       render={data => {
         const imageData = data.desktop.childImageSharp.fluid;
+        const content = data.text.topSection;
         return (
           <div>
             <BackgroundImage
@@ -97,19 +108,18 @@ const TopSection = () => {
               <div className={classes.backgroundGradient} />
               <div className={classes.text}>
                 <Title color="primary" variant="h1">
-                  Meet your climate commitments. Regenerate the planet.
+                  {content.header}
                 </Title>
                 <Typography component="div" className={classes.subtitle}>
-                  Buying{' '}
+                  {content.body.start}{' '}
                   <Tooltip
                     arrow
                     placement="top"
                     title="Ecosystem services are the benefits people receive from healthy ecosystems, including clean air and water, resilient food systems, and mitigation of extreme climate events."
                   >
-                    <span className={classes.tooltip}>ecosystem service credits</span>
+                    <span className={classes.tooltip}>{content.body.underlined}</span>
                   </Tooltip>{' '}
-                  motivates land stewards around the world to sequester carbon, improve biodiversity, and
-                  more.
+                  {content.body.end}
                 </Typography>
               </div>
             </BackgroundImage>

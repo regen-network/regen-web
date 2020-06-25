4b558aea7d177e14880e4ba95d39fee9dae749da
Jared <neuralsplash@gmail.com>
2020-06-25 10:23:40 -0600

    hamburger svg component

--- web-components/src/components/mobile-menu/index.tsx
+++ web-components/src/components/mobile-menu/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Theme } from '@material-ui/core';
 import clsx from 'clsx';
+import HamburgerIcon from 'web-components/lib/components/icons/HamburgerIcon';
 
 //replacing #000 on line 15, color property, with theme.palette.primary.contrastText doesn't yield black text in the dropdown. Not sure why
 const useStyles = makeStyles((theme: Theme) => ({
@@ -32,9 +33,7 @@ const MobileMenu = ({ textColor, children, className }: Props): JSX.Element => {
   let classes = useStyles({});
   return (
     <div className={clsx(className, classes.root)}>
-      <hr className={classes.bar} />
-      <hr className={classes.bar} />
-      <hr className={classes.bar} />
+      <HamburgerIcon />
     </div>
   );
 };

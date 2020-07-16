b18bee2b91aab7f48d94ab81c823bdd050fbca13
blushi <marie.gauthier63@gmail.com>
2020-07-16 13:47:44 +0200

    Linting

--- web-components/src/components/banner/index.tsx
+++ web-components/src/components/banner/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles, useTheme, Theme } from '@material-ui/core';
+import { makeStyles, Theme } from '@material-ui/core';
 
 interface BannerProps {
   text: string;

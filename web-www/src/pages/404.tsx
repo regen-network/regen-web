7f43c8808022aca34d6345019dd046d78eda6923
Jared <neuralsplash@gmail.com>
2020-07-28 09:54:36 -0600

    smaller padding on mobile

--- web-www/src/pages/404.tsx
+++ web-www/src/pages/404.tsx
@@ -21,7 +21,6 @@ const useStyles = makeStyles((theme: Theme) => ({
 			paddingTop: theme.spacing(44.5),
 			paddingLeft: 'unset',
 			paddingRight: 'unset',
-			paddingBottom: theme.spacing(30),
 			width: theme.spacing(132.75),
 			margin: '0 auto',
 		},
@@ -29,6 +28,7 @@ const useStyles = makeStyles((theme: Theme) => ({
 			paddingTop: theme.spacing(11),
 			paddingLeft: theme.spacing(5),
 			paddingRight: theme.spacing(5),
+			paddingBottom: theme.spacing(7),
 		},
 	},
 	image: {

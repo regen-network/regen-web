327dfeac1d5ff4c0799c12baffc2caced5a2f70d
blushi <marie.gauthier63@gmail.com>
2020-07-28 09:04:39 +0200

    Add sign up form to lands stewards page

--- web-www/src/pages/land-stewards.tsx
+++ web-www/src/pages/land-stewards.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import SEO from '../components/seo';
 import TopSection from '../sections/land-stewards/TopSection';
@@ -7,8 +7,19 @@ import JoinFarmersSection from '../sections/land-stewards/JoinFarmersSection';
 import PracticesOutcomesSection from '../sections/land-stewards/PracticesOutcomesSection';
 import MoreQuestionsSection from '../sections/land-stewards/MoreQuestionsSection';
 import FeaturedSection from '../sections/shared/FeaturedSection';
+import FixedFooter from 'web-components/lib/components/fixed-footer';
+import Modal from 'web-components/lib/components/modal';
+import ContainedButton from 'web-components/lib/components/buttons/ContainedButton';
 
 const LandStewardsPage = (): JSX.Element => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = (): void => {
+    setOpen(true);
+  };
+
+  const handleClose = (): void => {
+    setOpen(false);
+  };
 
   return (
     <>
@@ -20,6 +31,17 @@ const LandStewardsPage = (): JSX.Element => {
       {/* TODO: add "How it works" section here */}
       <FeaturedSection />
       <MoreQuestionsSection />
+      <FixedFooter justify="flex-end">
+        <>
+          <ContainedButton onClick={handleOpen}>sign up now</ContainedButton>
+        </>
+      </FixedFooter>
+      <Modal open={open} onClose={handleClose}>
+        <iframe
+          title="airtable-signup-form"
+          src="https://airtable.com/embed/shr1dXKQ3vszCKs6Z?backgroundColor=gray"
+        />
+      </Modal>
     </>
   );
 };

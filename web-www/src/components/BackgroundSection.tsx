1534e9058e778d4f986642b9ac8c5126f7b549d8
Jared <neuralsplash@gmail.com>
2020-08-11 09:11:58 -0500

    remove 25% paddingRight on backgroundsection

--- web-www/src/components/BackgroundSection.tsx
+++ web-www/src/components/BackgroundSection.tsx
@@ -31,7 +31,7 @@ const useStyles = makeStyles<Theme, StyleProps>((theme: Theme) => ({
       padding: `${theme.spacing(80)} ${theme.spacing(37.5)} ${theme.spacing(27.5)} ${theme.spacing(37.5)}`,
     },
     [theme.breakpoints.up('xl')]: {
-      padding: `${theme.spacing(80)} 25% ${theme.spacing(27.5)} ${theme.spacing(37.5)}`,
+      padding: `${theme.spacing(80)} ${theme.spacing(37.5)} ${theme.spacing(27.5)} ${theme.spacing(37.5)}`,
     },
     backgroundSize: 'cover',
   }),

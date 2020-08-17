96deda907c1d8fc72cdb2caaf4659d3121f36623
Jared <neuralsplash@gmail.com>
2020-08-17 11:57:53 -0500

    fix top section padding for mobile

--- web-www/src/sections/resources/TopSection.tsx
+++ web-www/src/sections/resources/TopSection.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { graphql, StaticQuery } from 'gatsby';
-import { useTheme } from '@material-ui/core/';
+import { Theme, makeStyles } from '@material-ui/core/';
 
 import BackgroundSection from '../../components/BackgroundSection';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  section: {
+    [theme.breakpoints.down('xs')]: {
+      paddingTop: theme.spacing(42.75),
+    },
+  },
+}));
 
 const TopSection = (): JSX.Element => {
   const gradient =
     'linear-gradient(211.73deg, #FAEBD1 12.63%, #7DC9BF 44.03%, #515D89 75.43%), linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0) 25.06%)';
-  const theme = useTheme();
+  const classes = useStyles();
   /**
    * Tried using background position to make the mobile background-image correct, but it's actually a totally different image in the mockup, not just a different position. Wired up BackgroundSection to take an imageDataMobile prop.
    */
@@ -44,12 +52,7 @@ const TopSection = (): JSX.Element => {
               linearGradient={gradient}
               header="Dig into our documentation."
               body="We strive to be a transparent and open system for creating ecological credits that reward regenerative practices. Learn more about the ins and outs of how the Regen Ledger and Regen Registry function."
-              paddingMobile={`
-                ${theme.spacing(45)} 
-                ${theme.spacing(6.5)} 
-                ${theme.spacing(21)} 
-                ${theme.spacing(6)}
-                `}
+              className={classes.section}
               imageData={data.background.childImageSharp.fluid}
               imageDataMobile={data.backgroundMobile.childImageSharp.fluid}
             />

2a56bf4b7f4301a8ab143aadd6e7a8244da6d8a9
blushi <marie.gauthier63@gmail.com>
2020-08-18 11:57:37 +0200

    Add withHoverColor HOC for social icons

--- web-components/src/components/icons/social/FacebookIcon.tsx
+++ web-components/src/components/icons/social/FacebookIcon.tsx
@@ -3,20 +3,17 @@ import SvgIcon from '@material-ui/core/SvgIcon';
 import { makeStyles, Theme } from '@material-ui/core';
 import clsx from 'clsx';
 
-interface props extends React.HTMLProps<HTMLDivElement> {
-  width?: string;
-  height?: string;
-}
+import withHoverColor, { Props } from './withHoverColor';
 
-const useStyles = makeStyles<Theme, props>((theme: Theme) => ({
-  root: props => ({
-    width: props.width || theme.spacing(9.25),
-    height: props.height || theme.spacing(9.25),
-  }),
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    width: theme.spacing(9.25),
+    height: theme.spacing(9.25),
+  },
 }));
 
-export default function FacebookIcon({ width, height, className }: props): JSX.Element {
-  const classes = useStyles({ width, height });
+function FacebookIcon({ className, color, onMouseEnter, onMouseLeave }: Props): JSX.Element {
+  const classes = useStyles();
 
   return (
     <SvgIcon
@@ -24,13 +21,17 @@ export default function FacebookIcon({ width, height, className }: props): JSX.E
       viewBox="0 0 38 37"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
     >
       <path
         fillRule="evenodd"
         clipRule="evenodd"
         d="M20.9148 12.2536H23.0956V8.78418H20.2209C16.0577 8.78418 16.1568 12.1544 16.1568 12.6501V15.3264H14.2734V18.6967H16.1568V27.8162H20.1218V18.6967H22.7982C22.7982 18.6967 22.9964 17.1107 23.1947 15.3264C22.8973 15.3264 20.2209 15.3264 20.2209 15.3264C20.2209 15.3264 20.2209 13.3439 20.2209 13.0466C20.1218 12.7492 20.5183 12.2536 20.9148 12.2536Z"
-        fill="#FAFAFA"
+        fill={color}
       />
     </SvgIcon>
   );
 }
+
+export default withHoverColor(FacebookIcon);
--- web-components/src/components/icons/social/YoutubeIcon.tsx
+++ web-components/src/components/icons/social/YoutubeIcon.tsx
@@ -3,20 +3,17 @@ import SvgIcon from '@material-ui/core/SvgIcon';
 import { makeStyles, Theme } from '@material-ui/core';
 import clsx from 'clsx';
 
-interface props extends React.HTMLProps<HTMLDivElement> {
-  width?: string;
-  height?: string;
-}
+import withHoverColor, { Props } from './withHoverColor';
 
-const useStyles = makeStyles<Theme, props>((theme: Theme) => ({
-  root: props => ({
-    width: props.width || theme.spacing(9.25),
-    height: props.height || theme.spacing(9.25),
-  }),
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    width: theme.spacing(9.25),
+    height: theme.spacing(9.25),
+  },
 }));
 
-export default function YoutubeIcon({ width, height, className }: props): JSX.Element {
-  const classes = useStyles({ width, height });
+function YoutubeIcon({ className, color, onMouseEnter, onMouseLeave }: Props): JSX.Element {
+  const classes = useStyles();
 
   return (
     <SvgIcon
@@ -24,12 +21,16 @@ export default function YoutubeIcon({ width, height, className }: props): JSX.El
       viewBox="0 0 37 37"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
     >
       <path
         d="M30.6206 12.2647C30.3378 11.1879 29.4894 10.3378 28.4147 10.0403C26.4774 9.51611 18.6999 9.51611 18.6999 9.51611C18.6999 9.51611 10.9224 9.51611 8.97091 10.0403C7.8962 10.3237 7.06189 11.1737 6.76493 12.2647C6.25586 14.2198 6.25586 18.3001 6.25586 18.3001C6.25586 18.3001 6.25586 22.3804 6.77907 24.3356C7.06189 25.4123 7.91035 26.2624 8.98505 26.5599C10.9224 27.0841 18.6999 27.0841 18.6999 27.0841C18.6999 27.0841 26.4774 27.0841 28.4288 26.5599C29.5035 26.2766 30.3378 25.4265 30.6348 24.3356C31.1439 22.3804 31.1439 18.3001 31.1439 18.3001C31.1439 18.3001 31.1439 14.2198 30.6206 12.2647Z"
-        fill="#FAFAFA"
+        fill={color}
       />
       <path d="M16.5039 21.9601V14.6401L22.3599 18.3001L16.5039 21.9601Z" fill="#545555" />
     </SvgIcon>
   );
 }
+
+export default withHoverColor(YoutubeIcon);

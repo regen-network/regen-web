48daa8ea0d19becc0523f953e7c9d6f526e9bb83
blushi <marie.gauthier63@gmail.com>
2020-08-19 13:09:17 +0200

    Complete new timeline UI

--- web-components/src/components/timeline/NewTimeline.tsx
+++ web-components/src/components/timeline/NewTimeline.tsx
@@ -45,17 +45,31 @@ const useStyles = makeStyles<Theme, StyleProps>((theme: Theme) => ({
       },
     },
   },
+  oppositeContent: {
+    marginTop: theme.spacing(7.5),
+    '& img': {
+      marginRight: theme.spacing(4),
+      marginLeft: theme.spacing(4),
+    },
+  },
   content: props => ({
     backgroundColor: theme.palette.primary.main,
     // maxWidth: '75%',
     position: 'relative',
     border: `1px solid ${theme.palette.info.light}`,
     borderRadius: '10px',
-    paddingTop: theme.spacing(7.5),
-    paddingRight: theme.spacing(5.75),
-    paddingBottom: theme.spacing(7.5),
-    paddingLeft: theme.spacing(5.75),
     boxShadow: theme.shadows[1],
+    paddingRight: theme.spacing(5.75),
+    paddingLeft: theme.spacing(5.75),
+    [theme.breakpoints.up('sm')]: {
+      paddingTop: theme.spacing(7.5),
+      paddingBottom: theme.spacing(9.5),
+      marginLeft: props.even ? theme.spacing(3) : theme.spacing(-3),
+    },
+    [theme.breakpoints.down('xs')]: {
+      paddingTop: theme.spacing(5.75),
+      paddingBottom: theme.spacing(8),
+    },
     '&:after': {
       content: '""',
       borderRadius: '2px',
@@ -68,7 +82,6 @@ const useStyles = makeStyles<Theme, StyleProps>((theme: Theme) => ({
         borderTop: props.even ? 'none' : `1px solid ${theme.palette.info.light}`,
         borderLeft: props.even ? `1px solid ${theme.palette.info.light}` : 'none',
         borderBottom: props.even ? `1px solid ${theme.palette.info.light}` : 'none',
-        // bottom: props.last ? theme.spacing(2) : 'auto',
       },
       [theme.breakpoints.down('xs')]: {
         left: theme.spacing(-2),
@@ -82,8 +95,21 @@ const useStyles = makeStyles<Theme, StyleProps>((theme: Theme) => ({
       zIndex: 100,
     },
   }),
+  tags: {
+    textAlign: 'left',
+  },
   tag: props => ({
     backgroundColor: props.color || theme.palette.secondary.main,
+    color: theme.palette.primary.main,
+    lineHeight: theme.spacing(4.5),
+    fontSize: theme.spacing(3.5),
+    letterSpacing: '1px',
+    textTransform: 'uppercase',
+    textAlign: 'center',
+    borderRadius: '2px',
+    fontFamily: theme.typography.h1.fontFamily,
+    padding: `${theme.spacing(0.75)} ${theme.spacing(2)}`,
+    marginRight: theme.spacing(2.5),
   }),
   connector: {
     backgroundColor: theme.palette.grey[100],
@@ -109,21 +135,48 @@ const useStyles = makeStyles<Theme, StyleProps>((theme: Theme) => ({
     paddingLeft: theme.spacing(2.375),
     paddingRight: theme.spacing(2.375),
   },
+  number: {
+    color: theme.palette.grey[100],
+    lineHeight: '130%',
+    fontWeight: 900,
+    fontFamily: theme.typography.h1.fontFamily,
+    paddingRight: theme.spacing(5),
+    [theme.breakpoints.up('sm')]: {
+      fontSize: theme.spacing(22.5),
+      marginTop: theme.spacing(-3.5),
+    },
+    [theme.breakpoints.down('xs')]: {
+      fontSize: theme.spacing(15),
+      marginTop: theme.spacing(-3),
+    },
+  },
+  title: {
+    color: theme.palette.primary.light,
+    lineHeight: '140%',
+    paddingBottom: theme.spacing(4),
+    [theme.breakpoints.down('xs')]: {
+      fontSize: theme.spacing(5.25),
+    },
+  },
 }));
 
 function Tag({ name, color }: Tag): JSX.Element {
   const classes = useStyles({ color });
-  return <div className={classes.tag}>{name}</div>;
+  return <span className={classes.tag}>{name}</span>;
 }
 
 function Content({ item, index }: ContentProps): JSX.Element {
   const classes = useStyles({ even: index % 2 === 0 });
   return (
-    <Grid container className={classes.content}>
-      <Grid item>{index + 1}</Grid>
+    <Grid container wrap="nowrap" className={classes.content}>
+      <Grid className={classes.number} item>
+        {index + 1}
+      </Grid>
       <Grid item>
-        <Title variant="h3">{item.title}</Title>
-        <div>
+        <Title className={classes.title} variant="h3">
+          {item.title}
+        </Title>
+        <div className={classes.tags}>
           {item.tags.map((t, i) => (
             <Tag key={i} name={t.name} color={t.color} />
           ))}
@@ -142,7 +195,7 @@ export default function NewTimeline({ items }: Props): JSX.Element {
         <Timeline align="alternate">
           {items.map((item, index) => (
             <TimelineItem key={index}>
-              <TimelineOppositeContent>
+              <TimelineOppositeContent className={classes.oppositeContent}>
                 <img src={item.imgSrc} alt={item.title} />
               </TimelineOppositeContent>
               <TimelineSeparator className={classes.separator}>

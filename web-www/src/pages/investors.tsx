d3542743ccbc521cab45dc2ced94423df45163d0
Jared <neuralsplash@gmail.com>
2020-09-03 09:34:11 -0600

    add netlify config

--- web-www/src/pages/investors.tsx
+++ web-www/src/pages/investors.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 
 import TopSection from '../sections/investors/TopSection';
-import CoreTeam from '../sections/team/CoreTeamSection';
-import ContributorSection from '../sections/team/ContributorSection';
-import AdvisorSection from '../sections/team/AdvisorSection';
 import SEO from '../components/seo';
 
 const InvestorsPage = (): JSX.Element => {

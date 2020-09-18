57004605530219b60e40417c9bf8d0864d4b5472
Jared <neuralsplash@gmail.com>
2020-09-18 17:26:21 -0600

    remove unused

--- web-components/src/components/cards/PartnerCard.tsx
+++ web-components/src/components/cards/PartnerCard.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { makeStyles, Theme } from '@material-ui/core';
-import clsx from 'clsx';
 
 import Card from './Card';
 
@@ -23,13 +22,7 @@ export default function PartnerCard({ imageUrl }: PartnerCardProps): JSX.Element
   const classes = useStyles({});
 
   return (
-    <Card
-      className={classes.card}
-      width={width}
-      elevation={elevation}
-      borderColor={borderColor}
-      borderRadius={borderRadius}
-    >
+    <Card className={classes.card}>
       <img className={classes.img} src={imageUrl} alt={imageUrl}></img>
     </Card>
   );

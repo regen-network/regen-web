e790a4bbe5be1fca42c02d061d44adff0a327115
blushi <marie.gauthier63@gmail.com>
2020-09-28 12:38:32 +0200

    Add get involved section

--- web-www/src/pages/developers.tsx
+++ web-www/src/pages/developers.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import SEO from '../components/seo';
 import TopSection from '../sections/developers/TopSection';
+import InvolvedSection from '../sections/developers/InvolvedSection';
 import ApproachSection from '../sections/developers/ApproachSection';
 import LedgerSection from '../sections/developers/LedgerSection';
 import ConnectSection from '../sections/developers/ConnectSection';
@@ -12,6 +13,7 @@ const DevelopersPage = (): JSX.Element => {
       <SEO title="For Developers" />
       <TopSection />
       <ApproachSection />
+      <InvolvedSection />
       <LedgerSection />
       <ConnectSection />
     </>

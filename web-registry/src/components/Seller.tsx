2113349fe35b48e07913e77d9709f9399cda5788
blushi <marie.gauthier63@gmail.com>
2020-11-24 16:32:03 +0100

    Add stripe account / payouts button

--- web-registry/src/components/Seller.tsx
+++ web-registry/src/components/Seller.tsx
@@ -1,16 +1,32 @@
 import React, { useCallback } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
 
 import NavBar from './NavBar';
 import getOrigin from '../lib/origin';
+import OutlinedButton from 'web-components/lib/components/buttons/OutlinedButton';
 import ContainedButton from 'web-components/lib/components/buttons/ContainedButton';
 import Title from 'web-components/lib/components/title';
 
 const origin: string = getOrigin();
 
+const GET_USER = gql`
+  query UserByEmail($email: String!) {
+    userByEmail(email: $email) {
+      stripeAccountId
+    }
+  }
+`;
+
 const Seller = (): JSX.Element => {
   const { getAccessTokenSilently, isAuthenticated, user } = useAuth0();
+
+  const { data, loading } = useQuery(GET_USER, {
+    errorPolicy: 'ignore',
+    variables: { email: user?.email },
+  });
 
   const accountSetup = useCallback(() => {
     const getAccountLink = async (): Promise<void> => {
@@ -36,24 +52,66 @@ const Seller = (): JSX.Element => {
     getAccountLink();
   }, [getAccessTokenSilently, user]);
 
+  const viewDashboard = useCallback(
+    (account?: boolean) => (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      const getLoginLink = async (): Promise<void> => {
+        try {
+          const accessToken = await getAccessTokenSilently();
+          const apiUri = process.env.REACT_APP_API_URI || 'http://localhost:5000';
+          const res = await axios({
+            method: 'POST',
+            url: `${apiUri}/create-login-link`,
+            data: {
+              accountId: data?.userByEmail?.stripeAccountId,
+            },
+            headers: { authorization: `Bearer ${accessToken}` },
+          });
+          let url = res.data.url;
+          if (account) {
+            // Directly link to the account tab
+            url = url + '#/account';
+          }
+          window.location.assign(url);
+        } catch (e) {
+          // console.log(e.message);
+        }
+      };
+
+      getLoginLink();
+    },
+    [data, getAccessTokenSilently],
+  );
+
   const search = new URLSearchParams(window.location.search);
 
   return (
     <div style={{ padding: '1rem' }}>
       <div style={{ textAlign: 'center', paddingBottom: '1rem' }}>
         {isAuthenticated &&
-          (search.get('setup') === 'true' ? (
+          !loading &&
+          (data && data.userByEmail && data.userByEmail.stripeAccountId ? (
             <div>
-              <Title variant="h3" align="center">
-                Thank you!
-              </Title>
-              <ContainedButton
-                style={{ marginTop: '1rem' }}
-                href="https://dashboard.stripe.com/"
-                target="_blank"
-              >
-                View stripe dashboard
-              </ContainedButton>
+              {search.get('setup') === 'true' && (
+                <Title variant="h3" align="center">
+                  Thank you!
+                </Title>
+              )}
+              <div>
+                <ContainedButton
+                  style={{ fontSize: '1.25rem', marginTop: '1rem', padding: '1rem' }}
+                  onClick={viewDashboard(true)}
+                >
+                  View stripe account
+                </ContainedButton>
+              </div>
+              <div>
+                <OutlinedButton
+                  style={{ fontSize: '1.25rem', marginTop: '1rem', padding: '1rem' }}
+                  onClick={viewDashboard()}
+                >
+                  View payouts on stripe
+                </OutlinedButton>
+              </div>
             </div>
           ) : (
             <ContainedButton onClick={accountSetup}>Account Setup</ContainedButton>

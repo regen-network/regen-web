c04242c8094c587d45a0a06153ecb13ce741e661
blushi <marie.gauthier63@gmail.com>
2020-12-17 10:52:22 +0100

    Update to project page

--- web-registry/src/components/sections/MoreProjects.tsx
+++ web-registry/src/components/sections/MoreProjects.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import { makeStyles, Theme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
 import Section from 'web-components/lib/components/section';
 import ProjectCard from 'web-components/lib/components/cards/ProjectCard';
+import Theme from 'web-components/lib/theme/muiTheme';
 import { Project } from '../../mocks';
 import background from '../../assets/more-projects-bg.jpg';
 import getRegistryUrl from '../../lib/registryUrl';
@@ -13,7 +14,7 @@ interface MoreProjectsProps {
   projects: Project[];
 }
 
-const useStyles = makeStyles((theme: Theme) => ({
+const useStyles = makeStyles((theme: typeof Theme) => ({
   background: {
     paddingBottom: theme.spacing(20),
     backgroundImage: `url("${background}")`,
@@ -21,6 +22,13 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   grid: {
     paddingTop: theme.spacing(8.75),
+  },
+  item: {
+    [theme.breakpoints.down(theme.breakpoints.values.tablet)]: {
+      flexGrow: 0,
+      maxWidth: '100%',
+      flexBasis: '100%',
+    },
   },
 }));
 
@@ -31,7 +39,7 @@ const MoreProjects = ({ projects }: MoreProjectsProps): JSX.Element => {
       <Section title="More Projects">
         <Grid container className={classes.grid} spacing={5}>
           {projects.map((project, i) => (
-            <Grid item xs={12} sm={4} key={project.id}>
+            <Grid item xs={12} sm={6} md={4} key={project.id} className={classes.item}>
               <Link href={getRegistryUrl(`/projects/${project.id}`)}>
                 <ProjectCard
                   name={project.name}

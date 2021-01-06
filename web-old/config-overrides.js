b2f0f0907305eb9572724c44a7078fe3c71d6d7f
blushi <marie.gauthier63@gmail.com>
2021-01-06 16:15:28 +0100

    Remove old folders

--- web-old/config-overrides.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-
-module.exports = function override(config) {
-  config.plugins = config.plugins.filter(plugin => {
-    return plugin.constructor.name !== 'ForkTsCheckerWebpackPlugin';
-  });
-  return config;
-};
--- web-old/legacy/reducers/index.js
+++
@@ -1,3 +0,0 @@
-export { default as auth } from "./auth";
-export { default as map } from "./map";
-export { default as entry } from "./entry";

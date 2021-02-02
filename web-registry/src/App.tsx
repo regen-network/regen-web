c38a0127dcda3a65d93cb3f2b5ed4eadccc6148f
blushi <marie.gauthier63@gmail.com>
2021-02-02 11:32:28 +0100

    Debug

--- web-registry/src/App.tsx
+++ web-registry/src/App.tsx
@@ -267,7 +267,7 @@ const App: React.FC = (): JSX.Element => {
     const email: string = authError.error_description.split(':')[1];
     history.push(`/verify-email?email=${email}`);
   }
-
+  console.log(process.env.PUBLIC_URL);
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <ScrollToTop />

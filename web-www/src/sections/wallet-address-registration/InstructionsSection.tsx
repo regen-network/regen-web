344253100d3e56f84a505c93215d0dabcbc4c65d
Mark Hagelberg <mark.hagelberg@gmail.com>
2021-02-18 11:22:03 -0500

    routing poc
    transparent header
    
    instructions section
    
    - connect to keplr button
    - show error message if no keplr detected
    
    WalletConnectionButton seperate component
    
    FormSection
    
    recaptcha wip
    
    config yml
    recaptcha
    
    recaptcha then form
    
    hard-coded user and pw
    
    PR feedback:
    use production recaptcha key,
    cleanup
    
    verify recaptcha through api
    
    mobile style
    
    cleanup
    
    cleanup
    
    check for window for server-side render - netlify error
    
    better keplr check organization
    
    remove space from pw
    
    check for keplr after mount
    
    check for keplr always
    
    green anchor link
    
    useLayoutEffect
    
    wallet found message, keplr enable
    
    tweak
    
    setTimeout 500ms
    
    200ms
    
    yaml edits
    
    yaml fix
    
    yaml fix
    
    move checkForKeplr up higher to mouseover, so not blocked by rendering flow
    
    enable on click wip
    
    regen-devnet-4
    
    enable chain call on click instead of load
    
    remove enable call, just one popup
    
    recaptchaMessage to prompt user
    
    features: ['stargate']
    
    recaptcha msg fontsize

---
+++ web-www/src/sections/wallet-address-registration/InstructionsSection.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import { Theme, makeStyles } from '@material-ui/core';
+import ReactHtmlParser from 'react-html-parser';
+import Title from 'web-components/lib/components/title';
+import Section from 'web-components/lib/components/section';
+import Description from 'web-components/lib/components/description';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  section: {
+    [theme.breakpoints.up('sm')]: {
+      paddingTop: theme.spacing(21.5),
+    },
+    [theme.breakpoints.down('xs')]: {
+      paddingLeft: theme.spacing(3),
+      paddingTop: theme.spacing(17),
+    },
+  },
+  title: {
+    marginBottom: theme.spacing(8.5),
+    [theme.breakpoints.up('sm')]: {
+      marginBottom: theme.spacing(6.75),
+    },
+  },
+  body: {
+    textAlign: 'center',
+    marginBottom: theme.spacing(15),
+    maxWidth: theme.spacing(225),
+    margin: '0 auto',
+    [theme.breakpoints.down('sm')]: {
+      fontSize: theme.spacing(4),
+    },
+    [theme.breakpoints.up('sm')]: {
+      fontSize: theme.spacing(5.5),
+    },
+  },
+}));
+
+const InstructionsSection = (): JSX.Element => {
+  const data = useStaticQuery(graphql`
+    query {
+      text: walletAddressRegistrationYaml {
+        instructionsSection {
+          header
+          body
+        }
+      }
+    }
+  `);
+  const content = data?.text?.instructionsSection;
+  const classes = useStyles();
+  return (
+    <Section className={classes.section}>
+      <Title className={classes.title} variant="h3" align="center">
+        {content?.header}
+      </Title>
+      <Description className={classes.body}>{ReactHtmlParser(content?.body)}</Description>
+    </Section>
+  );
+};
+
+export default InstructionsSection;

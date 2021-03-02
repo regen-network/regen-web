e83246ba15e3814ba9727fa6d557da6031720582
blushi <marie.gauthier63@gmail.com>
2021-03-02 14:04:32 +0100

    Refactor Faq

--- web-www/src/pages/faq.tsx
+++ web-www/src/pages/faq.tsx
@@ -1,39 +1,9 @@
 import React from 'react';
-import { useStaticQuery, graphql, PageProps, navigate } from 'gatsby';
-import { makeStyles, Theme } from '@material-ui/core';
+import { useStaticQuery, graphql, PageProps } from 'gatsby';
 
-import SEO from '../components/seo';
-import Section from 'web-components/src/components/section';
-import FAQ from 'web-components/lib/components/faq';
+import Faq from '../components/Faq';
 
-const useStyles = makeStyles((theme: Theme) => ({
-  root: {
-    backgroundColor: theme.palette.grey[200],
-  },
-  section: {
-    [theme.breakpoints.up('sm')]: {
-      paddingTop: theme.spacing(13.75),
-      paddingBottom: theme.spacing(30),
-    },
-    [theme.breakpoints.down('xs')]: {
-      paddingTop: theme.spacing(7.5),
-      paddingBottom: theme.spacing(20),
-      position: 'relative',
-    },
-  },
-  title: {
-    [theme.breakpoints.up('sm')]: {
-      paddingBottom: theme.spacing(12),
-    },
-    [theme.breakpoints.down('xs')]: {
-      paddingBottom: theme.spacing(7.5),
-    },
-  },
-}));
-
-const FAQPage = ({ location }: PageProps): JSX.Element => {
-  const classes = useStyles();
-
+const FAQPage = (props: PageProps): JSX.Element => {
   const data = useStaticQuery(graphql`
     query {
       faqYaml {
@@ -48,16 +18,7 @@ const FAQPage = ({ location }: PageProps): JSX.Element => {
     }
   `);
 
-  return (
-    <>
-      <SEO location={location} title="FAQ" description="Explore Regen Network’s frequently asked questions" />
-      <div className={classes.root}>
-        <Section title="FAQ" titleVariant="h1" titleClassName={classes.title} className={classes.section}>
-          <FAQ categories={data.faqYaml.categories} navigate={navigate} />
-        </Section>
-      </div>
-    </>
-  );
+  return <Faq categories={data.faqYaml.categories} {...props} />;
 };
 
 export default FAQPage;
--- web-www/src/templates/Faq.tsx
+++ web-www/src/templates/Faq.tsx
@@ -1,35 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { useStaticQuery, graphql, PageProps, navigate } from 'gatsby';
-import { makeStyles, Theme } from '@material-ui/core';
+import { useStaticQuery, graphql, PageProps } from 'gatsby';
 
-import SEO from '../components/seo';
-import Section from 'web-components/src/components/section';
-import FAQ from 'web-components/lib/components/faq';
-
-const useStyles = makeStyles((theme: Theme) => ({
-  root: {
-    backgroundColor: theme.palette.grey[200],
-  },
-  section: {
-    [theme.breakpoints.up('sm')]: {
-      paddingTop: theme.spacing(13.75),
-      paddingBottom: theme.spacing(30),
-    },
-    [theme.breakpoints.down('xs')]: {
-      paddingTop: theme.spacing(7.5),
-      paddingBottom: theme.spacing(20),
-      position: 'relative',
-    },
-  },
-  title: {
-    [theme.breakpoints.up('sm')]: {
-      paddingBottom: theme.spacing(12),
-    },
-    [theme.breakpoints.down('xs')]: {
-      paddingBottom: theme.spacing(7.5),
-    },
-  },
-}));
+import Faq from '../components/Faq';
 
 interface Props extends PageProps {
   pageContext: {
@@ -37,8 +9,7 @@ interface Props extends PageProps {
   };
 }
 
-const FAQPage = ({ pageContext, location }: Props): JSX.Element => {
-  const classes = useStyles();
+const FAQPage = (props: Props): JSX.Element => {
   const data = useStaticQuery(graphql`
     query {
       faqYaml: faqYaml {
@@ -52,31 +23,28 @@ const FAQPage = ({ pageContext, location }: Props): JSX.Element => {
       }
     }
   `);
-  const [question, setQuestion] = useState<string | null>();
+
+  const [question, setQuestion] = useState<number | undefined>();
   useEffect(() => {
     if (typeof window !== 'undefined') {
       const queryString = window.location.search;
       const urlParams = new URLSearchParams(queryString);
       if (urlParams) {
-        setQuestion(urlParams.get('question'));
+        const question = urlParams.get('question');
+        if (question) {
+          setQuestion(parseInt(question));
+        }
       }
     }
   }, []);
 
   return (
-    <>
-      <SEO location={location} title="FAQ" description="Explore Regen Network’s frequently asked questions" />
-      <div className={classes.root}>
-        <Section title="FAQ" titleVariant="h1" titleClassName={classes.title} className={classes.section}>
-          <FAQ
-            categories={data.faqYaml.categories}
-            navigate={navigate}
-            header={pageContext.header}
-            question={question}
-          />
-        </Section>
-      </div>
-    </>
+    <Faq
+      header={props.pageContext.header}
+      question={question}
+      categories={data.faqYaml.categories}
+      {...props}
+    />
   );
 };
 

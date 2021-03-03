f325fc16fc12c124a775f955e7ff84d12628bc34
Mark Hagelberg <mark.hagelberg@gmail.com>
2021-03-03 09:58:27 -0500

    buttons desktop style

--- web-www/src/sections/land-stewards/MoreQuestionsSection.tsx
+++ web-www/src/sections/land-stewards/MoreQuestionsSection.tsx
@@ -5,15 +5,18 @@ import ReactHtmlParser from 'react-html-parser';
 
 import FAQSection from '../shared/FAQSection';
 import Description from 'web-components/lib/components/description';
+import ContainedButton from 'web-components/src/components/buttons/ContainedButton';
+import OutlinedButton from 'web-components/lib/components/buttons/OutlinedButton';
 
 const useStyles = makeStyles((theme: Theme) => ({
   content: {
     display: 'flex',
+    flexDirection: 'column',
   },
   description: {
-    // flex: '1 0 auto',
     color: theme.palette.primary.main,
     fontSize: theme.spacing(7),
+    fontWeight: 600,
     textAlign: 'center',
 
     // [theme.breakpoints.down('xs')]: {
@@ -24,9 +27,28 @@ const useStyles = makeStyles((theme: Theme) => ({
     //   fontSize: theme.spacing(3.5),
     //   padding: `${theme.spacing(2)} ${theme.spacing(5)}`,
     // },
+    '& a': {
+      color: theme.palette.secondary.light,
+    },
   },
   buttons: {
     display: 'flex',
+    justifyContent: 'space-evenly',
+    marginTop: theme.spacing(5),
+    paddingTop: theme.spacing(5),
+    paddingBottom: theme.spacing(5),
+  },
+  button: {
+    width: 300, //todo
+
+    [theme.breakpoints.down('xs')]: {
+      padding: `${theme.spacing(3)} ${theme.spacing(4)}`,
+      fontSize: '1.125rem',
+      width: 250, //todo
+    },
+    [theme.breakpoints.up('sm')]: {
+      fontSize: '1.3125rem',
+    },
   },
 }));
 
@@ -56,7 +78,10 @@ const MoreQuestionsSection = (): JSX.Element => {
     <FAQSection header={content.header} category="carbonplus credits" imageData={imageData}>
       <div className={classes.content}>
         <Description className={classes.description}>{ReactHtmlParser(content.description)}</Description>
-        <div className={classes.buttons}></div>
+        <div className={classes.buttons}>
+          <ContainedButton className={classes.button}>view resources</ContainedButton>
+          <OutlinedButton className={classes.button}>start the process</OutlinedButton>
+        </div>
       </div>
     </FAQSection>
   );

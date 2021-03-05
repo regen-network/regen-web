92f2cc55fa9349c90977157b4d246dd5a71b3636
Mark Hagelberg <mark.hagelberg@gmail.com>
2021-03-05 16:18:06 -0500

    test with ProjectMedia

--- web-components/src/components/image/index.tsx
+++ web-components/src/components/image/index.tsx
@@ -1,16 +1,33 @@
 import React, { useRef, useState, useEffect } from 'react';
+import { makeStyles, Theme } from '@material-ui/core';
+import clsx from 'clsx';
 
-export interface ImageProps {
+// interface ImageProps extends React.HTMLProps<HTMLImageElement> { TODO - errors with this
+interface ImageProps {
   src: string; // image storage url
-  alt: string;
+  alt?: string;
   options?: any; //todo
+  className?: string;
+  width?: number;
+  height?: number;
 }
+
+const useStyles = makeStyles<Theme>((theme: Theme) => ({
+  figure: {
+    marginBlockStart: 0,
+    marginBlockEnd: 0,
+    marginInlineStart: 0,
+    marginInlineEnd: 0,
+  },
+}));
 
 /**
  * Use this component if image is stored in S3 (or app's main image store)
+ * TODO: fallback to original if server fails
  */
-export default function Image({ src, alt = '', options = {} }: ImageProps): JSX.Element {
-  const imgRef = useRef<HTMLElement | null>(null);
+const Image: React.FC<ImageProps> = ({ src = '', alt = '', options = {}, className, ...rest }) => {
+  const classes = useStyles({});
+  const imgRef = useRef<HTMLDivElement | null>(null);
   const [width, setWidth] = useState(0);
   const [queryString, setQueryString] = useState('');
   const [path, setPath] = useState('');
@@ -44,15 +61,17 @@ export default function Image({ src, alt = '', options = {} }: ImageProps): JSX.
         return (queryParams += `${i < 1 ? '?' : '&'}${option}=${options[option]}`);
       });
       setQueryString(queryParams);
-    } else {
-      console.log('width', width);
     }
   }, [width, imgRef, options]);
 
   return (
-    <figure ref={imgRef}>
+    <figure ref={imgRef} className={clsx(className, classes.figure)}>
       {// If the container width has been set, display the image else null
-      width > 0 && queryString ? <img src={`${imageServer}${path}${queryString}`} alt={alt} /> : null}
+      width > 0 && queryString ? (
+        <img {...rest} className={className} src={`${imageServer}${path}${queryString}`} alt={alt} />
+      ) : null}
     </figure>
   );
-}
+};
+
+export default Image;

1e7dc69245475e26de7dbad26ca8ec4046d1ce81
David Daniel <davedan7@gmail.com>
2021-04-11 22:20:35 -0600

    Choose credit class page

---
+++ web-registry/src/components/ChooseCreditClass.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+
+import ImageActionCard from 'web-components/lib/components/cards/ImageActionCard';
+import Section from 'web-components/lib/components/section';
+import { getImgSrc } from '../lib/imgSrc';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  card: {
+    margin: theme.spacing(4),
+  },
+}));
+
+type CreditClass = {
+  title: string;
+  description: string;
+  image: string;
+};
+const CREDIT_CLASSES: CreditClass[] = [
+  {
+    title: 'Grasslands',
+    description:
+      'This credit class is a built as a holistic credit that includes multiple ecological benefits: Carbon Sequestration and Net GHG reduction, increased animal welfare, ecosystem health, and soil health.',
+    image: getImgSrc('credit-class-grasslands.png'),
+  },
+  {
+    title: 'Forestry',
+    description:
+      'This credit class is a built as a holistic credit that includes multiple ecological benefits: Carbon Sequestration and Net GHG reduction, ecosystem health, and soil health.',
+    image: getImgSrc('credit-class-forestry.png'),
+  },
+];
+
+const ChooseCrediClass: React.FC = () => {
+  const classes = useStyles();
+
+  function handleSelection(cc: CreditClass): void {
+    console.log('Selected credit class:  :>> ', cc); // eslint-disable-line no-console
+  }
+
+  return (
+    <Section title="Choose a credit class">
+      <Box display="flex" flexWrap="wrap" justifyContent="center">
+        {CREDIT_CLASSES.map((c, i) => (
+          <ImageActionCard
+            key={i}
+            className={classes.card}
+            description={c.description}
+            imgSrc={c.image}
+            onClick={() => handleSelection(c)}
+            title={() => (
+              <>
+                Carbon<i>Plus</i> {c.title}
+              </>
+            )}
+          />
+        ))}
+      </Box>
+    </Section>
+  );
+};
+
+export default ChooseCrediClass;

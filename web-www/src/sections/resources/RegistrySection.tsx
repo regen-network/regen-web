fe8dd3d372bfcf50ab83904bd613db779b4c7ac8
David Daniel <davedan7@gmail.com>
2021-04-25 23:05:43 -0600

    Split resource page into sections, add documents, yaml

--- web-www/src/sections/resources/RegistrySection.tsx
+++ web-www/src/sections/resources/RegistrySection.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { graphql, StaticQuery } from 'gatsby';
 import { Theme, makeStyles } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 
 import Title from 'web-components/lib/components/title';
 import ResourceCardsSlider from 'web-components/lib/components/sliders/ResourceCards';
+import Table from 'web-components/lib/components/table';
 import BackgroundSection from '../../components/BackgroundSection';
 
 const useStyles = makeStyles((theme: Theme) => ({
@@ -15,6 +18,31 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   section: {
     borderBottom: `1px solid ${theme.palette.grey[300]}`,
+  },
+  subtitle: {
+    color: theme.palette.info.dark,
+    textTransform: 'uppercase',
+    fontWeight: 800,
+    letterSpacing: '1px',
+    [theme.breakpoints.up('sm')]: {
+      fontSize: theme.spacing(4),
+      margin: theme.spacing(8, 0),
+    },
+    [theme.breakpoints.down('xs')]: {
+      fontSize: theme.spacing(3.5),
+      margin: theme.spacing(8, 0),
+    },
+  },
+  table: {
+    border: `2px solid ${theme.palette.secondary.contrastText}`,
+    background: theme.palette.grey[50],
+    borderRadius: '5px',
+    '& .MuiTableCell-head': {
+      padding: theme.spacing(5, 5),
+    },
+    '& .MuiTableCell-body': {
+      padding: theme.spacing(3, 4),
+    },
   },
 }));
 
@@ -34,16 +62,26 @@ const RegistrySection = (): JSX.Element => {
           text: resourcesYaml {
             registrySection {
               header
-              cards {
-                image {
-                  extension
-                  publicURL
-                }
+              documentTableTitle
+              documents {
+                name
+                type
+                date
+                url
+              }
+              subsections {
                 title
-                updated
-                description
-                buttonText
-                link
+                cards {
+                  image {
+                    extension
+                    publicURL
+                  }
+                  title
+                  updated
+                  description
+                  buttonText
+                  link
+                }
               }
             }
           }
@@ -62,7 +100,20 @@ const RegistrySection = (): JSX.Element => {
               <Title className={classes.title} variant="h3" align="left">
                 {content.header}
               </Title>
-              <ResourceCardsSlider items={content.cards} />
+              {content?.subsections?.map((sub: any, i: number) => (
+                <React.Fragment key={i}>
+                  <Typography variant="h1" className={classes.subtitle}>
+                    {sub.title}
+                  </Typography>
+                  <ResourceCardsSlider items={sub.cards} />
+                </React.Fragment>
+              ))}
+              <Typography variant="h1" className={classes.subtitle}>
+                {content.documentTableTitle}
+              </Typography>
+              <Box className={classes.table}>
+                <Table rows={content.documents} />
+              </Box>
             </BackgroundSection>
           </>
         );

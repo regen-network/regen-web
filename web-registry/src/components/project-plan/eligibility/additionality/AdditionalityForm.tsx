030fcf74121d529308a8ffc7e498bd6add5c6866
David Daniel <davedan7@gmail.com>
2021-05-19 08:45:54 -0600

    Pushing for re-build

--- web-registry/src/components/project-plan/eligibility/additionality/AdditionalityForm.tsx
+++ web-registry/src/components/project-plan/eligibility/additionality/AdditionalityForm.tsx
@@ -93,6 +93,7 @@ export const AdditionalityForm: React.FC<AdditionalityFormProps> = p => {
         return errors;
       }}
       onSubmit={async (values, { setSubmitting }) => {
+        console.log('values :>> ', values);
         setSubmitting(true);
         try {
           await p.submit(values);

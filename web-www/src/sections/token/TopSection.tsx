64fded7c2de5abe521e2315053b0fb45a2cf9dbd
Mark Hagelberg <mark.hagelberg@gmail.com>
2021-05-25 15:09:13 -0400

    token image logo

--- web-www/src/sections/token/TopSection.tsx
+++ web-www/src/sections/token/TopSection.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
-import { Theme, makeStyles } from '@material-ui/core/';
+import Img, { FluidObject } from 'gatsby-image';
+import { Theme, makeStyles } from '@material-ui/core';
 
 import BackgroundSection from '../../components/BackgroundSection';
+import Title from 'web-components/lib/components/title';
 
 const useStyles = makeStyles<Theme>((theme: Theme) => ({
   section: props => ({
     [theme.breakpoints.down('xs')]: {
-      paddingTop: theme.spacing(20),
       paddingLeft: 'none',
       paddingRight: 'none',
     },
   }),
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  token: {
+    width: 70,
+    height: 70,
+    marginRight: theme.spacing(4),
+    [theme.breakpoints.down('xs')]: {
+      marginRight: theme.spacing(2),
+    },
+  },
 }));
 
 const TopSection = (): JSX.Element => {
@@ -29,6 +42,13 @@ const TopSection = (): JSX.Element => {
         topSection {
           header
           body
+          image {
+            childImageSharp {
+              fluid(quality: 90) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
         }
       }
     }
@@ -39,7 +59,18 @@ const TopSection = (): JSX.Element => {
     <BackgroundSection
       className={classes.section}
       linearGradient="linear-gradient(209.83deg, rgba(250, 235, 209, 0.8) 11.05%, rgba(125, 201, 191, 0.8) 43.17%, rgba(81, 93, 137, 0.8) 75.29%)"
-      header={content?.header}
+      header={
+        <div className={classes.header}>
+          <Img className={classes.token} fluid={content?.image?.childImageSharp?.fluid} />
+          <Title
+            color="primary"
+            variant="h1"
+            // className={clsx(titleClassName, classes.title)}
+          >
+            {content.header}
+          </Title>
+        </div>
+      }
       body={content?.body}
       imageData={imageData}
     />

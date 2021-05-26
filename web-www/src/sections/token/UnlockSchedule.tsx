f3ee81b0b36bbad680d0e7ccead18d11d764c73d
Mark Hagelberg <mark.hagelberg@gmail.com>
2021-05-26 01:18:29 -0400

    hide scrollbar, hardcode label

--- web-www/src/sections/token/UnlockSchedule.tsx
+++ web-www/src/sections/token/UnlockSchedule.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import { Theme, makeStyles } from '@material-ui/core';
-import moment from 'moment';
 import clsx from 'clsx';
 
 import Section from 'web-components/src/components/section';
@@ -30,8 +29,13 @@ const useStyles = makeStyles<Theme>((theme: Theme) => ({
   },
   chartScroll: {
     width: '100%',
-    overflowX: 'scroll',
     justifyContent: 'center',
+    '&::-webkit-scrollbar': {
+      display: 'none',
+    },
+    [theme.breakpoints.down('sm')]: {
+      overflowX: 'scroll',
+    },
   },
   chartWrapper: {
     width: theme.spacing(300),
@@ -49,47 +53,48 @@ type QueryData = {
 };
 
 interface TokenUnlock {
+  label: string;
   date: string;
   tokens: number;
 }
 
 const data: TokenUnlock[] = [
-  { date: '2021-Apr', tokens: 56861.875 },
-  { date: '2021-May', tokens: 344298.5694 },
-  { date: '2021-Jun', tokens: 337154.7361 },
-  { date: '2021-Jul', tokens: 345489.0694 },
-  { date: '2021-Aug', tokens: 341917.1528 },
-  { date: '2021-Sep', tokens: 396050.0694 },
-  { date: '2021-Oct', tokens: 396698.2778 },
-  { date: '2021-Nov', tokens: 393827.0278 },
-  { date: '2021-Dec', tokens: 418542.4028 },
-  { date: '2022-Jan', tokens: 604735.6944 },
-  { date: '2022-Feb', tokens: 1193871.111 },
-  { date: '2022-Mar', tokens: 3029425.195 },
-  { date: '2022-Apr', tokens: 3390454.361 },
-  { date: '2022-May', tokens: 1701706.569 },
-  { date: '2022-Jun', tokens: 1425918.319 },
-  { date: '2022-Jul', tokens: 1650848.736 },
-  { date: '2022-Aug', tokens: 1392180.903 },
-  { date: '2022-Sep', tokens: 1357516.361 },
-  { date: '2022-Oct', tokens: 1372091.903 },
-  { date: '2022-Nov', tokens: 1352185.569 },
-  { date: '2022-Dec', tokens: 1349644.861 },
-  { date: '2023-Jan', tokens: 1355450.778 },
-  { date: '2023-Feb', tokens: 1179245.486 },
-  { date: '2023-Mar', tokens: 1343155.194 },
-  { date: '2023-Apr', tokens: 1316934.569 },
-  { date: '2023-May', tokens: 1310127.653 },
-  { date: '2023-Jun', tokens: 1306387.736 },
-  { date: '2023-Jul', tokens: 1441900.444 },
-  { date: '2023-Aug', tokens: 1311723.153 },
-  { date: '2023-Sep', tokens: 1260604.403 },
-  { date: '2023-Oct', tokens: 1262894.444 },
-  { date: '2023-Nov', tokens: 1249142.444 },
-  { date: '2023-Dec', tokens: 1235097.903 },
-  { date: '2024-Jan', tokens: 1044475.028 },
-  { date: '2024-Feb', tokens: 810971.5694 },
-  { date: '2024-Mar', tokens: 678487.1944 },
+  { label: 'April 2021', date: '2021-Apr', tokens: 56861.875 },
+  { label: 'May 2021', date: '2021-May', tokens: 344298.5694 },
+  { label: 'June 2021', date: '2021-Jun', tokens: 337154.7361 },
+  { label: 'July 2021', date: '2021-Jul', tokens: 345489.0694 },
+  { label: 'August 2021', date: '2021-Aug', tokens: 341917.1528 },
+  { label: 'September 2021', date: '2021-Sep', tokens: 396050.0694 },
+  { label: 'October 2021', date: '2021-Oct', tokens: 396698.2778 },
+  { label: 'November 2021', date: '2021-Nov', tokens: 393827.0278 },
+  { label: 'December 2021', date: '2021-Dec', tokens: 418542.4028 },
+  { label: 'January 2022', date: '2022-Jan', tokens: 604735.6944 },
+  { label: 'February 2022', date: '2022-Feb', tokens: 1193871.111 },
+  { label: 'March 2022', date: '2022-Mar', tokens: 3029425.195 },
+  { label: 'April 2022', date: '2022-Apr', tokens: 3390454.361 },
+  { label: 'May 2022', date: '2022-May', tokens: 1701706.569 },
+  { label: 'June 2022', date: '2022-Jun', tokens: 1425918.319 },
+  { label: 'July 2022', date: '2022-Jul', tokens: 1650848.736 },
+  { label: 'August 2022', date: '2022-Aug', tokens: 1392180.903 },
+  { label: 'September 2022', date: '2022-Sep', tokens: 1357516.361 },
+  { label: 'October 2022', date: '2022-Oct', tokens: 1372091.903 },
+  { label: 'November 2022', date: '2022-Nov', tokens: 1352185.569 },
+  { label: 'December 2022', date: '2022-Dec', tokens: 1349644.861 },
+  { label: 'January 2023', date: '2023-Jan', tokens: 1355450.778 },
+  { label: 'February 2023', date: '2023-Feb', tokens: 1179245.486 },
+  { label: 'March 2023', date: '2023-Mar', tokens: 1343155.194 },
+  { label: 'April 2023', date: '2023-Apr', tokens: 1316934.569 },
+  { label: 'May 2023', date: '2023-May', tokens: 1310127.653 },
+  { label: 'June 2023', date: '2023-Jun', tokens: 1306387.736 },
+  { label: 'July 2023', date: '2023-Jul', tokens: 1441900.444 },
+  { label: 'August 2023', date: '2023-Aug', tokens: 1311723.153 },
+  { label: 'September 2023', date: '2023-Sep', tokens: 1260604.403 },
+  { label: 'October 2023', date: '2023-Oct', tokens: 1262894.444 },
+  { label: 'November 2023', date: '2023-Nov', tokens: 1249142.444 },
+  { label: 'December 2023', date: '2023-Dec', tokens: 1235097.903 },
+  { label: 'January 2024', date: '2024-Jan', tokens: 1044475.028 },
+  { label: 'February 2024', date: '2024-Feb', tokens: 810971.5694 },
+  { label: 'March 2024', date: '2024-Mar', tokens: 678487.1944 },
 ];
 
 const UnlockSchedule = (): JSX.Element => {
@@ -122,7 +127,6 @@ const UnlockSchedule = (): JSX.Element => {
             width={907}
             height={300}
             barWidth={14.5}
-            labels={({ datum }) => moment(datum.date).format('MMMM YYYY')}
             tickFormatX={(t: string) => {
               if (t && t.includes('Jan')) return t.replace('-Jan', '');
               return '';

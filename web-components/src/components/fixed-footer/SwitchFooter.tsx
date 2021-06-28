ddd3dd09013ab5dbfaa408d7251987a3ece5dca4
Mark Hagelberg <mark.hagelberg@gmail.com>
2021-06-28 16:24:03 -0500

    web component

--- web-components/src/components/fixed-footer/SwitchFooter.tsx
+++ web-components/src/components/fixed-footer/SwitchFooter.tsx
@@ -6,34 +6,130 @@ import { makeStyles } from '@material-ui/styles';
 import FixedFooter from './';
 import ContainedButton from '../buttons/ContainedButton';
 
-interface Props {
+interface SwitchFooterProps {
   buttonText: string;
+  activeOption: string;
+  leftOption: string;
+  rightOption: string;
+  label?: string;
   onCtaClick: () => void;
   onToggleClick: () => void;
 }
 
 const useStyles = makeStyles<Theme>((theme: Theme) => ({
-  root: {},
+  root: {
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  switch: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  label: {
+    marginRight: theme.spacing(2),
+  },
+  toggleContainer: {
+    display: 'flex',
+    width: theme.spacing(82.25),
+    height: theme.spacing(12.5),
+    borderRadius: theme.spacing(12.5),
+    background: theme.palette.info.light,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    textTransform: 'uppercase',
+    fontSize: theme.typography.pxToRem(14),
+    fontFamily: theme.typography.h1.fontFamily,
+  },
+  option: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: theme.spacing(11),
+    minWidth: '35%',
+    padding: theme.spacing(0, 9),
+    borderRadius: theme.spacing(12.5),
+    margin: theme.spacing(0, 0.75),
+    cursor: 'pointer',
+  },
+  active: {
+    background: theme.palette.secondary.dark,
+    animation: `$activate 0.2s`,
+  },
+  inactive: {
+    color: '#3D7ACF',
+    animation: `$deactivate 0.2s`,
+    '&:hover': {
+      background: theme.palette.grey[100],
+    },
+  },
   btn: {
     padding: theme.spacing(2, 4),
-    minWidth: 0,
-    height: '100%',
+    height: theme.spacing(15),
     [theme.breakpoints.up('sm')]: {
-      fontSize: theme.spacing(4),
+      minWidth: theme.spacing(74.75),
+      fontSize: theme.typography.pxToRem(21),
     },
     [theme.breakpoints.down('xs')]: {
-      fontSize: theme.spacing(3),
+      minWidth: theme.spacing(39.75),
+      fontSize: theme.typography.pxToRem(12),
+    },
+  },
+  '@keyframes activate': {
+    '0%': {
+      background: theme.palette.grey[100],
+      color: '#3D7ACF',
+    },
+    '100%': {
+      background: theme.palette.secondary.dark,
+      color: theme.palette.primary.contrastText,
+    },
+  },
+  '@keyframes deactivate': {
+    '0%': {
+      background: theme.palette.secondary.dark,
+      color: theme.palette.primary.contrastText,
+    },
+    '100%': {
+      background: theme.palette.info.light,
+      color: '#3D7ACF',
     },
   },
 }));
 
-const SwitchFooter: React.FC<Props> = ({ buttonText, ...props }) => {
+const SwitchFooter: React.FC<SwitchFooterProps> = ({
+  buttonText,
+  activeOption,
+  leftOption,
+  rightOption,
+  label,
+  onCtaClick,
+  onToggleClick,
+}) => {
   const styles = useStyles();
 
   return (
     <FixedFooter>
       <div className={styles.root}>
-        <ContainedButton className={styles.btn} onClick={props.onCtaClick}>
+        <div className={styles.switch}>
+          {label && <span className={styles.label}>{label}</span>}
+          <div className={styles.toggleContainer}>
+            <div
+              className={clsx(styles.option, activeOption === leftOption ? styles.active : styles.inactive)}
+              onClick={onToggleClick}
+            >
+              {leftOption}
+            </div>
+            <div
+              className={clsx(styles.option, activeOption === rightOption ? styles.active : styles.inactive)}
+              onClick={onToggleClick}
+            >
+              {rightOption}
+            </div>
+          </div>
+        </div>
+        <ContainedButton className={styles.btn} onClick={onCtaClick}>
           {buttonText}
         </ContainedButton>
       </div>
--- web-components/src/components/fixed-footer/footer.stories.tsx
+++ web-components/src/components/fixed-footer/footer.stories.tsx
@@ -1,15 +1,9 @@
 import * as React from 'react';
 import { withKnobs, number, boolean, text } from '@storybook/addon-knobs';
-import clsx from 'clsx';
 
 import FixedFooter from 'web-components/lib/components/fixed-footer';
 import OnboardingFooter from 'web-components/lib/components/fixed-footer/OnboardingFooter';
-
-import ContainedButton from '../buttons/ContainedButton';
-
-import { Theme } from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles';
-import { set } from 'js-cookie';
+import { SwitchFooter } from 'web-components/lib/components/fixed-footer/SwitchFooter';
 
 export default {
   title: 'Components|Footers',
@@ -41,136 +35,6 @@ export const onboardingFooter = (): JSX.Element => (
   />
 );
 
-interface Props {
-  buttonText: string;
-  activeOption: string;
-  leftOption: string;
-  rightOption: string;
-  label?: string;
-  onCtaClick: () => void;
-  onToggleClick: () => void;
-}
-
-const useStyles = makeStyles<Theme>((theme: Theme) => ({
-  root: {
-    display: 'flex',
-    flex: 1,
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  switch: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  label: {
-    marginRight: 8,
-  },
-  toggleContainer: {
-    display: 'flex',
-    width: 329,
-    height: 50,
-    borderRadius: 50,
-    background: theme.palette.info.light,
-    alignItems: 'center',
-    justifyContent: 'space-between',
-
-    textTransform: 'uppercase',
-    fontSize: theme.typography.pxToRem(14),
-    fontFamily: theme.typography.h1.fontFamily,
-  },
-  option: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: 44,
-
-    minWidth: '35%',
-    padding: '0 36px',
-    borderRadius: 50,
-    margin: '0 3px',
-    cursor: 'pointer',
-  },
-  active: {
-    background: theme.palette.secondary.dark,
-
-    animation: `$activate 0.2s`,
-  },
-  inactive: {
-    '&:hover': {
-      background: theme.palette.grey[100],
-    },
-    animation: `$deactivate 0.2s`,
-  },
-  btn: {
-    padding: theme.spacing(2, 4),
-    height: 60,
-    [theme.breakpoints.up('sm')]: {
-      minWidth: 299,
-      fontSize: theme.typography.pxToRem(21),
-    },
-    [theme.breakpoints.down('xs')]: {
-      minWidth: 159,
-      fontSize: theme.typography.pxToRem(12),
-    },
-  },
-
-  '@keyframes activate': {
-    '0%': {
-      background: theme.palette.grey[100],
-    },
-    '100%': {
-      background: theme.palette.secondary.dark,
-    },
-  },
-  '@keyframes deactivate': {
-    '0%': {
-      background: theme.palette.secondary.dark,
-    },
-    '100%': {
-      background: theme.palette.info.light,
-    },
-  },
-}));
-
-const SwitchFooter: React.FC<Props> = ({
-  buttonText,
-  activeOption,
-  leftOption,
-  rightOption,
-  label,
-  onCtaClick,
-  onToggleClick,
-}) => {
-  const styles = useStyles();
-
-  return (
-    <FixedFooter>
-      <div className={styles.root}>
-        <div className={styles.switch}>
-          {label && <span className={styles.label}>{label}</span>}
-          <div className={styles.toggleContainer}>
-            <div
-              className={clsx(styles.option, activeOption === leftOption ? styles.active : styles.inactive)}
-              onClick={onToggleClick}
-            >
-              {leftOption}
-            </div>
-            <div
-              className={clsx(styles.option, activeOption === rightOption ? styles.active : styles.inactive)}
-              onClick={onToggleClick}
-            >
-              {rightOption}
-            </div>
-          </div>
-        </div>
-        <ContainedButton className={styles.btn} onClick={onCtaClick}>
-          {buttonText}
-        </ContainedButton>
-      </div>
-    </FixedFooter>
-  );
-};
-
 const SwitchFooterDemo = (): JSX.Element => {
   const [activeOption, setActiveOption] = React.useState<'Buyer' | 'Land Steward'>('Buyer');
 
@@ -192,8 +56,8 @@ const SwitchFooterDemo = (): JSX.Element => {
 
   return (
     <SwitchFooter
-      buttonText={activeOption === 'Buyer' ? 'buy credits' : 'fill in intake form'}
       activeOption={activeOption}
+      buttonText={activeOption === 'Buyer' ? 'buy credits' : 'fill in intake form'}
       label="I am a:"
       leftOption="Land Steward"
       rightOption="Buyer"

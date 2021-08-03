544a8cd0a4e71b6ee92249bd9bfaaa1596962615
blushi <marie.gauthier63@gmail.com>
2021-08-03 15:30:03 +0200

    Fix format date

--- web-components/src/utils/format.ts
+++ web-components/src/utils/format.ts
@@ -2,7 +2,10 @@ export function getFormattedDate(
   date?: string | Date | null,
   options?: Intl.DateTimeFormatOptions | undefined,
 ): string {
-  return new Intl.DateTimeFormat('en-US', { timeZone: 'UTC', ...options }).format(new Date(date));
+  if (date) {
+    return new Intl.DateTimeFormat('en-US', { timeZone: 'UTC', ...options }).format(new Date(date));
+  }
+  return '';
 }
 
 export function getFormattedNumber(number: number, options?: Intl.NumberFormatOptions | undefined): string {

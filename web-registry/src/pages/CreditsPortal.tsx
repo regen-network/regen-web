a84d2701d3f1104ec9d14d658fa7c2237690ddf2
David Daniel <davedan7@gmail.com>
2021-08-09 17:20:35 -0600

    remove styles, add buyer

--- web-registry/src/pages/CreditsPortal.tsx
+++ web-registry/src/pages/CreditsPortal.tsx
@@ -1,54 +1,17 @@
-import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React from 'react';
+// import { makeStyles } from '@material-ui/core/styles';
 
 import Box from '@material-ui/core/Box';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormControl from '@material-ui/core/FormControl';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 
-import Title from 'web-components/lib/components/title';
-import { useAllProjectsQuery, useIssueCreditsMutation } from '../generated/graphql';
 import { CreditsIssue } from './CreditsIssue';
 import { CreditsTransfer } from './CreditsTransfer';
 import { CreditsRetire } from './CreditsRetire';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    padding: theme.spacing(5),
-  },
-  ownershipTitle: {
-    paddingTop: theme.spacing(5),
-    paddingBottom: theme.spacing(4),
-    fontSize: '1.3rem',
-  },
-  input: {
-    padding: theme.spacing(1),
-  },
-  form: {
-    paddingTop: theme.spacing(5),
-    paddingBottom: theme.spacing(5),
-  },
-  button: {
-    marginTop: theme.spacing(3),
-  },
-  formControl: {
-    margin: theme.spacing(1),
-    minWidth: 120,
-  },
-}));
+import { BuyerCreate } from './BuyerCreate';
 
 const CreditsPortal: React.FC = () => {
   return (
     <Box>
+      <BuyerCreate />
       <CreditsIssue />
       <CreditsTransfer />
       <CreditsRetire />

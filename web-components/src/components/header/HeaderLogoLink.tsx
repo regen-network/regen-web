b524c09bbf78b43e394497660476cc414147521d
David Daniel <davedan7@gmail.com>
2021-09-07 19:21:24 -0600

    Figured out a stop-gap hack for the safari bug

--- web-components/src/components/header/HeaderLogoLink.tsx
+++ web-components/src/components/header/HeaderLogoLink.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useEffect, useState } from 'react';
+import { makeStyles, Theme } from '@material-ui/core/styles';
 import RegenIcon from '../icons/RegenIcon';
 
-const useStyles = makeStyles(theme => {
+const useStyles = makeStyles<Theme, { isLoaded: boolean }>(theme => {
   const { pxToRem } = theme.typography;
+
   return {
-    icon: {
+    icon: props => ({
+      display: props.isLoaded ? 'block' : 'inline-block',
       height: 'auto',
       width: pxToRem(186),
       [theme.breakpoints.down('sm')]: {
@@ -14,12 +16,23 @@ const useStyles = makeStyles(theme => {
       [theme.breakpoints.down('xs')]: {
         width: pxToRem(104),
       },
-    },
+    }),
   };
 });
 
 export const HeaderLogoLink: React.FC<{ color: string }> = ({ color }) => {
-  const styles = useStyles();
+  const [isLoaded, setIsLoaded] = useState(false);
+  const styles = useStyles({ isLoaded });
+  const location = window.location.href;
+
+  // TODO: this is a hack to make the SVG render properly in safari - it's not a
+  // perfect solution but should work until we can dedicate time to find
+  // something more long-term. You can see the bug by visiting the /community
+  // page in safari
+  useEffect(() => {
+    setIsLoaded(!isLoaded);
+  }, [location]); // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
     <a href="/">
       <RegenIcon className={styles.icon} color={color} />

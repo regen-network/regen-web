da555fe7a30058a9022781eb303740112fc57e88
Mark Hagelberg <mark.hagelberg@gmail.com>
2021-09-08 15:37:19 -0600

    check for window in useEffect:

--- web-components/src/components/header/HeaderLogoLink.tsx
+++ web-components/src/components/header/HeaderLogoLink.tsx
@@ -23,15 +23,14 @@ const useStyles = makeStyles<Theme, { isLoaded: boolean }>(theme => {
 export const HeaderLogoLink: React.FC<{ color: string }> = ({ color }) => {
   const [isLoaded, setIsLoaded] = useState(false);
   const styles = useStyles({ isLoaded });
-  const location = typeof window !== undefined && window.location && window.location.href;
 
   // TODO: this is a hack to make the SVG render properly in safari - it's not a
   // perfect solution but should work until we can dedicate time to find
   // something more long-term. You can see the bug by visiting the /community
   // page in safari
   useEffect(() => {
-    setIsLoaded(!isLoaded);
-  }, [location]); // eslint-disable-line react-hooks/exhaustive-deps
+    if (typeof window !== undefined && window.location && window.location.href) setIsLoaded(!isLoaded);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <a href="/">

6e873131a5f3bb46737afa70ddf3bb7be6f5f8c2
Mark Hagelberg <mark.hagelberg@gmail.com>
2021-10-05 13:48:13 -0600

    env vars

--- web-registry/src/components/atoms/WalletButton.tsx
+++ web-registry/src/components/atoms/WalletButton.tsx
@@ -40,109 +40,21 @@ const useStyles = makeStyles(theme => ({
 const WalletButton: React.FC = () => {
   const styles = useStyles();
   const theme = useTheme();
-  const { wallet, getWallet } = useWallet();
+  const { wallet, suggestChain } = useWallet();
   const [showAlert, setShowAlert] = useState(false);
 
-  const connectToKeplr = async (): Promise<any> => {
-    if (window.keplr) {
-      window.keplr
-        .experimentalSuggestChain({
-          // Chain-id of the Regen chain.
-          chainId,
-          // The name of the chain to be displayed to the user.
-          chainName: 'Regen - Hambach Testnet',
-          // RPC endpoint of the chain.
-          rpc: 'http://137.184.24.185:26657/',
-          // REST endpoint of the chain.
-          rest: 'http://137.184.24.185:1337/',
-          // Staking coin information
-          stakeCurrency: {
-            // Coin denomination to be displayed to the user.
-            coinDenom: 'REGEN',
-            // Actual denom (i.e. uatom, uscrt) used by the blockchain.
-            coinMinimalDenom: 'uregen',
-            // # of decimal points to convert minimal denomination to user-facing denomination.
-            coinDecimals: 6,
-            // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
-            // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
-            // coinGeckoId: ""
-          },
-          // (Optional) If you have a wallet webpage used to stake the coin then provide the url to the website in `walletUrlForStaking`.
-          // The 'stake' button in Keplr extension will link to the webpage.
-          // walletUrlForStaking: "",
-          // The BIP44 path.
-          bip44: {
-            // You can only set the coin type of BIP44.
-            // 'Purpose' is fixed to 44.
-            coinType: 118,
-          },
-          // Bech32 configuration to show the address to user.
-          bech32Config: {
-            bech32PrefixAccAddr: 'regen',
-            bech32PrefixAccPub: 'regenpub',
-            bech32PrefixValAddr: 'regenvaloper',
-            bech32PrefixValPub: 'regenvaloperpub',
-            bech32PrefixConsAddr: 'regenvalcons',
-            bech32PrefixConsPub: 'regenvalconspub',
-          },
-          // List of all coin/tokens used in this chain.
-          currencies: [
-            {
-              // Coin denomination to be displayed to the user.
-              coinDenom: 'REGEN',
-              // Actual denom (i.e. uatom, uscrt) used by the blockchain.
-              coinMinimalDenom: 'uregen',
-              // # of decimal points to convert minimal denomination to user-facing denomination.
-              coinDecimals: 6,
-              // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
-              // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
-              // coinGeckoId: ""
-            },
-          ],
-          // List of coin/tokens used as a fee token in this chain.
-          feeCurrencies: [
-            {
-              // Coin denomination to be displayed to the user.
-              coinDenom: 'REGEN',
-              // Actual denom (i.e. uatom, uscrt) used by the blockchain.
-              coinMinimalDenom: 'uregen',
-              // # of decimal points to convert minimal denomination to user-facing denomination.
-              coinDecimals: 6,
-              // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
-              // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
-              // coinGeckoId: ""
-            },
-          ],
-          // (Optional) The number of the coin type.
-          // This field is only used to fetch the address from ENS.
-          // Ideally, it is recommended to be the same with BIP44 path's coin type.
-          // However, some early chains may choose to use the Cosmos Hub BIP44 path of '118'.
-          // So, this is separated to support such chains.
-          coinType: 118,
-          // (Optional) This is used to set the fee of the transaction.
-          // If this field is not provided, Keplr extension will set the default gas price as (low: 0.01, average: 0.025, high: 0.04).
-          // Currently, Keplr doesn't support dynamic calculation of the gas prices based on on-chain data.
-          // Make sure that the gas prices are higher than the minimum gas prices accepted by chain validators and RPC/REST endpoint.
-          gasPriceStep: {
-            low: 0.01,
-            average: 0.025,
-            high: 0.04,
-          },
-          // features: ['stargate'],
-        })
-        .then(() => {
-          getWallet && getWallet();
-        })
-        .catch(() => {
-          // setWallet('');
-        });
+  const connectToKeplr = async (): Promise<void> => {
+    if (chainId && suggestChain) {
+      await suggestChain();
     } else {
       setShowAlert(true);
       setTimeout(() => setShowAlert(false), 5000);
     }
   };
 
-  return (
+  console.log('wallet', wallet);
+
+  return chainId ? (
     <div className={styles.root}>
       <div className={styles.walletButtonWrapper}>
         <IconButton className={styles.walletButton} onClick={connectToKeplr}>
@@ -152,6 +64,8 @@ const WalletButton: React.FC = () => {
       <span className={styles.walletAddress}>{wallet?.shortAddress}</span>
       {showAlert && <ErrorBanner text="Please install Keplr extension to use Regen Ledger features" />}
     </div>
+  ) : (
+    <></>
   );
 };
 
--- web-registry/src/wallet.tsx
+++ web-registry/src/wallet.tsx
@@ -15,54 +15,158 @@ interface ChainKey {
   isNanoLedger: boolean;
 }
 
-// interface OfflineSigner {
-//   getAccounts: () => Promise<any>;
-// }
+interface KeplrWallet {
+  address: string;
+  shortAddress: string;
+}
 
 declare global {
   interface Window {
     keplr?: Keplr;
-    // getOfflineSigner: (chainId: string) => OfflineSigner;
   }
 }
 
 type ContextType = {
   wallet?: any;
-  getWallet?: () => Promise<void>;
+  suggestChain?: () => Promise<void>;
 };
 
-const wallet = {};
-const WalletContext = createContext<ContextType>({ wallet });
-export const chainId = 'regen-hambach-1'; //TODO env
+const WalletContext = createContext<ContextType>({});
+
+export const chainId = process.env.REACT_APP_LEDGER_CHAIN_ID;
+const chainName = process.env.REACT_APP_LEDGER_CHAIN_NAME;
+const chainRpc = process.env.REACT_APP_LEDGER_RPC;
+const chainRestEndpoint = process.env.REACT_APP_LEDGER_REST_ENDPOINT;
 
 export const WalletProvider: React.FC = ({ children }) => {
-  const [wallet, setWallet] = useState<any>();
+  const [wallet, setWallet] = useState<KeplrWallet | null>();
 
   window.onload = async () => {
-    await getWallet();
+    if (!wallet) {
+      await getWallet();
+    }
   };
 
   const getWallet = async (): Promise<void> => {
-    if (!wallet) {
-      const shortAddress = await checkForWallet();
-      setWallet({ shortAddress });
+    const key = await checkForWallet();
+    console.log('key', key);
+
+    if (key && key.bech32Address) {
+      const keplrWallet = {
+        address: key.bech32Address,
+        shortAddress: `${key.bech32Address.substring(0, 10)}...`,
+      };
+      setWallet(keplrWallet);
     }
   };
 
-  const checkForWallet = async (): Promise<string | undefined> => {
-    if (window.keplr) {
+  const checkForWallet = async (): Promise<ChainKey | null> => {
+    if (chainId && window.keplr) {
       // Enabling before using the Keplr is recommended.
       // This method will ask the user whether or not to allow access if they haven't visited this website.
       // Also, it will request user to unlock the wallet if the wallet is locked.
-      const key = await window.keplr.getKey(chainId);
-      if (key && key.bech32Address) {
-        return `${key.bech32Address.substring(0, 10)}...`;
-      }
+      return window.keplr.getKey(chainId);
     }
-    return undefined;
+    return null;
   };
 
-  return <WalletContext.Provider value={{ wallet, getWallet }}>{children}</WalletContext.Provider>;
+  const suggestChain = async (): Promise<void> => {
+    if (window.keplr) {
+      return window.keplr
+        .experimentalSuggestChain({
+          // Chain-id of the Regen chain.
+          chainId,
+          // The name of the chain to be displayed to the user.
+          chainName,
+          // RPC endpoint of the chain.
+          rpc: chainRpc,
+          // REST endpoint of the chain.
+          rest: chainRestEndpoint,
+          // Staking coin information
+          stakeCurrency: {
+            // Coin denomination to be displayed to the user.
+            coinDenom: 'REGEN',
+            // Actual denom (i.e. uatom, uscrt) used by the blockchain.
+            coinMinimalDenom: 'uregen',
+            // # of decimal points to convert minimal denomination to user-facing denomination.
+            coinDecimals: 6,
+            // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
+            // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
+            // coinGeckoId: ""
+          },
+          // (Optional) If you have a wallet webpage used to stake the coin then provide the url to the website in `walletUrlForStaking`.
+          // The 'stake' button in Keplr extension will link to the webpage.
+          // walletUrlForStaking: "",
+          // The BIP44 path.
+          bip44: {
+            // You can only set the coin type of BIP44.
+            // 'Purpose' is fixed to 44.
+            coinType: 118,
+          },
+          // Bech32 configuration to show the address to user.
+          bech32Config: {
+            bech32PrefixAccAddr: 'regen',
+            bech32PrefixAccPub: 'regenpub',
+            bech32PrefixValAddr: 'regenvaloper',
+            bech32PrefixValPub: 'regenvaloperpub',
+            bech32PrefixConsAddr: 'regenvalcons',
+            bech32PrefixConsPub: 'regenvalconspub',
+          },
+          // List of all coin/tokens used in this chain.
+          currencies: [
+            {
+              // Coin denomination to be displayed to the user.
+              coinDenom: 'REGEN',
+              // Actual denom (i.e. uatom, uscrt) used by the blockchain.
+              coinMinimalDenom: 'uregen',
+              // # of decimal points to convert minimal denomination to user-facing denomination.
+              coinDecimals: 6,
+              // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
+              // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
+              // coinGeckoId: ""
+            },
+          ],
+          // List of coin/tokens used as a fee token in this chain.
+          feeCurrencies: [
+            {
+              // Coin denomination to be displayed to the user.
+              coinDenom: 'REGEN',
+              // Actual denom (i.e. uatom, uscrt) used by the blockchain.
+              coinMinimalDenom: 'uregen',
+              // # of decimal points to convert minimal denomination to user-facing denomination.
+              coinDecimals: 6,
+              // (Optional) Keplr can show the fiat value of the coin if a coingecko id is provided.
+              // You can get id from https://api.coingecko.com/api/v3/coins/list if it is listed.
+              // coinGeckoId: ""
+            },
+          ],
+          // (Optional) The number of the coin type.
+          // This field is only used to fetch the address from ENS.
+          // Ideally, it is recommended to be the same with BIP44 path's coin type.
+          // However, some early chains may choose to use the Cosmos Hub BIP44 path of '118'.
+          // So, this is separated to support such chains.
+          coinType: 118,
+          // (Optional) This is used to set the fee of the transaction.
+          // If this field is not provided, Keplr extension will set the default gas price as (low: 0.01, average: 0.025, high: 0.04).
+          // Currently, Keplr doesn't support dynamic calculation of the gas prices based on on-chain data.
+          // Make sure that the gas prices are higher than the minimum gas prices accepted by chain validators and RPC/REST endpoint.
+          gasPriceStep: {
+            low: 0.01,
+            average: 0.025,
+            high: 0.04,
+          },
+          features: ['stargate'],
+        })
+        .then(() => {
+          getWallet();
+        })
+        .catch(() => {
+          setWallet(null);
+        });
+    }
+  };
+
+  return <WalletContext.Provider value={{ wallet, suggestChain }}>{children}</WalletContext.Provider>;
 };
 
 export const useWallet = (): ContextType => React.useContext(WalletContext);

e363b694e6a4ab8fa3fe165bac667b7b411f1801
David Daniel <davedan7@gmail.com>
2021-10-05 22:35:03 -0600

    update config text

--- web-www/sanity.ts
+++ web-www/sanity.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-// Alternative Apollo client for querying data from Sanity (CMS)
+// client for querying data from Sanity (CMS)
 const client = new ApolloClient({
   uri: `https://${process.env.SANITY_PROJECT_ID}.api.sanity.io/v1/graphql/${process.env.SANITY_DATASET}/default`,
   cache: new InMemoryCache(),

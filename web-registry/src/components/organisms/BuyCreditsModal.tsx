a25cad48722f75636dc5b4aa9fad117c508f76c0
Mark Hagelberg <mark.hagelberg@gmail.com>
2021-10-07 10:59:33 -0600

    form in modal

--- web-registry/src/components/organisms/BuyCreditsModal.tsx
+++ web-registry/src/components/organisms/BuyCreditsModal.tsx
@@ -1,11 +1,19 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { Formik, Form, Field } from 'formik';
 
 import Modal, { RegenModalProps } from 'web-components/lib/components/modal';
-import { BuyCreditsForm, BuyCreditsValues } from './BuyCreditsForm';
+import FormLabel from 'web-components/lib/components/inputs/FormLabel';
+import RegenTextField from 'web-components/lib/components/inputs/TextField';
+import ContainedButton from 'web-components/lib/components/buttons/ContainedButton';
 
 interface BuyCreditsModalProps extends RegenModalProps {
   onClose: () => void;
+  initialValues?: BuyCreditsValues;
+}
+
+export interface BuyCreditsValues {
+  'http://regen.network/test': string;
 }
 
 const useStyles = makeStyles(theme => ({
@@ -27,9 +35,25 @@ const useStyles = makeStyles(theme => ({
     flexDirection: 'column',
     justifyContent: 'space-between',
   },
+
+  description: {
+    marginBottom: 0,
+    fontSize: theme.typography.pxToRem(16),
+  },
+  field: {
+    [theme.breakpoints.up('sm')]: {
+      marginBottom: theme.spacing(12),
+    },
+    [theme.breakpoints.down('xs')]: {
+      marginBottom: theme.spacing(10),
+    },
+  },
+  error: {
+    marginTop: 0,
+  },
 }));
 
-const BuyCreditsModal: React.FC<BuyCreditsModalProps> = ({ open, onClose }) => {
+const BuyCreditsModal: React.FC<BuyCreditsModalProps> = ({ open, onClose, initialValues }) => {
   const styles = useStyles();
 
   const submit = async (values: BuyCreditsValues): Promise<void> => {
@@ -38,7 +62,41 @@ const BuyCreditsModal: React.FC<BuyCreditsModalProps> = ({ open, onClose }) => {
 
   return (
     <Modal open={open} onClose={onClose}>
-      <BuyCreditsForm submit={submit} />
+      <Formik
+        enableReinitialize
+        validateOnMount
+        initialValues={
+          initialValues || {
+            'http://regen.network/test': initialValues?.['http://regen.network/test'] || '',
+          }
+        }
+        onSubmit={async (values, { setSubmitting }) => {
+          setSubmitting(true);
+          try {
+            await submit(values);
+            setSubmitting(false);
+          } catch (e) {
+            setSubmitting(false);
+          }
+        }}
+      >
+        {({ submitForm, isValid, isSubmitting }) => {
+          return (
+            <>
+              <Form translate="yes">
+                <Field
+                  className={styles.field}
+                  component={RegenTextField}
+                  label="test"
+                  description="desc"
+                  name="['http://regen.network/test']"
+                />
+              </Form>
+              <ContainedButton onClick={submitForm}>submeezy</ContainedButton>
+            </>
+          );
+        }}
+      </Formik>
     </Modal>
   );
 };

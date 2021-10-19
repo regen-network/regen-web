d1b09ee7bf09efb8f10ed529b9ec7839c3ec83e7
Mark Hagelberg <mark.hagelberg@gmail.com>
2021-10-19 15:04:46 -0600

    spinner

--- web-registry/src/components/molecules/ProcessingModal.tsx
+++ web-registry/src/components/molecules/ProcessingModal.tsx
@@ -5,6 +5,8 @@ import Description from 'web-components/lib/components/description';
 import Title from 'web-components/lib/components/title';
 import Modal, { RegenModalProps } from 'web-components/lib/components/modal';
 
+import spinner from './spinner.svg';
+
 interface Props extends RegenModalProps {
   pendingTx?: string;
 }
@@ -13,12 +15,17 @@ const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
+    alignItems: 'center',
     [theme.breakpoints.up('md')]: {
       height: 458, //todo
     },
     [theme.breakpoints.down('xs')]: {
       padding: 0,
     },
+  },
+  spinner: {
+    marginTop: 19,
+    marginBottom: theme.spacing(4),
   },
 }));
 
@@ -27,7 +34,8 @@ const ProcessingModal: React.FC<Props> = ({ open, onClose, pendingTx }) => {
 
   return (
     <Modal className={styles.root} open={open} onClose={onClose}>
-      Pending Tx: {pendingTx}
+      <img className={styles.spinner} src={spinner} height={48} width={48} alt="processing" />
+      <span>Pending Tx: {pendingTx}</span>
     </Modal>
   );
 };

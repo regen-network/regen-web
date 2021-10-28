15d8f6387872fedd6424b1f81142a29b15835951
Mark Hagelberg <mark.hagelberg@gmail.com>
2021-10-27 22:27:08 -0600

    simplify foldsection

--- web-www/src/sections/fund/FoldSection.tsx
+++ web-www/src/sections/fund/FoldSection.tsx
@@ -1,34 +1,8 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
-import { Theme, makeStyles } from '@material-ui/core';
-import clsx from 'clsx';
-import ReactHtmlParser from 'react-html-parser';
 
 import Section from 'web-components/src/components/section';
-import { MarketingDescription as Description } from '../../components/Description';
-
-const useStyles = makeStyles<Theme>((theme: Theme) => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    '& p': {
-      textAlign: 'center',
-    },
-  },
-  content: {
-    width: '80%',
-    maxWidth: theme.spacing(236.5),
-    [theme.breakpoints.down('xs')]: {
-      width: '100%',
-    },
-  },
-  title: {
-    [theme.breakpoints.down('xs')]: {
-      fontSize: theme.typography.pxToRem(32),
-    },
-  },
-}));
+import TitleDescription from 'web-components/src/components/title-description';
 
 type QueryData = {
   text: {
@@ -40,8 +14,6 @@ type QueryData = {
 };
 
 const FoldSection = (): JSX.Element => {
-  const styles = useStyles();
-
   const {
     text: {
       foldSection: { title, body },
@@ -58,8 +30,8 @@ const FoldSection = (): JSX.Element => {
   `);
 
   return (
-    <Section title={title} classes={{ root: clsx(styles.root, styles.center), title: styles.title }}>
-      <Description className={clsx(styles.content, styles.center)}>{ReactHtmlParser(body)}</Description>
+    <Section>
+      <TitleDescription title={title} description={body}></TitleDescription>
     </Section>
   );
 };

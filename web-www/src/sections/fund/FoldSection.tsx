6ce103c49d85e9f0f1b51f1ee08b8b9bcad61bc6
Mark Hagelberg <9438198+mhagel@users.noreply.github.com>
2021-10-29 22:24:42 -0600

    Ecosystem fund page (#789)
    * fun page topSection and fold
    
    * fund under Get Involved
    
    * simplify foldsection
    
    * cta section

---
+++ web-www/src/sections/fund/FoldSection.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+
+import Section from 'web-components/src/components/section';
+import TitleDescription from 'web-components/src/components/title-description';
+
+type QueryData = {
+  text: {
+    foldSection: {
+      title: string;
+      body: string;
+    };
+  };
+};
+
+const FoldSection = (): JSX.Element => {
+  const {
+    text: {
+      foldSection: { title, body },
+    },
+  } = useStaticQuery<QueryData>(graphql`
+    query {
+      text: fundYaml {
+        foldSection {
+          title
+          body
+        }
+      }
+    }
+  `);
+
+  return (
+    <Section>
+      <TitleDescription title={title} description={body} />
+    </Section>
+  );
+};
+
+export default FoldSection;

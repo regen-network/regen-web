6e60c474b33fe3a62c3d7abfb5414e20cdf87a82
victordelval <victor@regen.network>
2022-04-07 16:05:53 +0200

    Add logic to use getMetadata related to credit classes, but disabled for now.

--- web-registry/src/hooks/useBasketDetails.ts
+++ web-registry/src/hooks/useBasketDetails.ts
@@ -9,10 +9,17 @@ import useQueryListBatchInfo from './useQueryListBatchInfo';
 import { useProjectByBatchDenomLazyQuery } from '../generated/graphql';
 // ui types
 import { BasketOverviewProps, CreditBatch } from '../components/organisms';
+// import { getMetadata } from '../lib/metadata-graph';
+import { QueryClassInfoResponse } from '@regen-network/api/lib/generated/regen/ecocredit/v1alpha1/query';
 
 type BasketDetails = {
   overview: BasketOverviewProps;
   creditBatches: CreditBatch[];
+};
+
+type ClassInfo = {
+  id: string;
+  name: string;
 };
 
 type BatchWithProject = {
@@ -21,12 +28,32 @@ type BatchWithProject = {
   projectDisplay: string;
 };
 
+const overviewInitial = {
+  name: '-',
+  displayDenom: '-',
+  description: '-',
+  totalAmount: 0,
+  curator: '-',
+  allowedCreditClasses: [{ id: '-', name: '-' }],
+  minStartDate: '-',
+  // startDateWindow?: Long;
+  // startDateWindow?: string;
+};
+
 const useBasketDetails = (basketDenom?: string): BasketDetails => {
+  // custom basket overview data for <BasketOverview />
+  const [overview, setOverview] =
+    useState<BasketOverviewProps>(overviewInitial);
+  // custom ecocredit batches data for <BasketEcocreditsTable />
+  const [creditBatches, setCreditBatches] = useState<CreditBatch[]>([]);
+
+  // fetching necessary data
   const basket = useQueryBasket(basketDenom);
   const basketBalances = useQueryBasketBalances(basketDenom);
   const basketMetadata = useQueryDenomMetadata(basketDenom);
 
-  const basketClasses = useQueryListClassInfo(basket?.classes);
+  const basketClassesInfo = useQueryListClassInfo(basket?.classes);
+  const [basketClasses, setBasketClasses] = useState<ClassInfo[]>();
 
   const [batches, setBatches] = useState<string[]>();
   const basketBatches = useQueryListBatchInfo(batches);
@@ -34,27 +61,44 @@ const useBasketDetails = (basketDenom?: string): BasketDetails => {
   const [fetchProjectByBatchDenom] = useProjectByBatchDenomLazyQuery();
   const [batchesProjects, setBatchesProjects] = useState<BatchWithProject[]>();
 
-  // custom basket overview data for <BasketOverview />
-  const [overview, setOverview] = useState<BasketOverviewProps>({
-    name: '-',
-    displayDenom: '-',
-    description: '-',
-    totalAmount: 0,
-    curator: '-',
-    allowedCreditClasses: [{ id: '-', name: '-' }],
-    minStartDate: '-',
-    // startDateWindow?: Long;
-    // startDateWindow?: string;
-  });
-  // custom ecocredit batches data for <BasketEcocreditsTable />
-  const [creditBatches, setCreditBatches] = useState<CreditBatch[]>([]);
-
   // Batches string list query param for `useQueryListBatchInfo(batches)`
   useEffect(() => {
     if (!basketBalances?.balances) return;
     setBatches(basketBalances.balances.map(balance => balance.batchDenom));
   }, [basketBalances?.balances]);
 
+  // TODO ? overview data: extract into its own hook / function ?
+  // fetch credit classes metadata
+  useEffect(() => {
+    if (!basketClassesInfo || basketClassesInfo.length === 0) return;
+
+    async function fetchData(
+      basketClassesInfo: QueryClassInfoResponse[],
+    ): Promise<void> {
+      try {
+        const _basketClasses = await Promise.all(
+          basketClassesInfo.map(async basketClass => {
+            // const metadata = await getMetadata(basketClass.info!.metadata);
+            // eslint-disable-next-line no-console
+            // console.log('***** metadata', metadata);
+            return {
+              id: basketClass.info!.classId,
+              name: basketClass.info!.classId,
+            };
+          }),
+        );
+        setBasketClasses(_basketClasses);
+      } catch (error) {
+        // TODO
+        // eslint-disable-next-line no-console
+        console.error(error);
+      }
+    }
+
+    fetchData(basketClassesInfo);
+  }, [basketClassesInfo]);
+
+  // TODO ? creditaBatches data >> extract into its own hook / function ?
   // fetch project data related to credit batch using graphql lazy hook
   useEffect(() => {
     if (!batches) return;
@@ -117,6 +161,7 @@ const useBasketDetails = (basketDenom?: string): BasketDetails => {
   }, [batches, basketBatches, batchesProjects]);
 
   // finally, data preparation for <BasketOverview />
+  // TODO ? split state update into different useEffect based on data source ?
   useEffect(() => {
     if (!basket || !basketMetadata || !basketBalances || !basketClasses) return;
 
@@ -124,10 +169,6 @@ const useBasketDetails = (basketDenom?: string): BasketDetails => {
       (acc, obj) => acc + parseFloat(obj.balance),
       0,
     );
-    const allowedCreditClasses = basketClasses?.map(basketClass => ({
-      id: basketClass.info?.classId || '-',
-      name: basketClass?.info?.classId || '-',
-    }));
 
     setOverview({
       name: basket?.basket?.name || '-',
@@ -135,7 +176,7 @@ const useBasketDetails = (basketDenom?: string): BasketDetails => {
       description: basketMetadata?.metadata?.description || '-',
       totalAmount: totalAmount || 0,
       curator: 'Regen Network Development, Inc.' || '-', // TODO
-      allowedCreditClasses: allowedCreditClasses || [{ id: '-', name: '-' }],
+      allowedCreditClasses: basketClasses, // || [{ id: '-', name: '-' }],
       minStartDate: '-',
       // startDateWindow: '-',
     });

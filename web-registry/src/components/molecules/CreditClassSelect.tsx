78bc783ac5044d07fb4a8db0c154ac93dd5272b6
victordelval <victor@regen.network>
2022-06-07 16:17:40 +0200

    Generalize credit class select and metadata json field

--- web-registry/src/components/molecules/CreditClassSelect.tsx
+++ web-registry/src/components/molecules/CreditClassSelect.tsx
@@ -9,7 +9,6 @@ import { blocksToText } from 'web-components/lib/components/block-content';
 import { client } from '../../sanity';
 import useQueryListClasses from '../../hooks/useQueryListClasses';
 import { useAllCreditClassQuery } from '../../generated/sanity-graphql';
-import { BatchBasicsFormValues } from '../organisms/BatchBasicsForm';
 
 interface Props {
   name?: string;
@@ -18,8 +17,11 @@ interface Props {
 
 const defaultCCOption = { value: '', label: 'Choose Credit Class' };
 
-const CreditClassSelect: React.FC<Props> = ({ name = 'classId', required }) => {
-  const { setFieldValue } = useFormikContext<BatchBasicsFormValues>();
+export function CreditClassSelect<T>({
+  name = 'classId',
+  required,
+}: Props): React.ReactElement {
+  const { setFieldValue } = useFormikContext<T>();
   const [creditClassOptions, setCreditClassOptions] = useState<Option[]>();
   const onChainClasses = useQueryListClasses();
   const { data: creditClassData } = useAllCreditClassQuery({ client });
@@ -52,11 +54,9 @@ const CreditClassSelect: React.FC<Props> = ({ name = 'classId', required }) => {
     <Field
       name={name}
       label="Credit Class"
-      required
+      required={required}
       component={SelectTextField}
       options={creditClassOptions}
     />
   );
-};
-
-export { CreditClassSelect };
+}
--- web-registry/src/components/molecules/MetadataJSONField.tsx
+++ web-registry/src/components/molecules/MetadataJSONField.tsx
@@ -1,27 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import { Field, useFormikContext } from 'formik';
+import { Field, FormikContextType, useFormikContext } from 'formik';
 
 import { Body } from 'web-components/lib/components/typography';
 import InputLabel from 'web-components/lib/components/inputs/InputLabel';
 import ControlledTextField from 'web-components/lib/components/inputs/ControlledTextField';
-
-import { BatchBasicsFormValues } from '../organisms/BatchBasicsForm';
+import { Box } from '@mui/material';
 
 interface FieldProps {
   name?: string;
   required?: boolean;
 }
 
-const MetadataJSONField: React.FC<FieldProps> = ({
+export function MetadataJSONField<T extends object>({
   name = 'metadata',
   required,
   ...props
-}) => {
-  const {
-    setFieldValue,
-    setFieldTouched,
-    values: { classId, metadata },
-  } = useFormikContext<BatchBasicsFormValues>();
+}: FieldProps): JSX.Element {
+  const { setFieldValue, setFieldTouched, values } = useFormikContext<T>();
+  const { classId, metadata }: any = values; // TODO: T
   const [formikName, setFormikName] = useState('loading');
 
   useEffect(() => {
@@ -36,24 +32,26 @@ const MetadataJSONField: React.FC<FieldProps> = ({
     }
   }, [classId, metadata, setFieldValue, name, setFieldTouched]);
 
-  return formikName === name && typeof metadata === 'string' ? (
-    <>
-      <InputLabel>Metadata</InputLabel>
-      <Body size="sm" mt={1} mb={3}>
-        Attach arbitrary JSON-LD metadata to the credit batch below.{' '}
-        <a href="#TODO">Learn more»</a>
-      </Body>
-      <Field
-        {...props}
-        name={formikName}
-        component={ControlledTextField}
-        multiline
-        minRows={6}
-        maxRows={18}
-        defaultStyle={false}
-      />
-    </>
-  ) : null;
-};
-
-export { MetadataJSONField };
+  return (
+    <Box sx={{ mt: 10 }}>
+      {formikName === name && typeof metadata === 'string' ? (
+        <>
+          <InputLabel>Metadata</InputLabel>
+          <Body size="sm" mt={1} mb={3}>
+            Attach arbitrary JSON-LD metadata to the credit batch below.{' '}
+            <a href="#TODO">Learn more»</a>
+          </Body>
+          <Field
+            {...props}
+            name={formikName}
+            component={ControlledTextField}
+            multiline
+            minRows={6}
+            maxRows={18}
+            defaultStyle={false}
+          />
+        </>
+      ) : null}
+    </Box>
+  );
+}

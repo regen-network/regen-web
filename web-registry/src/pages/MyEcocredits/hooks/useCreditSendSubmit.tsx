adb1acfea84eba0c631d7bf4af920aa3336a5ab3
Florent <florent.lagrede@gmail.com>
2022-06-20 16:09:37 +0200

    refactor: split callbacks into hooks
    (cherry picked from commit 6abf11e7b1067625ae8b8c98ab8c8483b05a9d01)
    
    # Conflicts:
    #	web-registry/src/pages/MyEcocredits/MyEcocredits.tsx

---
+++ web-registry/src/pages/MyEcocredits/hooks/useCreditSendSubmit.tsx
@@ -0,0 +1,91 @@
+import { RegenApi } from '@regen-network/api/lib';
+import { MsgSend } from '@regen-network/api/lib/generated/regen/ecocredit/v1alpha1/tx';
+import { useCallback } from 'react';
+import { FormValues as CreditSendFormValues } from 'web-components/lib/components/form/CreditSendForm';
+import { Item } from 'web-components/lib/components/modal/TxModal';
+import { SignAndBroadcastType } from '../../../hooks/useMsgClient';
+import { BatchInfoWithBalance } from '../../../types/ledger/ecocredit';
+import { useStateSetter } from '../../../types/react/use-state';
+
+type Props = {
+  api?: RegenApi;
+  accountAddress?: string;
+  credits: BatchInfoWithBalance[];
+  creditSendOpen: number;
+  creditSendTitle: string;
+  signAndBroadcast: SignAndBroadcastType;
+  setCreditSendOpen: useStateSetter<number>;
+  setCardItems: useStateSetter<Item[] | undefined>;
+  setTxModalTitle: useStateSetter<string | undefined>;
+};
+
+type ReturnType = (values: CreditSendFormValues) => Promise<void>;
+
+const useCreditSendSubmit = ({
+  api,
+  accountAddress,
+  credits,
+  creditSendOpen,
+  creditSendTitle,
+  signAndBroadcast,
+  setCreditSendOpen,
+  setCardItems,
+  setTxModalTitle,
+}: Props): ReturnType => {
+  const creditSendSubmit = useCallback(
+    async (values: CreditSendFormValues): Promise<void> => {
+      if (!api?.msgClient?.broadcast || !accountAddress)
+        return Promise.reject();
+      const batchDenom = credits[creditSendOpen].batch_denom;
+      const recipient = values.recipient;
+      const msg = MsgSend.fromPartial({
+        sender: accountAddress,
+        recipient,
+        credits: [
+          {
+            batchDenom,
+            tradableAmount: values.tradableAmount.toString(),
+            retiredAmount: values.retiredAmount.toString(),
+            retirementLocation: values.retirementLocation,
+          },
+        ],
+      });
+
+      const tx = {
+        msgs: [msg],
+        fee: undefined,
+        memo: values?.note,
+      };
+
+      await signAndBroadcast(tx, () => setCreditSendOpen(-1));
+      if (batchDenom && recipient) {
+        setCardItems([
+          {
+            label: 'batch denom',
+            value: { name: batchDenom, url: `/credit-batches/${batchDenom}` },
+          },
+          {
+            label: 'recipient',
+            value: { name: recipient },
+          },
+        ]);
+        setTxModalTitle(creditSendTitle);
+      }
+    },
+    [
+      accountAddress,
+      api?.msgClient?.broadcast,
+      creditSendOpen,
+      creditSendTitle,
+      credits,
+      setCardItems,
+      setCreditSendOpen,
+      setTxModalTitle,
+      signAndBroadcast,
+    ],
+  );
+
+  return creditSendSubmit;
+};
+
+export default useCreditSendSubmit;

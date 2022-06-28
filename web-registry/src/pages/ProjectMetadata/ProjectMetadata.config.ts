777544cabd734f35889342a3e5dffd2e803e6a84
Florent <florent.lagrede@gmail.com>
2022-06-28 16:25:05 +0200

    review: add more omitted keys

--- web-registry/src/pages/ProjectMetadata/ProjectMetadata.config.ts
+++ web-registry/src/pages/ProjectMetadata/ProjectMetadata.config.ts
@@ -2,4 +2,10 @@ export const OMITTED_METADATA_KEYS = [
   'schema:name',
   'regen:projectSize',
   'schema:location',
+  'regen:projectDeveloper',
+  'schema:description',
+  'regen:previewPhoto',
+  'regen:galleryPhotos',
+  'schema:creditText',
+  'regen:videoURL',
 ];

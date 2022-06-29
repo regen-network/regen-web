dd945c0459c6cbe677c9a57e0a9bf21a143c4782
Florent <florent.lagrede@gmail.com>
2022-06-29 15:37:30 +0200

    chore: remove react unused eslint rule

--- web-registry/.eslintrc.js
+++ web-registry/.eslintrc.js
@@ -3,4 +3,7 @@ const baseConfig = require('../.eslintrc');
 module.exports = {
   ...baseConfig,
   ignorePatterns: ['src/generated/*.tsx'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
 };

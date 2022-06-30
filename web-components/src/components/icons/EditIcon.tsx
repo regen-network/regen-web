e4325a8dd2aedfc7917826747bbfd3100f06804e
Víctor del Val <victor@regen.network>
2022-06-30 18:53:52 +0200

    Victor/899 create batch (#1002)
    * Add Cypress to registry with initial setup for e2e
    
    * Add MultiStep form template module, with generic context and section UI component
    
    * Integrate create batch form, steps and context
    
    * Generalize credit class select and metadata json field
    
    * Improve and refactor MultiStep module, context and section component
    
    * Add useLocalStorage hook
    
    * Add initial display in review, and minor improvements
    
    * Add optional initial selection to project select field
    
    * Add handleActiveStep to multi-step context. Improve Review step view
    
    * Improve step management, initialization and validation flows
    
    * Improve e2e create batch test. Add name prop to datepickers
    
    * Add Testing Library for Cypress
    
    * Improve create batch e2e test with query selectors and data fixture
    
    * Improve create batch e2e, form data fixture and new test case
    
    * Add useCreateBatch with initial approach to submission flow
    
    * Complete basic submit flow for create batch
    
    * Add initial selection to province/state selector to update possible previous state
    
    * Enable additional certifications, display in review and add to ledger Msg
    
    * Add a new dataDisplay state, both local and persistent, to complete the info display in the review step
    
    * Improve review screen. Also add EditIcon
    
    * Fix auxiliar dataDisplay when partial submit and save to context
    
    * Add success view in finished step
    
    * Add EditIcon snapshot
    
    * Fix EditIcon props
    
    * Add missing export for CardItem
    
    * Improve locationStandard query when result with missing state/province but informed
    
    * Remove cypress poc
    
    * Use axios instead of fetch api for iri-gen request
    
    * Improve result behavior, for success and error. Also add error view and clear storage
    
    * Fix button styles
    
    * Add first recipient note found to Tx memo
    
    * Improve additional certifications display in review step
    
    * Update credit class select component to fetch data from ledger only
    
    * Add error title to result component, with broken link icon
    
    * Add check to display retirement notes only for the first item in case of FieldArray form
    
    * feat: improve style and link in create batch review and success
    
    * review: undo attempt to use generic types in component
    
    * review: improve projectId as number, not found component and deprecation comment
    
    * feat: add bench32 address validation utility function and apply to recipients
    
    * feat: improve restrictions on date pickers and past date validation
    
    * review: improve address validation, move keplr dep and reuse from roles too
    
    * review: move keplr-wallet/cosmos as a production dep in web-components
    
    (cherry picked from commit 42baeaf08e2bea8eeefc2ad886327670062e8b46)

---
+++ web-components/src/components/icons/EditIcon.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import SvgIcon from '@mui/material/SvgIcon';
+import { SxProps, Theme } from '@mui/material';
+
+interface EditIconProps {
+  sx?: SxProps<Theme>;
+}
+
+export default function EditIcon({ sx }: EditIconProps): JSX.Element {
+  return (
+    <SvgIcon
+      sx={sx}
+      width="13"
+      height="13"
+      viewBox="0 0 13 13"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <svg fill="none">
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M0.198198 10.3981C0.151314 10.445 0.124974 10.5086 0.124974 10.5749L0.124969 12.6246C0.124969 12.7628 0.237136 12.8748 0.37536 12.8746L2.42156 12.8714C2.48773 12.8713 2.55116 12.845 2.59795 12.7982L10.8856 4.51056L8.48566 2.11065L0.198198 10.3981ZM9.19277 1.40355L11.5927 3.80345L12.439 2.95709C12.8296 2.56656 12.8296 1.93339 12.439 1.54287L11.4534 0.557175C11.0628 0.166651 10.4297 0.166651 10.0391 0.557175L9.19277 1.40355Z"
+          fill="#4FB573"
+        />
+      </svg>
+    </SvgIcon>
+  );
+}
---
+++ web-registry/src/features/ecocredit/CreateBatchBySteps/CreateBatchMultiStepForm/index.ts
@@ -0,0 +1,9 @@
+import { default as CreateBatchMultiStepForm } from './CreateBatchMultiStepForm';
+export default CreateBatchMultiStepForm;
+
+// config about Credit Basics fields for formModel
+export {
+  validationSchema as creditBasicsValidationSchema,
+  validationSchemaFields as creditBasicsValidationSchemaFields,
+  initialValues as creditBasicsInitialValues,
+} from './CreditBasics';

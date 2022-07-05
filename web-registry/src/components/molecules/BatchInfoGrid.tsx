bc28d1b26d4b8599afbea23022788dd9e5688f51
Mark Hagelberg <mark.hagelberg@gmail.com>
2022-07-05 17:23:27 -0600

    chore: fix merge error

--- web-registry/src/components/molecules/BatchInfoGrid.tsx
+++ web-registry/src/components/molecules/BatchInfoGrid.tsx
@@ -50,8 +50,10 @@ export const BatchInfoGrid: React.FC<{
   </Grid>
 );
 
-const batchDate = (date: string | Date): string =>
-  formatDate(date, 'MMM D, YYYY', true);
+const batchDate = (date?: string | Date): string => {
+  if (!date) return '-';
+  return formatDate(date, 'MMM D, YYYY', true);
+};
 
 const GridItem: React.FC = ({ children }) => (
   <Grid item xs={12} sm={5}>

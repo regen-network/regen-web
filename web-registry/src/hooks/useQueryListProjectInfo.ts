ff70c754e4e9427df89bee18ba8d1c7765e29d7d
victordelval <victor@regen.network>
2022-07-07 18:57:23 +0200

    chore: basic update for basket details hooks

--- web-registry/src/hooks/useQueryListProjectInfo.ts
+++ web-registry/src/hooks/useQueryListProjectInfo.ts
@@ -1,19 +1,19 @@
 import { useState, useEffect } from 'react';
 
 import {
-  QueryBatchResponse,
+  QueryProjectResponse,
   QueryClientImpl,
 } from '@regen-network/api/lib/generated/regen/ecocredit/v1/query';
 
 import { useLedger } from '../ledger';
 
 // is a wrapper for a batch of requests
-export default function useQueryListBatchInfo(
-  basketBatches?: string[],
-): QueryBatchResponse[] | undefined {
+export default function useQueryListProjectInfo(
+  projectIds?: string[],
+): QueryProjectResponse[] | undefined {
   const { api } = useLedger();
   const [queryClient, setQueryClient] = useState<QueryClientImpl>();
-  const [dataList, setDataList] = useState<QueryBatchResponse[]>();
+  const [dataList, setDataList] = useState<QueryProjectResponse[]>();
 
   useEffect(() => {
     if (!api?.queryClient) return;
@@ -22,21 +22,21 @@ export default function useQueryListBatchInfo(
   }, [api?.queryClient]);
 
   useEffect(() => {
-    if (!queryClient || !basketBatches) return;
+    if (!queryClient || !projectIds) return;
 
     async function fetchData(
       client: QueryClientImpl,
-      batches: string[],
+      projects: string[],
     ): Promise<void> {
       Promise.all(
-        batches.map(
-          async (batchDenom: string) => await client.Batch({ batchDenom }),
+        projects.map(
+          async (projectId: string) => await client.Project({ projectId }),
         ),
       ).then(setDataList);
     }
 
-    fetchData(queryClient, basketBatches);
-  }, [queryClient, basketBatches]);
+    fetchData(queryClient, projectIds);
+  }, [queryClient, projectIds]);
 
   return dataList;
 }

09a1c27f9ce2a14464bd05fdd848c138bf796fdd
Florent <florent.lagrede@gmail.com>
2022-08-22 14:49:46 +0200

    feat: add projects sorted by orders on homepage

---
+++ web-registry/src/pages/Home/Home.styles.tsx
@@ -0,0 +1,35 @@
+import { makeStyles } from '@mui/styles';
+
+export const useHomeStyles = makeStyles(theme => ({
+  section: {
+    [theme.breakpoints.up('sm')]: {
+      paddingBottom: theme.spacing(22.25),
+    },
+    [theme.breakpoints.down('sm')]: {
+      paddingBottom: theme.spacing(17.75),
+    },
+  },
+  title: {
+    marginBottom: theme.spacing(8.75),
+    [theme.breakpoints.down('sm')]: {
+      marginBottom: theme.spacing(8),
+    },
+  },
+  projectCards: {
+    [theme.breakpoints.down('md')]: {
+      width: '100vw',
+      marginLeft: theme.spacing(-8),
+    },
+    [theme.breakpoints.down('sm')]: {
+      marginLeft: theme.spacing(-4),
+    },
+  },
+  bottomSectionWidth: {
+    maxWidth: theme.spacing(200),
+  },
+  bottomSection: {
+    display: 'flex',
+    justifyContent: 'center',
+    paddingBottom: theme.spacing(17.75),
+  },
+}));

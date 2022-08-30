38d7885d51c9e9ba6ddf17f040678636b17781af
Florent <florent.lagrede@gmail.com>
2022-08-30 11:45:33 +0200

    feat: revert sort to projects with sell orders first

--- web-registry/src/pages/Projects/hooks/useProjectsSellOrders.tsx
+++ web-registry/src/pages/Projects/hooks/useProjectsSellOrders.tsx
@@ -5,7 +5,10 @@ import { ProjectInfo } from '@regen-network/api/lib/generated/regen/ecocredit/v1
 import { getMetadata } from 'lib/metadata-graph';
 
 import { ProjectWithOrderData } from '../Projects.types';
-import { getPurchaseInfo } from './useProjectsSellOrders.utils';
+import {
+  getPurchaseInfo,
+  sortProjectsBySellOrdersAvailability,
+} from './useProjectsSellOrders.utils';
 
 import DefaultProject from 'assets/default-project.jpg';
 
@@ -52,42 +55,46 @@ const getProjectDisplayData = async (
   limit: number,
 ): Promise<ProjectWithOrderData[]> => {
   const projectsWithOrderData = await Promise.all(
-    projects.slice(0, limit).map(async (project: ProjectInfo) => {
-      const sellOrdersNormalized = sellOrders
-        .filter(sellOrder => sellOrder?.batchDenom?.startsWith(project.id))
-        .map(sellOrder => {
-          return {
-            ...sellOrder,
-            id: String(sellOrder.id),
-          };
-        });
-      const purchaseInfo = getPurchaseInfo(project.id, sellOrders);
-      let metadata;
-      if (project.metadata.length) {
-        try {
-          metadata = await getMetadata(project.metadata);
-        } catch (error) {
-          // eslint-disable-next-line
-          console.error(error);
+    projects
+      .sort(sortProjectsBySellOrdersAvailability(sellOrders))
+      .slice(0, limit)
+      .map(async (project: ProjectInfo) => {
+        const sellOrdersNormalized = sellOrders
+          .filter(sellOrder => sellOrder?.batchDenom?.startsWith(project.id))
+          .map(sellOrder => {
+            return {
+              ...sellOrder,
+              id: String(sellOrder.id),
+            };
+          });
+        const purchaseInfo = getPurchaseInfo(project.id, sellOrders);
+        let metadata;
+        if (project.metadata.length) {
+          try {
+            metadata = await getMetadata(project.metadata);
+          } catch (error) {
+            // eslint-disable-next-line
+            console.error(error);
+          }
         }
-      }
 
-      return {
-        id: project.id,
-        name: metadata?.['schema:name'] || project.id,
-        imgSrc: metadata?.['regen:previewPhoto']?.['@value'] || DefaultProject,
-        place:
-          metadata?.['schema:location']?.place_name || project.jurisdiction,
-        area: metadata?.['regen:projectSize']?.['qudt:numericValue']?.[
-          '@value'
-        ],
-        areaUnit:
-          metadata?.['regen:projectSize']?.['qudt:unit']?.['@value'] || '',
-        purchaseInfo,
-        href: `/projects/${project.id}`,
-        sellOrders: sellOrdersNormalized,
-      } as ProjectWithOrderData;
-    }),
+        return {
+          id: project.id,
+          name: metadata?.['schema:name'] || project.id,
+          imgSrc:
+            metadata?.['regen:previewPhoto']?.['@value'] || DefaultProject,
+          place:
+            metadata?.['schema:location']?.place_name || project.jurisdiction,
+          area: metadata?.['regen:projectSize']?.['qudt:numericValue']?.[
+            '@value'
+          ],
+          areaUnit:
+            metadata?.['regen:projectSize']?.['qudt:unit']?.['@value'] || '',
+          purchaseInfo,
+          href: `/projects/${project.id}`,
+          sellOrders: sellOrdersNormalized,
+        } as ProjectWithOrderData;
+      }),
   );
   return projectsWithOrderData;
 };

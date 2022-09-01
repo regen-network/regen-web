c9438de2fc67c9ae48100db10ab2cead6d01f87d
Víctor del Val <victor@regen.network>
2022-09-01 11:49:33 +0200

    Review 1116 location state region (#1163)
    * review: country and state location fields with lazy and suspense
    
    * review: add generic fallback selector component, and use with location fields
    
    * chore: improve country options preparation with util function
    
    * refactor: improve country subdivisions options preparation with util function
    
    * chore: Add a comment to the new location standard utility functions. It also makes the default country generic
    
    (cherry picked from commit fe49a817bac395bad911ebcf72a93e3c58ffd170)

--- web-registry/src/components/organisms/BuyCreditsModal/BuyCreditsModal.tsx
+++ web-registry/src/components/organisms/BuyCreditsModal/BuyCreditsModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactHtmlParser from 'react-html-parser';
 import { Link } from 'react-router-dom';
 import { Box, useTheme } from '@mui/material';
@@ -15,9 +15,8 @@ import InfoIcon from 'web-components/lib/components/icons/InfoIcon';
 import { RegenTokenIcon } from 'web-components/lib/components/icons/RegenTokenIcon';
 import { Image } from 'web-components/lib/components/image';
 import ControlledTextField from 'web-components/lib/components/inputs/ControlledTextField';
-import LocationCountryField from 'web-components/lib/components/inputs/LocationCountryField';
-import LocationStateField from 'web-components/lib/components/inputs/LocationStateField';
 import NumberTextField from 'web-components/lib/components/inputs/NumberTextField';
+import SelectFieldFallback from 'web-components/lib/components/inputs/SelectFieldFallback';
 import SelectTextField from 'web-components/lib/components/inputs/SelectTextField';
 import Toggle from 'web-components/lib/components/inputs/Toggle';
 import Modal, { RegenModalProps } from 'web-components/lib/components/modal';
@@ -37,6 +36,13 @@ import { BUY_CREDITS_MODAL_DEFAULT_VALUES } from './BuyCreditsModal.constants';
 import { useBuyCreditsModalStyles } from './BuyCreditsModal.styles';
 import { getOptions, handleBuyCreditsSubmit } from './BuyCreditsModal.utils';
 import { useSetSelectedSellOrder } from './hooks/useSetSelectedSellOrder';
+
+const LocationCountryField = lazy(
+  () => import('web-components/lib/components/inputs/LocationCountryField'),
+);
+const LocationStateField = lazy(
+  () => import('web-components/lib/components/inputs/LocationStateField'),
+);
 
 export interface Credits {
   purchased: number;
@@ -301,7 +307,16 @@ const BuyCreditsModal: React.FC<BuyCreditsModalProps> = ({
                         sm={6}
                         className={styles.stateCountryTextField}
                       >
-                        <LocationCountryField />
+                        <Suspense
+                          fallback={
+                            <SelectFieldFallback
+                              label="Country"
+                              name="country"
+                            />
+                          }
+                        >
+                          <LocationCountryField />
+                        </Suspense>
                       </Grid>
                       <Grid
                         item
@@ -309,7 +324,19 @@ const BuyCreditsModal: React.FC<BuyCreditsModalProps> = ({
                         sm={6}
                         className={styles.stateCountryTextField}
                       >
-                        <LocationStateField country={values.country} optional />
+                        <Suspense
+                          fallback={
+                            <SelectFieldFallback
+                              label="State / Region"
+                              name="state"
+                            />
+                          }
+                        >
+                          <LocationStateField
+                            country={values.country}
+                            optional
+                          />
+                        </Suspense>
                       </Grid>
                     </Grid>
                     <Field

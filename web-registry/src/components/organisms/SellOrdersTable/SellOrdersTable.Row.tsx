03a50f9f64ec4e3827c351ec683c10a507b498e8
Florent <florent.lagrede@gmail.com>
2022-09-01 18:10:01 +0200

    feat: add new coin icons

--- web-registry/src/components/organisms/SellOrdersTable/SellOrdersTable.Row.tsx
+++ web-registry/src/components/organisms/SellOrdersTable/SellOrdersTable.Row.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactHtmlParser from 'react-html-parser';
 import { Box } from '@mui/material';
 
+import AxlUsdcIcon from 'web-components/lib/components/icons/coins/AxlUsdcIcon';
+import EeurIcon from 'web-components/lib/components/icons/coins/EeurIcon';
+import GravUsdcIcon from 'web-components/lib/components/icons/coins/GravUsdcIcon';
+import UsdcIcon from 'web-components/lib/components/icons/coins/UsdcIcon';
 import { RegenTokenIcon } from 'web-components/lib/components/icons/RegenTokenIcon';
 import { formatDate, formatNumber } from 'web-components/lib/utils/format';
 import { truncate } from 'web-components/lib/utils/truncate';
 
 import { microToDenom } from 'lib/denom.utils';
 
-import { ReactComponent as USDCIcon } from '../../../assets/svgs/usdc.svg';
 import { getAccountUrl } from '../../../lib/block-explorer';
 import { NormalizedSellOrder } from '../../../pages/Marketplace/Storefront/Storefront.types';
 import { Link } from '../../atoms';
@@ -37,7 +40,10 @@ const getSellOrdersTableRow = ({
   </WithLoader>,
   <Box sx={{ fontWeight: 700, display: 'flex', alignItems: 'center' }}>
     <Box sx={{ mr: 1, display: 'flex', alignItems: 'center' }}>
-      {askDenom === 'usdc' && <USDCIcon />}
+      {askDenom === 'uusdc' && <UsdcIcon />}
+      {askDenom === 'ugravusdc' && <GravUsdcIcon />}
+      {askDenom === 'uaxlusdc' && <AxlUsdcIcon />}
+      {askDenom === 'eeur' && <EeurIcon />}
       {askDenom === 'uregen' && <RegenTokenIcon />}
     </Box>
     {`${formatNumber({ num: microToDenom(askAmount) })}`}

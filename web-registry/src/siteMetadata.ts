0c414845787cc5fe4d18b56e4c4a2fcf4dd62d29
Kyle Lawlor-Bagcal <wgwz@users.noreply.github.com>
2022-09-06 16:52:49 -0400

    feat: redirect registry to app (#1176)
    * feat: redirect registry to app
    
    * fix: replace registry.regen.network with app.regen.network
    
    * chore: update url in env.example
    
    (cherry picked from commit d5a97eaf1b38187f9753f1da6120b41f6a46ad01)

--- web-registry/src/siteMetadata.ts
+++ web-registry/src/siteMetadata.ts
@@ -2,7 +2,7 @@ const siteMetadata = {
   title: `Regen Network Registry`,
   description: `Buy and sell ecosystem service credits at the open marketplace for climate solutions.registry`,
   author: `Regen Network`,
-  siteUrl: 'https://registry.regen.network',
+  siteUrl: 'https://app.regen.network',
 };
 
 export default siteMetadata;

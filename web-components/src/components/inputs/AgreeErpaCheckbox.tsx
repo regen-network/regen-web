f691dccebe5621701af664708a3f7751653b2a7e
victordelval <victor@regen.network>
2022-09-13 13:01:36 +0200

    feat: update agreement language, links and style

--- web-components/src/components/inputs/AgreeErpaCheckbox.tsx
+++ web-components/src/components/inputs/AgreeErpaCheckbox.tsx
@@ -1,7 +1,11 @@
-import { Link, SxProps, Theme } from '@mui/material';
+import { Link as LinkExt, SxProps, Theme } from '@mui/material';
 import { useTheme } from '@mui/styles';
 
 import AgreeCheckbox from './AgreeCheckbox';
+
+const URL_TERMS_SERVICE = 'https://www.regen.network/terms-service/';
+const URL_MARKETPLACE_LEGAL =
+  'https://regennetwork.notion.site/Regen-Marketplace-Contracts-7bc2b0075ee64c1a957463bb5eddcead';
 
 interface Props {
   sx?: SxProps<Theme>;
@@ -15,17 +19,30 @@ const AgreeErpaCheckbox: React.FC<Props> = ({ sx }: Props) => {
       label={
         <>
           {'I agree to the '}
-          <Link
-            href={'https://regen.network'}
+          <LinkExt
+            href={URL_MARKETPLACE_LEGAL}
             target="_blank"
             rel="noopener noreferrer"
             sx={{ color: theme.palette.secondary.main }}
           >
-            ERPA agreement
-          </Link>
+            Ecocredit Sales Agreement
+          </LinkExt>
+          {' and '}
+          <LinkExt
+            href={URL_TERMS_SERVICE}
+            target="_blank"
+            rel="noopener noreferrer"
+            sx={{ color: theme.palette.secondary.main }}
+          >
+            terms of service
+          </LinkExt>
         </>
       }
-      sx={sx}
+      sx={{
+        alignItems: 'start',
+        '& .MuiCheckbox-root': { mt: 0.75 },
+        ...sx,
+      }}
     />
   );
 };

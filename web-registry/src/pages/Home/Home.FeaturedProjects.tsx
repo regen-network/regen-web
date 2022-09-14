3605c71ae3c7ed6f243e07e85311775e62d71aba
Víctor del Val <victor@regen.network>
2022-09-14 20:43:37 +0200

    Feat 1098 show 3 more projects (#1180)
    * feat: add initial section reusing the new query hooks
    
    * feat: filter by id and select a subset of random projects
    
    * refactor: create config file in project details page
    
    * feat: improves the selection of projects to prioritize those with sell orders
    
    * feat: improve projects selection
    
    * refactor: move some constants to component config file
    
    * refactor: extract project hook and cards section organism
    
    * refactor: apply changes to featured projects section in the Home page
    
    * refactor: improve some filter and apply to projects page
    
    * chore: remove unused styles
    
    * perf: add check on hook effect to avoid repeating requests
    
    * refactor: move shuffle and filter optional logic to avoid unnecessary requests
    
    * chore: little improvement for prioritize with sell orders
    
    * chore: remove unused code
    
    * refactor: extract util functions to hook utils file
    
    * review: extract project selection function from hook, and fix parameter override
    
    * review: remove preliminary sort by sell orders and fix the other existing function
    
    * perf: improve comparison fn swapping filter for some approach

--- web-registry/src/pages/Home/Home.FeaturedProjects.tsx
+++ web-registry/src/pages/Home/Home.FeaturedProjects.tsx
@@ -1,77 +1,28 @@
 import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Box } from '@mui/material';
 
 import ContainedButton from 'web-components/lib/components/buttons/ContainedButton';
-import ProjectCard from 'web-components/lib/components/cards/ProjectCard';
-import Section from 'web-components/lib/components/section';
 
 import { BuySellOrderFlow } from 'features/marketplace/BuySellOrderFlow';
-import {
-  API_URI,
-  IMAGE_STORAGE_BASE_URL,
-} from 'pages/Projects/Projects.config';
 import { ProjectWithOrderData } from 'pages/Projects/Projects.types';
-import WithLoader from 'components/atoms/WithLoader';
+import { ProjectCardsSection } from 'components/organisms/ProjectCardsSection/ProjectCardsSection';
 
-import { useHomeStyles } from './Home.styles';
+import { useFeaturedProjects } from './hooks/useFeaturedProjects';
 
-type Props = {
-  projects: ProjectWithOrderData[];
-  isLoading: boolean;
-};
-
-export const FeaturedProjects = ({
-  projects,
-  isLoading,
-}: Props): JSX.Element => {
-  const styles = useHomeStyles();
-  const navigate = useNavigate();
+export function FeaturedProjects(): JSX.Element {
+  const { featuredProjects } = useFeaturedProjects();
   const [selectedProject, setSelectedProject] =
     useState<ProjectWithOrderData | null>(null);
 
   return (
     <div id="projects">
-      <Section
+      <ProjectCardsSection
         title="Featured Projects"
-        titleAlign="center"
-        classes={{ root: styles.section, title: styles.title }}
-      >
-        <WithLoader
-          isLoading={isLoading}
-          sx={{ width: '100%', display: 'flex', justifyContent: 'center' }}
-        >
-          <Box
-            sx={{
-              display: 'grid',
-              gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 338px))',
-              gridGap: '1.125rem',
-              flex: 1,
-              justifyContent: 'center',
-            }}
-          >
-            {projects?.map(project => (
-              <Box key={project?.id}>
-                <ProjectCard
-                  name={project?.name}
-                  imgSrc={project?.imgSrc}
-                  place={project?.place}
-                  area={project?.area}
-                  areaUnit={project?.areaUnit}
-                  onButtonClick={() => setSelectedProject(project)}
-                  purchaseInfo={project.purchaseInfo}
-                  onClick={() => navigate(`/projects/${project.id}`)}
-                  imageStorageBaseUrl={IMAGE_STORAGE_BASE_URL}
-                  apiServerUrl={API_URI}
-                  truncateTitle={true}
-                  sx={{ width: 338, height: 479 }}
-                />
-              </Box>
-            ))}
-          </Box>
-        </WithLoader>
-      </Section>
-      <Box sx={{ display: 'flex', justifyContent: 'center', mb: 30 }}>
+        projects={featuredProjects}
+        onButtonClick={setSelectedProject}
+      />
+      <Box sx={{ display: 'flex', justifyContent: 'center', mb: 20 }}>
         <Link to="/projects">
           <ContainedButton>{'DISCOVER PROJECTS'}</ContainedButton>
         </Link>
@@ -82,4 +33,4 @@ export const FeaturedProjects = ({
       />
     </div>
   );
-};
+}

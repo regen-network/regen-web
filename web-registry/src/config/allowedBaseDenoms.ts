be3d50b6b88cf5bef00b03175dacd0e5563dc6e2
Florent Lagrede <florent.lagrede@gmail.com>
2022-09-15 15:44:28 +0200

    feat: show correct denom on sell order table and buy order modal (#1199)
    * feat: add new coin icons
    
    * feat: handle allowed denoms on buy and sell orders table
    
    * chore: update token assets
    
    * feat: call DenomTrace to unwrap ibc denom on sell orders
    
    * chore: update regen-js version
    
    * chore: remove unused import
    
    * fix: handle testnet denom
    
    * feat: use dynamic denom icon everywhere
    
    * fix: my eco credits early display
    
    * fix: sepate bank and display denom
    
    * fix: remove unused imports
    
    * fix: use display denom in BuyCreditsModal
    
    * refactor: always use governance approved display denom

---
+++ web-registry/src/config/allowedBaseDenoms.ts
@@ -0,0 +1,11 @@
+const isRedwood = process.env.REACT_APP_LEDGER_CHAIN_ID === 'regen-redwood-1';
+
+export const GRAVITY_USDC_DENOM = isRedwood
+  ? 'eco.uC.NCT'
+  : 'gravity0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
+export const AXELAR_USDC_DENOM = isRedwood ? 'uausdc' : 'uusdc';
+export const EEUR_DENOM = 'eeur';
+export const REGEN_DENOM = 'uregen';
+
+export const USD_DENOMS = [GRAVITY_USDC_DENOM, AXELAR_USDC_DENOM];
+export const EUR_DENOMS = [EEUR_DENOM];

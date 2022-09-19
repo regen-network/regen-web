c5da7df57f0f5104ffc496fdc5276bc0ffe0a5ce
Florent Lagrede <florent.lagrede@gmail.com>
2022-09-19 10:25:52 +0200

    fix: confirmation buy button label and link (#1236)
    (cherry picked from commit dfc2c619e67b502ba8c6fb7e6a83e77f474e63a7)

--- web-registry/src/pages/Marketplace/Storefront/Storefront.constants.ts
+++ web-registry/src/pages/Marketplace/Storefront/Storefront.constants.ts
@@ -1,7 +1,7 @@
 export const BUY_SELL_ORDER_HEADER = 'Congrats! Your purchase was successful.';
 export const BUY_SELL_ORDER_TITLE = 'Buy';
 export const BUY_SELL_ORDER_ACTION = 'BUY';
-export const BUY_SELL_ORDER_BUTTON = 'View all sell orders';
+export const BUY_SELL_ORDER_BUTTON = 'View your ecocredits';
 
 export const CANCEL_SELL_ORDER_HEADER = 'Your sell order was cancelled';
 export const CANCEL_SELL_ORDER_TITLE = 'Sell Order #';

058b572bf57b26e50b7eb3666f942a673f50cdab
Víctor del Val <victor@regen.network>
2022-09-28 20:11:36 +0200

    fix: add buy credits flow and modal state to more projects section (#1312)
    (cherry picked from commit 1500ca3d3e3c8d091b1686141e1e4bf277cc2b96)

--- web-registry/src/components/templates/ProjectDetails/ProjectDetails.MoreProjects.tsx
+++ web-registry/src/components/templates/ProjectDetails/ProjectDetails.MoreProjects.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Box from '@mui/material/Box';
 
+import { BuySellOrderFlow } from 'features/marketplace/BuySellOrderFlow/BuySellOrderFlow';
+import { ProjectWithOrderData } from 'pages/Projects/Projects.types';
 import { ProjectCardsSection } from 'components/organisms/ProjectCardsSection/ProjectCardsSection';
 
 import useMoreProjects from './hooks/useMoreProjects';
@@ -10,9 +12,25 @@ export function MoreProjects(): JSX.Element {
   const { projectId } = useParams();
   const projects = useMoreProjects(projectId as string);
 
+  const [selectedProject, setSelectedProject] =
+    useState<ProjectWithOrderData | null>(null);
+  const [isBuyFlowStarted, setIsBuyFlowStarted] = useState(false);
+
   return (
     <Box className="topo-background-alternate" sx={{ pb: 9 }}>
-      <ProjectCardsSection title="More Projects" projects={projects} />
+      <ProjectCardsSection
+        title="More Projects"
+        projects={projects}
+        onButtonClick={({ project }) => {
+          setSelectedProject(project);
+          setIsBuyFlowStarted(true);
+        }}
+      />
+      <BuySellOrderFlow
+        isFlowStarted={isBuyFlowStarted}
+        setIsFlowStarted={setIsBuyFlowStarted}
+        selectedProject={selectedProject}
+      />
     </Box>
   );
 }

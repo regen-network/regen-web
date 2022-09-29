fc0ea38ecd103a27598672750e308043e5a064c0
Florent Lagrede <florent.lagrede@gmail.com>
2022-09-29 09:47:40 +0200

    feat: new homepage header (#1295)
    * feat: update home header
    
    * fix: logo height on md breakpoint
    
    * refactor: remove js responsive check
    
    * styles: align home header
    
    * chore: remove hardcoded sanity url
    
    (cherry picked from commit 451a71374754686d0474cbb3652e056bd736bb98)

---
+++ web-registry/src/styles/gradients.ts
@@ -0,0 +1,10 @@
+import { SxProps } from '@mui/material';
+
+export const gradients = {
+  blueGreen: {
+    background:
+      'linear-gradient(204.4deg, #527984 5.94%, #79C6AA 51.92%, #C4DAB5 97.89%)',
+    backgroundClip: 'text',
+    textFillColor: 'transparent',
+  } as SxProps,
+};

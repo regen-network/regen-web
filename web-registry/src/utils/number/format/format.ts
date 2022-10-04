44e07f650ceb3caac2d460da43479182b82f4a27
Florent Lagrede <florent.lagrede@gmail.com>
2022-10-04 16:02:37 +0200

    feat: update sell order dropdown (#1337)
    * feat: update sell order dropdown
    
    * feat: switch to mui select
    
    * feat: floor quantity in sell orders
    
    * feat: handle placeholder
    
    * feat: sort by sell order id
    
    * feat: remove dollar prefix from buy order modal
    
    (cherry picked from commit 3d3059ba7f0dd3e9add8fb083f5fdc8ce13ab9c1)

--- web-registry/src/utils/number/format/format.ts
+++ web-registry/src/utils/number/format/format.ts
@@ -1,3 +1,5 @@
+/* roundFloatNumber */
+
 export type RoundFloatNumberParamsOptions = {
   decimals?: number;
 };
@@ -9,4 +11,19 @@ export const roundFloatNumber = (
   const decimals = options?.decimals ?? 2;
   const base = Math.pow(10, decimals);
   return Math.round(number * base) / base;
+};
+
+/* floorFloatNumber */
+
+export type FloorFloatNumberParamsOptions = {
+  decimals?: number;
+};
+
+export const floorFloatNumber = (
+  number: number,
+  options?: FloorFloatNumberParamsOptions,
+): number => {
+  const decimals = options?.decimals ?? 2;
+  const base = Math.pow(10, decimals);
+  return Math.floor(number * base) / base;
 };

014cd370be01efd5ec5974bafe7052fd16234d48
Mark Hagelberg <mark.hagelberg@gmail.com>
2022-10-06 15:58:42 -0600

    fix: just check for projectsWithOrderData.length to stop circular dependency

--- web-registry/src/pages/Projects/hooks/useProjectsSellOrders.tsx
+++ web-registry/src/pages/Projects/hooks/useProjectsSellOrders.tsx
@@ -40,7 +40,6 @@ export const useProjectsSellOrders = ({
   useEffect(() => {
     if (projectsWithOrderData.length > 0) return;
     const normalize = async (): Promise<void> => {
-      console.log('normalize');
       if (projects && sellOrders) {
         const _projectsWithOrders = await getProjectDisplayData(
           projects,

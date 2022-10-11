b4d68eaef93b5765416f45cfdae846ae2df101ec
Kyle Lawlor-Bagcal <kyle.lawlor@regen.network>
2022-10-11 13:57:11 -0400

    chore: remove unnecessary dependency from broadcast callback in msgClient

--- web-registry/src/hooks/useMsgClient.ts
+++ web-registry/src/hooks/useMsgClient.ts
@@ -83,7 +83,7 @@ export default function useMsgClient(
         handleTxDelivered(_deliverTxResponse);
       }
     },
-    [api?.msgClient, handleTxQueued, handleTxDelivered, handleError],
+    [api?.msgClient, handleTxQueued, handleTxDelivered],
   );
 
   const signAndBroadcast = useCallback(

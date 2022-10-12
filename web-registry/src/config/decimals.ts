3fd69444376f9ae6c4420dc8d4250de7785c03ff
Florent Lagrede <florent.lagrede@gmail.com>
2022-10-12 10:01:43 +0200

    fix: display quantity with 6 decimals (#1422)
    (cherry picked from commit 43649392acddada6868e14c2dbdd5024b8d50cce)

---
+++ web-registry/src/config/decimals.ts
@@ -0,0 +1,7 @@
+export const QUANTITY_MIN_DECIMALS = 0;
+export const QUANTITY_MAX_DECIMALS = 6;
+
+export const quantityFormatNumberOptions = {
+  minimumFractionDigits: QUANTITY_MIN_DECIMALS,
+  maximumFractionDigits: QUANTITY_MAX_DECIMALS,
+};

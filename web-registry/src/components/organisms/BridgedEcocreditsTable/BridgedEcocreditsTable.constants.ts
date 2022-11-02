6b4d774d9d7d148eabd3984cb82d1c7e2950b4d8
Víctor del Val <victor@regen.network>
2022-11-02 11:40:01 +0100

    Feat: add Bridge tab to dashboard (#1515)
    * feat: add bridge icon, also tab to ecocredit dashboard
    
    * feat: add initial scaffolding
    
    * feat: improve styles and extend some base components for custom styles
    
    * feat: improve styles for mobile
    
    * feat: add action button for bridge
    
    * feat: add bridge info panel
    
    * feat: extends no credits component. Remove mocked data
    
    * feat: add bridged table
    
    * feat: add status labels in bridged table. Add new icons
    
    * refactor: extract tables to organisms. Also review comments
    
    * review: styles and minior components refactor and improvements
    
    * chore: swap faked data for request data hook scaffolding

---
+++ web-registry/src/components/organisms/BridgedEcocreditsTable/BridgedEcocreditsTable.constants.ts
@@ -0,0 +1,6 @@
+export const BRIDGED_STATUS = ['pending', 'complete', 'error'];
+export const CREDIT_BATCH_TOOLTIP =
+  'Each credit batch has a unique ID (i.e. denomination) that starts with the abbreviation of the credit type followed by the project ID, start date and end date of the vintage year, and ending in the batch sequence number.';
+export const NO_BRIDGED_CREDITS = 'No bridged ecocredits found';
+export const AMOUNT_BRIDGED_TOOLTIP =
+  'Amount bridged is the same as amount cancelled in the ledger documentation';
--- web-registry/src/styles/table.ts
+++ web-registry/src/styles/table.ts
@@ -3,6 +3,15 @@ import { SxProps } from '@mui/material';
 export const ELLIPSIS_COLUMN_WIDTH = '125px';
 
 export const tableStyles = {
+  rootOnlyTopBorder: {
+    root: {
+      borderRadius: 0,
+      borderTopWidth: 1,
+      borderBottomWidth: 0,
+      borderLeftWidth: 0,
+      borderRightWidth: 0,
+    } as SxProps,
+  },
   ellipsisColumn: {
     whiteSpace: 'nowrap',
     overflow: 'hidden',

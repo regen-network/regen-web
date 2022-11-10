9ecc8a0b8926a5e70acdc06d3faf47dc8ae7c503
Kyle Lawlor-Bagcal <wgwz@users.noreply.github.com>
2022-11-10 12:33:43 -0500

    fix: add buy1 tracking event to project page, credit class page and sell order buy buttons (#1533)
    * fix: send buy2 tracking event from buy sell order modal on all pages
    
    * fix: remove duplicate buySuccess tracking event
    
    * feat: add buy1 tracking event to sticky bar
    
    * feat: add buy1 tracking event to storefront buy button
    
    * feat: use development version of google analytics by default
    
    * docs: clean up docs for analytics code
    
    * chore: remove unused vars
    
    * feat: hardcode default values for analytics
    
    * feat: consistent metadata for buy1 event
    
    * chore: enable analytics in deploy preview environment
    
    * fix: add back cardType field for project card buy1 event
    
    * fix: remove assertion and specify possible null for onChainProjectId
    
    * chore: add type info to trackData for the buy2 event
    
    * chore: remove unused import that causes build failure
    
    * chore: rename credit class name to credit class id
    
    * chore: disable analytics in deploy previews
    
    (cherry picked from commit 1beb42ec5b82d67ea2e446c173421fb3fba5818b)

--- web-registry/src/components/organisms/SellOrdersActionsBar/SellOrdersActionsBar.tsx
+++ web-registry/src/components/organisms/SellOrdersActionsBar/SellOrdersActionsBar.tsx
@@ -1,4 +1,6 @@
+import { useLocation } from 'react-router-dom';
 import { Box } from '@mui/material';
+import { useAnalytics } from 'use-analytics';
 
 import ContainedButton from 'web-components/lib/components/buttons/ContainedButton';
 import OutlinedButton from 'web-components/lib/components/buttons/OutlinedButton';
@@ -16,6 +18,10 @@ type Params = {
   isBuyButtonDisabled: boolean;
   onSellButtonClick: () => void;
   onBuyButtonClick: () => void;
+  onChainProjectId?: string | null;
+  projectName?: string;
+  onChainCreditClassId?: string;
+  creditClassName?: string;
 };
 
 export const SellOrdersActionsBar = ({
@@ -23,39 +29,60 @@ export const SellOrdersActionsBar = ({
   isBuyButtonDisabled,
   onSellButtonClick,
   onBuyButtonClick,
-}: Params): JSX.Element => (
-  <StickyBar>
-    <Box sx={{ display: 'flex', justifyContent: 'flex-end', width: '100%' }}>
-      <InfoTooltip
-        title={isSellButtonDisabled ? SELL_DISABLED_TOOLTIP : ''}
-        arrow
-        placement="top"
-      >
-        <span>
-          <OutlinedButton
-            onClick={onSellButtonClick}
-            disabled={isSellButtonDisabled}
-            sx={{ mr: 5 }}
-          >
-            {'SELL'}
-          </OutlinedButton>
-        </span>
-      </InfoTooltip>
-      <InfoTooltip
-        title={isBuyButtonDisabled ? BUY_DISABLED_TOOLTIP : ''}
-        arrow
-        placement="top"
-      >
-        <span>
-          <ContainedButton
-            startIcon={<CurrentCreditsIcon height="18px" width="18px" />}
-            onClick={onBuyButtonClick}
-            disabled={isBuyButtonDisabled}
-          >
-            {'BUY CREDITS'}
-          </ContainedButton>
-        </span>
-      </InfoTooltip>
-    </Box>
-  </StickyBar>
-);
+  onChainProjectId,
+  projectName,
+  onChainCreditClassId,
+  creditClassName,
+}: Params): JSX.Element => {
+  const location = useLocation();
+  const { track } = useAnalytics();
+  return (
+    <StickyBar>
+      <Box sx={{ display: 'flex', justifyContent: 'flex-end', width: '100%' }}>
+        <InfoTooltip
+          title={isSellButtonDisabled ? SELL_DISABLED_TOOLTIP : ''}
+          arrow
+          placement="top"
+        >
+          <span>
+            <OutlinedButton
+              onClick={onSellButtonClick}
+              disabled={isSellButtonDisabled}
+              sx={{ mr: 5 }}
+            >
+              {'SELL'}
+            </OutlinedButton>
+          </span>
+        </InfoTooltip>
+        <InfoTooltip
+          title={isBuyButtonDisabled ? BUY_DISABLED_TOOLTIP : ''}
+          arrow
+          placement="top"
+        >
+          <span>
+            <ContainedButton
+              startIcon={<CurrentCreditsIcon height="18px" width="18px" />}
+              onClick={() => {
+                // if any of the values used in event data are undefined,
+                // then they are not included in the tracking data.
+                // this is a feature of the track API.
+                track('buy1', {
+                  url: location.pathname,
+                  buttonLocation: 'stickyNav',
+                  projectName,
+                  projectId: onChainProjectId,
+                  creditClassName,
+                  creditClassId: onChainCreditClassId,
+                });
+                onBuyButtonClick();
+              }}
+              disabled={isBuyButtonDisabled}
+            >
+              {'BUY CREDITS'}
+            </ContainedButton>
+          </span>
+        </InfoTooltip>
+      </Box>
+    </StickyBar>
+  );
+};

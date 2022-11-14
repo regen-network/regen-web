f405429ccc88642f0230a70f69da80932ba07478
Marie Gauthier <marie.gauthier63@gmail.com>
2022-11-14 10:26:59 +0100

    fix: profile tabs spacing (#1550)
    * chore: update spacing in IconTabs for full width support on mobile
    
    * fix: update container display to get right padding
    
    * chore: rm useless padding
    
    * chore: revert formatting
    
    (cherry picked from commit bfdf4d0aa42429f19b4536573cda1b505be22b40)

--- web-components/src/components/tabs/IconTabs.tsx
+++ web-components/src/components/tabs/IconTabs.tsx
@@ -22,23 +22,39 @@ interface IconTabsProps {
     };
   };
   hideIndicator?: boolean;
+  mobileFullWidth?: boolean;
 }
 
 const StyledTabs = styled(Tabs, {
-  shouldForwardProp: prop => prop !== 'hideIndicator',
-})<TabsProps & { hideIndicator: boolean }>(({ theme, hideIndicator }) => ({
-  '& .MuiTabs-indicator': {
-    display: hideIndicator && 'none',
-    backgroundColor: theme.palette.secondary.main,
-    height: '3px',
-  },
-}));
+  shouldForwardProp: prop =>
+    prop !== 'hideIndicator' && prop !== 'mobileFullWidth',
+})<TabsProps & { mobileFullWidth: boolean; hideIndicator: boolean }>(
+  ({ mobileFullWidth, theme, hideIndicator }) => ({
+    '& .MuiTabs-flexContainer': {
+      display: 'block',
+    },
+    '& .MuiTabs-scroller': {
+      [theme.breakpoints.down('md')]: {
+        paddingLeft: mobileFullWidth ? theme.spacing(10) : 0,
+      },
+      [theme.breakpoints.down('sm')]: {
+        paddingLeft: mobileFullWidth ? theme.spacing(4) : 0,
+      },
+    },
+    '& .MuiTabs-indicator': {
+      display: hideIndicator && 'none',
+      backgroundColor: theme.palette.secondary.main,
+      height: '3px',
+    },
+  }),
+);
 
 const IconTabs: React.FC<IconTabsProps> = ({
   tabs,
   size,
   sxs,
   hideIndicator = false,
+  mobileFullWidth = false,
 }) => {
   const [value, setValue] = useState(0);
 
@@ -51,7 +67,12 @@ const IconTabs: React.FC<IconTabsProps> = ({
 
   return (
     <div>
-      <Box sx={{ ...sxs?.tab?.outer }}>
+      <Box
+        sx={{
+          ...sxs?.tab?.outer,
+          mx: mobileFullWidth ? { xs: -4, sm: -10, md: 0 } : 0,
+        }}
+      >
         <StyledTabs
           value={value}
           onChange={handleChange}
@@ -59,6 +80,7 @@ const IconTabs: React.FC<IconTabsProps> = ({
           scrollButtons={false}
           aria-label="tabs"
           hideIndicator={hideIndicator}
+          mobileFullWidth={mobileFullWidth}
         >
           {tabs.map((tab, index) => (
             <IconTab

988ef099932664d30de4b2ec47b843387f4c42ed
Florent Lagrede <florent.lagrede@gmail.com>
2022-11-17 15:27:22 +0100

    feat: add bridge link to user menu (#1567)
    (cherry picked from commit 83b67679a2b33c9594a74e80ddd7e05f9ee89af6)

---
+++ web-components/src/components/header/components/UserMenuItem.config.tsx
@@ -0,0 +1,38 @@
+import { Theme } from '@mui/material';
+
+import BridgeIcon from '../../icons/BridgeIcon';
+import CreditsIcon from '../../icons/CreditsIcon';
+import { HeaderDropdownItemProps } from './HeaderDropdownItems';
+import { NavLinkProps } from './NavLink';
+
+interface GetUserMenuItemsParams {
+  pathname: string;
+  linkComponent: React.FC<NavLinkProps>;
+  theme: Theme;
+}
+
+export const getUserMenuItems = ({
+  linkComponent,
+  pathname,
+  theme,
+}: GetUserMenuItemsParams): HeaderDropdownItemProps[] => [
+  {
+    pathname,
+    linkComponent,
+    title: 'My Portfolio',
+    href: '/ecocredits/portfolio',
+    icon: (
+      <CreditsIcon
+        sx={{ height: 18, width: 20 }}
+        color={theme.palette.secondary.main}
+      />
+    ),
+  },
+  {
+    pathname,
+    linkComponent,
+    title: 'Bridge',
+    icon: <BridgeIcon />,
+    href: '/ecocredits/bridge',
+  },
+];

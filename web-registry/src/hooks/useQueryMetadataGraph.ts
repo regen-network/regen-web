5a264354aaa366508ad65da28e542a22832bb91f
Mark Hagelberg <mark.hagelberg@gmail.com>
2022-11-23 13:35:30 -0700

    chore: use ProjectMetadataIntersectionLD

--- web-registry/src/hooks/useQueryMetadataGraph.ts
+++ web-registry/src/hooks/useQueryMetadataGraph.ts
@@ -1,14 +1,14 @@
 import { useEffect, useState } from 'react';
 
 import { getMetadata } from 'lib/db/api/metadata-graph';
-import { ProjectMetadataLD } from 'lib/db/types/json-ld';
+import { ProjectMetadataIntersectionLD } from 'lib/db/types/json-ld';
 
 /**
  * Returns metadata from off-chain metadata-graph resolver
  *  */
 export default function useQueryMetadataGraph(
   iri?: string,
-): ProjectMetadataLD | undefined {
+): Partial<ProjectMetadataIntersectionLD> | undefined {
   const [metadata, setMetadata] = useState();
 
   useEffect(() => {

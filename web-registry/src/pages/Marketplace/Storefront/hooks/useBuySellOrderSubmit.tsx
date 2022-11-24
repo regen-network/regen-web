8c2bb5d7924016b72ecb4177d9d93fda585d2bc1
Florent <florent.lagrede@gmail.com>
2022-11-24 15:58:00 +0100

    fix: refetch issue

--- web-registry/src/pages/Marketplace/Storefront/hooks/useBuySellOrderSubmit.tsx
+++ web-registry/src/pages/Marketplace/Storefront/hooks/useBuySellOrderSubmit.tsx
@@ -85,6 +85,10 @@ const useBuySellOrderSubmit = ({
 
       if (refetchSellOrders) {
         const sellOrders = await refetchSellOrders();
+        console.log(
+          '🚀 ~ file: useBuySellOrderSubmit.tsx ~ line 88 ~ sellOrders',
+          sellOrders,
+        );
         const uiSellOrdersInfo = sellOrders?.map(normalizeToUISellOrderInfo);
         const { isBuyOrderInvalid } = checkIsBuyOrderInvalid({
           creditCount,

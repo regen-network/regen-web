8e850e2050fd8c5e58c449cd02f3fe9aba943346
victordelval <victor@regen.network>
2022-12-01 16:01:45 +0100

    chore: add projects page loader

---
+++ web-registry/src/pages/Projects/Projects.loader.ts
@@ -0,0 +1,50 @@
+import { QueryClient } from '@tanstack/react-query';
+
+import { getEcocreditQueryClient } from 'lib/clients/ecocreditQueryClient';
+import { getMarketplaceQueryClient } from 'lib/clients/marketplaceQueryClient';
+import { getSimplePriceQuery } from 'lib/queries/react-query/coingecko/simplePrice/simplePriceQuery';
+import { getProjectsQuery } from 'lib/queries/react-query/ecocredit/getProjectsQuery/getProjectsQuery';
+import { getSellOrdersExtendedQuery } from 'lib/queries/react-query/marketplace/getSellOrdersExtendedQuery/getSellOrdersExtendedQuery';
+import { getFromCacheOrFetch } from 'lib/queries/react-query/utils/getFromCacheOrFetch';
+
+type LoaderType = {
+  queryClient: QueryClient;
+};
+
+export const projectsLoader =
+  ({ queryClient }: LoaderType) =>
+  async ({ params }: { params: any }) => {
+    // Clients
+    const ecocreditClient = await getEcocreditQueryClient();
+    const marketplaceClient = await getMarketplaceQueryClient();
+
+    // Queries
+    const projectsQuery = getProjectsQuery({
+      client: ecocreditClient,
+      request: {},
+    });
+    const sellOrdersQuery = getSellOrdersExtendedQuery({
+      client: marketplaceClient,
+      reactQueryClient: queryClient,
+      request: {},
+    });
+    const simplePriceQuery = getSimplePriceQuery({});
+
+    // Fetch or Cache
+
+    // Mandatory data
+    await getFromCacheOrFetch({
+      query: projectsQuery,
+      reactQueryClient: queryClient,
+    });
+    // Optionnal data
+    getFromCacheOrFetch({
+      query: sellOrdersQuery,
+      reactQueryClient: queryClient,
+    });
+    getFromCacheOrFetch({
+      query: simplePriceQuery,
+      reactQueryClient: queryClient,
+    });
+    return {};
+  };

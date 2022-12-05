0ee88713e70e9bd88ef24a2fa785961e5f650483
Florent Lagrede <florent.lagrede@gmail.com>
2022-12-05 17:09:47 +0100

    perf: fetching portfolio page (#1626)
    * perf: use react-query for ecocredits queries
    
    * fix: pagination issue
    
    * fix: prevent rerender loop
    
    * feat: add bank and basket queries
    
    * feat: add useFetchBaskets hook
    
    * perf: use react-query for basket queries
    
    * chore: move normalizer
    
    * doc: clean comments
    
    * fix: rebase
    
    * chore: change folders organization and add comments
    
    * review: typing improvements
    
    (cherry picked from commit 8f64a33c94858a3b7bca40cdfb25a7a3594564c5)

---
+++ web-registry/src/lib/normalizers/normalizeCreditBaskets/normalizeCreditBaskets.ts
@@ -0,0 +1,28 @@
+import type { QueryBasketResponse } from '@regen-network/api/lib/generated/regen/ecocredit/basket/v1/query';
+
+import type { BatchInfoWithBalance } from 'types/ledger/ecocredit';
+
+type Props = {
+  basketsWithClasses?: (QueryBasketResponse | undefined)[];
+  credits: BatchInfoWithBalance[];
+};
+
+type Response = (QueryBasketResponse | undefined)[][];
+
+const normalizeCreditBaskets = ({
+  basketsWithClasses,
+  credits,
+}: Props): Response => {
+  // Get available baskets to put credits into
+  if (credits && basketsWithClasses && basketsWithClasses.length > 0) {
+    return credits.map(credit =>
+      basketsWithClasses.filter(
+        basket => credit.classId && basket?.classes.includes(credit.classId),
+      ),
+    );
+  }
+
+  return [];
+};
+
+export default normalizeCreditBaskets;
---
+++ web-registry/src/lib/queries/react-query/cosmos/bank/getDenomMetadataQuery/getDenomMetadataQuery.types.ts
@@ -0,0 +1,19 @@
+import {
+  DeepPartial,
+  QueryClientImpl as BankQueryClientImpl,
+  QueryDenomMetadataRequest,
+  QueryDenomMetadataResponse,
+} from '@regen-network/api/lib/generated/cosmos/bank/v1beta1/query';
+import { QueryObserverOptions } from '@tanstack/react-query';
+
+import { ReactQueryBuilderResponse } from '../../../types/react-query.types';
+
+export type ReactQueryDenomMetadataResponse = QueryObserverOptions<
+  QueryDenomMetadataResponse | undefined
+>;
+
+export type ReactQueryDenomMetadataProps = {
+  request: DeepPartial<QueryDenomMetadataRequest>;
+} & {
+  client?: BankQueryClientImpl;
+} & ReactQueryBuilderResponse<ReactQueryDenomMetadataResponse>;

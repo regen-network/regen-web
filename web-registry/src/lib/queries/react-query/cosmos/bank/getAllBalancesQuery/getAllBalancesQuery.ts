3a7a321aa22448ea082e5a6871d43abb27fab556
Florent Lagrede <florent.lagrede@gmail.com>
2022-12-06 15:36:51 +0100

    feat: check user balance in buy modal (#1638)
    * feat: check user balance in buy modal
    
    * chore: clean condition
    
    * qa: change label
    
    * feat: capitalize regen denom

---
+++ web-registry/src/lib/queries/react-query/cosmos/bank/getAllBalancesQuery/getAllBalancesQuery.ts
@@ -0,0 +1,18 @@
+import { BANK_ALL_BALANCES_KEY } from './getAllBalancesQuery.constants';
+import {
+  ReactQueryAllBalancesProps,
+  ReactQueryAllBalancesResponse,
+} from './getAllBalancesQuery.types';
+
+export const getAllBalancesQuery = ({
+  client,
+  request,
+  ...params
+}: ReactQueryAllBalancesProps): ReactQueryAllBalancesResponse => ({
+  queryKey: [BANK_ALL_BALANCES_KEY, request.address],
+  queryFn: async () => {
+    if (!client) return undefined;
+    return await client.AllBalances(request);
+  },
+  ...params,
+});

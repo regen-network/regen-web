9dcaebb2752433f5857876c5db4dacecd682924e
Florent <florent.lagrede@gmail.com>
2023-01-02 17:54:03 +0100

    fix: prevent more projects flickering

--- web-registry/src/components/templates/ProjectDetails/hooks/useMoreProjects.ts
+++ web-registry/src/components/templates/ProjectDetails/hooks/useMoreProjects.ts
@@ -1,3 +1,5 @@
+import { useRef } from 'react';
+
 import { ProjectWithOrderData } from 'pages/Projects/Projects.types';
 import { useProjectsWithOrders } from 'hooks/projects/useProjectsWithOrders';
 
@@ -6,11 +8,16 @@ const PROJECTS_LIMIT = 3;
 export default function useMoreProjects(
   projectId: string,
 ): ProjectWithOrderData[] {
+  const projectsRef = useRef<ProjectWithOrderData[]>([]);
   const { projectsWithOrderData } = useProjectsWithOrders({
     limit: PROJECTS_LIMIT,
     random: true,
     projectId,
   });
 
-  return projectsWithOrderData;
+  if (projectsRef.current.length === 0 && projectsWithOrderData.length > 0) {
+    projectsRef.current = projectsWithOrderData;
+  }
+
+  return projectsRef.current;
 }

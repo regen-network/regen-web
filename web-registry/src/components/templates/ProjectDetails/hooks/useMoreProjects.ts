b35720cbb58308ea2297a25415710ab2542730c9
Florent <florent.lagrede@gmail.com>
2023-01-03 09:53:33 +0100

    fix: reduce metadata requests in useMoreProjects

--- web-registry/src/components/templates/ProjectDetails/hooks/useMoreProjects.ts
+++ web-registry/src/components/templates/ProjectDetails/hooks/useMoreProjects.ts
@@ -1,5 +1,3 @@
-import { useRef } from 'react';
-
 import { ProjectWithOrderData } from 'pages/Projects/Projects.types';
 import { useProjectsWithOrders } from 'hooks/projects/useProjectsWithOrders';
 
@@ -8,16 +6,11 @@ const PROJECTS_LIMIT = 3;
 export default function useMoreProjects(
   projectId: string,
 ): ProjectWithOrderData[] {
-  const projectsRef = useRef<ProjectWithOrderData[]>([]);
   const { projectsWithOrderData } = useProjectsWithOrders({
     limit: PROJECTS_LIMIT,
     random: true,
     projectId,
   });
 
-  if (projectsRef.current.length === 0 && projectsWithOrderData.length > 0) {
-    projectsRef.current = projectsWithOrderData;
-  }
-
-  return projectsRef.current;
+  return projectsWithOrderData;
 }

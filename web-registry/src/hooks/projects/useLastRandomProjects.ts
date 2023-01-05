261ee8d0e9e04f6cba807295e12d893a88e63c21
Florent Lagrede <florent.lagrede@gmail.com>
2023-01-05 09:56:04 +0100

    fix: random projects flickering (#1715)
    * fix: prevent more projects flickering
    
    * fix: reduce metadata requests in useMoreProjects
    
    (cherry picked from commit 7972a616ed43cfaab08b768b60a9d274fa46a8ef)

---
+++ web-registry/src/hooks/projects/useLastRandomProjects.ts
@@ -0,0 +1,24 @@
+import { useRef } from 'react';
+import { ProjectInfo } from '@regen-network/api/lib/generated/regen/ecocredit/v1/query';
+
+type Props = {
+  random: boolean;
+  selectedProjects: ProjectInfo[];
+};
+
+export const useLastRandomProjects = ({
+  random,
+  selectedProjects,
+}: Props): ProjectInfo[] | undefined => {
+  const lastRandomProjectsRef = useRef<ProjectInfo[] | undefined>();
+
+  if (
+    random &&
+    selectedProjects?.length > 0 &&
+    !lastRandomProjectsRef.current
+  ) {
+    lastRandomProjectsRef.current = selectedProjects;
+  }
+
+  return lastRandomProjectsRef.current;
+};

3274c32f0352fca1c9d1ac3f375226f7c23eee06
Florent <florent.lagrede@gmail.com>
2023-01-16 10:53:52 +0100

    doc: fix typo

--- web-registry/src/lib/ecocredit/ecocredit.config.ts
+++ web-registry/src/lib/ecocredit/ecocredit.config.ts
@@ -1,6 +1,6 @@
 // This is a batch denom mapping between v1(left) and v1alpha1(right) for mainnet.
 // This mapping has been created to find a transaction hashes made with v1alpha1.
-// It is needed because logs produce with v1alpha1 have a different batch denom pattern from v1.
+// It is needed because logs produced with v1alpha1 have a different batch denom pattern from v1.
 // Also a v1alpha1 batch denom can't be deduced from a v1 one, hence the static mapping.
 export const v1Alpha1BatchDenomMapping: Record<string, string> = {
   'C01-002-20190101-20191231-001': 'C01-20190101-20191231-001',

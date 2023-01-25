0a311c0e33ccc042f6d60de9f248d61174b47a6f
Florent Lagrede <florent.lagrede@gmail.com>
2023-01-25 10:30:48 +0100

    feat: add support for tco2 metadata (#1745)
    * chore: extract styles
    
    * feat: add tokenizationSource field
    
    (cherry picked from commit 737f69077000e90166992d48549a2ef68e20decd)

---
+++ web-registry/src/pages/CreditClassDetails/CreditClassDetailsSimple/CreditClassDetailsSimple.styles.ts
@@ -0,0 +1,71 @@
+import { makeStyles } from 'tss-react/mui';
+
+import type { Theme } from 'web-components/lib/theme/muiTheme';
+
+export const useCreditClassDetailsSimpleStyles = makeStyles()(
+  (theme: Theme) => ({
+    sectionPadding: {
+      [theme.breakpoints.up('sm')]: {
+        paddingTop: theme.spacing(30),
+        paddingBottom: theme.spacing(30),
+      },
+      [theme.breakpoints.down('sm')]: {
+        paddingTop: theme.spacing(20),
+        paddingBottom: theme.spacing(20),
+      },
+    },
+    topSection: {
+      [theme.breakpoints.up('sm')]: {
+        paddingBottom: theme.spacing(12),
+      },
+      [theme.breakpoints.down('sm')]: {
+        paddingBottom: theme.spacing(20),
+      },
+    },
+    label: {
+      fontSize: theme.typography.pxToRem(12),
+      color: theme.palette.primary.contrastText,
+      marginBottom: theme.spacing(3),
+    },
+    link: {
+      color: theme.palette.secondary.main,
+      fontWeight: 700,
+    },
+    description: {
+      [theme.breakpoints.up('sm')]: {
+        fontSize: theme.typography.pxToRem(22),
+      },
+      [theme.breakpoints.down('sm')]: {
+        fontSize: theme.typography.pxToRem(18),
+      },
+    },
+    marginBottom: {
+      [theme.breakpoints.up('sm')]: {
+        marginBottom: theme.spacing(12),
+      },
+      [theme.breakpoints.down('sm')]: {
+        marginBottom: theme.spacing(8.75),
+      },
+    },
+    sidebarItemMargin: {
+      [theme.breakpoints.up('sm')]: {
+        marginBottom: theme.spacing(8.75),
+      },
+      [theme.breakpoints.down('sm')]: {
+        marginBottom: theme.spacing(8),
+      },
+      '&:last-child': {
+        marginBottom: 0,
+      },
+    },
+    arrow: {
+      fontSize: theme.typography.pxToRem(16),
+    },
+    title: {
+      textAlign: 'left',
+    },
+    textContainer: {
+      paddingTop: 0,
+    },
+  }),
+);

f993a6b20d91865497d73bc0656a1bb01570422d
Florent <florent.lagrede@gmail.com>
2023-01-25 09:06:16 +0100

    chore: let typescript infer return type

--- web-registry/src/utils/mui/sxToArray.ts
+++ web-registry/src/utils/mui/sxToArray.ts
@@ -1,6 +1,6 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { SxProps, Theme } from '@mui/material';
 
 type SxPropsType = SxProps<Theme>;
 
-export const sxToArray = (sx?: SxPropsType): any =>
-  Array.isArray(sx) ? sx : [sx];
+export const sxToArray = (sx?: SxPropsType) => (Array.isArray(sx) ? sx : [sx]);

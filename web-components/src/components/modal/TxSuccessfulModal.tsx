aa6e2023cb88fbc0da5ecb22c4bf066c23b3ea6a
Florent Lagrede <florent.lagrede@gmail.com>
2023-01-26 11:43:25 +0100

    feat: use new icon in success modals (#1754)

--- web-components/src/components/modal/TxSuccessfulModal.tsx
+++ web-components/src/components/modal/TxSuccessfulModal.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import CelebrateRegenIcon from '../icons/CelebrateRegenIcon';
 import { TxModal, TxModalProps } from './TxModal';
 
 export interface TxSuccessfulModalProps extends TxModalProps {
@@ -10,6 +11,7 @@ const TxSuccessfulModal: React.FC<TxSuccessfulModalProps> = props => {
   return (
     <TxModal
       {...props}
+      icon={<CelebrateRegenIcon sx={{ fontSize: 150, height: 100, mb: 5 }} />}
       title={props?.title ?? 'Congrats! Your transaction was successful.'}
     />
   );

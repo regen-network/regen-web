f884e24ee493bc14b65053074a20a12758a55297
blushi <marie.gauthier63@gmail.com>
2023-01-30 12:30:48 +0100

    feat: update useProjectsWithOrders to display unanchored metadata (preview photo)

--- web-registry/src/lib/normalizers/projects/normalizeProjectsWithMetadata.ts
+++ web-registry/src/lib/normalizers/projects/normalizeProjectsWithMetadata.ts
@@ -5,21 +5,26 @@ import DefaultProject from 'assets/default-project.jpg';
 interface NormalizeProjectsWithOrderDataParams {
   projectsWithOrderData?: ProjectWithOrderData[];
   metadatas?: any[];
+  unanchoredMetadatas?: any[];
 }
 
 export const normalizeProjectsWithMetadata = ({
   projectsWithOrderData,
   metadatas,
+  unanchoredMetadatas,
 }: NormalizeProjectsWithOrderDataParams): ProjectWithOrderData[] => {
   const projectsWithMetadata = projectsWithOrderData?.map(
     (project: ProjectWithOrderData, index) => {
       const metadata: any = metadatas?.[index];
+      const unanchoredMetadata: any = unanchoredMetadatas?.[index];
 
       return {
         ...project,
         id: project.id,
         name: metadata?.['schema:name'] || project.name,
-        imgSrc: metadata?.['regen:previewPhoto']?.['@value'] || DefaultProject,
+        imgSrc:
+          unanchoredMetadata?.['regen:previewPhoto']?.['@value'] ||
+          DefaultProject,
         place: metadata?.['schema:location']?.place_name || project.place,
         area: metadata?.['regen:projectSize']?.['qudt:numericValue']?.[
           '@value'

2fc6140c5305a2a6b0dd3e2c29d6a90224b16877
Florent <florent.lagrede@gmail.com>
2023-02-13 18:11:28 +0100

    chore: remove v4 variable

--- web-registry/src/lib/ledger.ts
+++ web-registry/src/lib/ledger.ts
@@ -4,12 +4,7 @@ export const chainId = process.env.REACT_APP_LEDGER_CHAIN_ID;
 export const nctBasket = process.env.REACT_APP_NCT_BASKET === 'true';
 export const isBridgeEnabled = process.env.REACT_APP_BRIDGE === 'true';
 
-// v4Ledger is a feature flag indicating the app is connected to a testnet with Regen Ledger v4.0.
-// It can be removed once redwood/mainnet upgrades to v4.0.
-export const v4Ledger = process.env.REACT_APP_V4_LEDGER;
-
-const v4UriBase = 'v4-';
-const uriBase = `${getApiUri()}/${v4Ledger ? v4UriBase : ''}`;
+const uriBase = `${getApiUri()}/}`;
 
 // Simple proxy endpoint for REST requests.
 export const ledgerRESTUri = `${uriBase}ledger-rest`;

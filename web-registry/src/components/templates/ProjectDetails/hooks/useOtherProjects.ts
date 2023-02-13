1a9459eebebe927e8948b49c2aff777dce4effc7
Florent <florent.lagrede@gmail.com>
2023-02-13 12:29:36 +0100

    chore: remove disable eslint comments

--- web-registry/src/components/templates/ProjectDetails/hooks/useOtherProjects.ts
+++ web-registry/src/components/templates/ProjectDetails/hooks/useOtherProjects.ts
@@ -2,7 +2,6 @@ import { useMoreProjectsQuery } from '../../../../generated/graphql';
 
 // TODO - just offchain projects, missing onchain
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 export default function useOtherProjects(projectId: string) {
   const { data: projectsData } = useMoreProjectsQuery();
   const allProjects = projectsData?.allProjects?.nodes;

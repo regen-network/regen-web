5a59b97849ca8cf6f09e6c1fd7a11ce9637eab5d
Florent <florent.lagrede@gmail.com>
2023-02-14 14:18:08 +0100

    fix: base uri

--- web-registry/src/lib/ledger.ts
+++ web-registry/src/lib/ledger.ts
@@ -4,7 +4,7 @@ export const chainId = process.env.REACT_APP_LEDGER_CHAIN_ID;
 export const nctBasket = process.env.REACT_APP_NCT_BASKET === 'true';
 export const isBridgeEnabled = process.env.REACT_APP_BRIDGE === 'true';
 
-const uriBase = `${getApiUri()}/}`;
+const uriBase = `${getApiUri()}/`;
 
 // Simple proxy endpoint for REST requests.
 export const ledgerRESTUri = `${uriBase}ledger-rest`;

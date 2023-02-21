525198c11392db3422ccdf5517d3de9ac4f6eb11
Florent <florent.lagrede@gmail.com>
2023-02-21 14:14:05 +0100

    feat: add responsive carousel config

--- web-components/src/components/organisms/CarouselSection/CarouselSection.config.ts
+++ web-components/src/components/organisms/CarouselSection/CarouselSection.config.ts
@@ -8,4 +8,28 @@ export const carrouselDefaultSettings: Settings = {
   speed: 2000,
   autoplaySpeed: 2000,
   cssEase: 'linear',
+  responsive: [
+    {
+      breakpoint: 1280,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      },
+    },
+    {
+      breakpoint: 1064,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 };

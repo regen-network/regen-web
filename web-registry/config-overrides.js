b90ab9919f91a7e964142a10d0b9e126100c72ac
blushi <marie.gauthier63@gmail.com>
2023-02-21 09:57:10 +0100

    test: use react-app-rewired test

--- web-registry/config-overrides.js
+++ web-registry/config-overrides.js
@@ -1,8 +1,28 @@
+const webpack = require('webpack');
+
 module.exports = function override(webpackConfig) {
   webpackConfig.resolve.alias = {
     ...webpackConfig.resolve.alias,
     '@ledgerhq/devices/hid-framing': '@ledgerhq/devices/lib/hid-framing',
   };
+
+  webpackConfig.resolve.fallback = {
+    ...webpackConfig.resolve.fallback,
+    stream: require.resolve('stream-browserify'),
+    buffer: require.resolve('buffer'),
+  };
+  webpackConfig.resolve.extensions = [
+    ...webpackConfig.resolve.extensions,
+    '.ts',
+    '.js',
+  ];
+  webpackConfig.plugins = [
+    ...webpackConfig.plugins,
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ];
 
   return webpackConfig;
 };

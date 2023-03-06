2ec1e52bc105f7ee94dabd61274b9d6fb6b9c6ae
Florent <florent.lagrede@gmail.com>
2023-03-06 11:09:16 +0100

    styles: enhance mobile styles

--- web-registry/src/pages/ProjectEdit/ProjectEdit.Denied.tsx
+++ web-registry/src/pages/ProjectEdit/ProjectEdit.Denied.tsx
@@ -22,32 +22,38 @@ export const ProjectEditDenied = ({ address, projectId }: Props) => {
         mt: 17.5,
         maxWidth: 560,
         mx: 'auto',
+        px: 3.75,
         '& a': {
           color: 'secondary.main',
           fontWeight: 700,
-          fontSize: 22,
+          fontSize: { xs: 18, sm: 22 },
         },
       }}
     >
-      <Title as="h1" variant="h3" sx={{ mb: 7.5 }}>
+      <Title as="h1" variant="h3" sx={{ mb: { xs: 10, sm: 7.5 } }}>
         {'Edit Project - Access Denied'}
       </Title>
       <Subtitle
         size="sm"
         sx={{
-          mb: 3.375,
+          mb: { xs: 4, sm: 3.375 },
           textTransform: 'uppercase',
           color: 'info.dark',
           fontWeight: 800,
           fontFamily: headerFontFamily,
           letterSpacing: 1,
+          textAlign: 'center',
         }}
       >
         {'To edit you must have access to the following address: '}
       </Subtitle>
       <Link
         href={`/ecocredits/accounts/${address}/portfolio`}
-        sx={{ mb: 4.375 }}
+        sx={{
+          mb: { xs: 4, sm: 4.375 },
+          wordBreak: 'break-word',
+          textAlign: 'center',
+        }}
       >
         {address}
       </Link>
@@ -59,7 +65,7 @@ export const ProjectEditDenied = ({ address, projectId }: Props) => {
       <Subtitle
         size="sm"
         sx={{
-          mb: 3.375,
+          mb: { xs: 4, sm: 3.375 },
           textTransform: 'uppercase',
           color: 'info.dark',
           fontWeight: 800,

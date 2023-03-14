260a33b3d3792252086f032853a8d82f473f8585
Florent <florent.lagrede@gmail.com>
2023-03-14 16:51:41 +0100

    feat: add RadioCard component

---
+++ web-components/src/components/atoms/RadioCard/RadioCard.mock.tsx
@@ -0,0 +1,18 @@
+import OrganizationIcon from '../../../components/icons/OrganizationIcon';
+import UserIcon from '../../../components/icons/UserIcon';
+import { RadioCardItem } from './RadioCard.types';
+
+export const radioCardItemsMock: RadioCardItem[] = [
+  {
+    id: 'individual',
+    label: 'Individual',
+    value: 'individual',
+    icon: <UserIcon />,
+  },
+  {
+    id: 'organization',
+    label: 'Organization',
+    value: 'organization',
+    icon: <OrganizationIcon />,
+  },
+];

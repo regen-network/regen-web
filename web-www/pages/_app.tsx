681112ab248731d3282fee022aa197994b447664
Florent <florent.lagrede@gmail.com>
2023-04-06 16:02:09 +0200

    feat: home migration part 4

--- web-www/pages/_app.tsx
+++ web-www/pages/_app.tsx
@@ -3,22 +3,29 @@ import type { AppProps } from 'next/app';
 
 import theme from 'web-components/lib/theme/muiTheme';
 
-import { createEmotionSsrAdvancedApproach } from 'tss-react/next/pagesDir';
-
 import '../styles/font.css';
 import '../styles/web-components.css';
+import createEmotionCache from '@/lib/utils/createEmotionCache';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import Head from 'next/head';
 
-const { augmentDocumentWithEmotionCache, withAppEmotionCache } =
-  createEmotionSsrAdvancedApproach({ key: 'css' });
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
 
-export { augmentDocumentWithEmotionCache };
+export interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache;
+}
 
-const App = ({ Component, pageProps }: AppProps) => {
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
   );
-};
-
-export default withAppEmotionCache(App);
+}

79ea2a30efcf0dc6cd05b27e8b94e6dfd447e6ab
Kyle Lawlor-Bagcal <wgwz@users.noreply.github.com>
2023-04-13 16:57:58 -0400

    chore: use usdc as from denom for buy button link
    Co-authored-by: Cory <cjlevinson@gmail.com>

--- web-registry/src/components/organisms/BasketDetailsActionsBar/BasketDetailsActionsBar.constants.ts
+++ web-registry/src/components/organisms/BasketDetailsActionsBar/BasketDetailsActionsBar.constants.ts
@@ -1,4 +1,4 @@
 export const BUY_BASKET_TOKENS_TOOLTIP =
   'NCT represents the first IBC-compatible carbon token to the Interchain economy, and launches via NCT: Regen and NCT: Osmo pools on the Osmosis exchange.';
 export const BUY_BASKET_TOKENS = 'buy NCT';
-export const BUY_BUTTON_LINK = 'https://app.osmosis.zone/?from=REGEN&to=NCT';
+export const BUY_BUTTON_LINK = 'https://app.osmosis.zone/?from=USDC&to=NCT';

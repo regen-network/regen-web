8c1eb8d55266411cd70de22b6f545b5c6b2469d8
Daniel Pittman <dpdanpittman@gmail.com>
2023-04-13 10:15:01 -0600

    updated buy_button_link to Osmosis Pool

--- web-registry/src/components/organisms/BasketDetailsActionsBar/BasketDetailsActionsBar.constants.ts
+++ web-registry/src/components/organisms/BasketDetailsActionsBar/BasketDetailsActionsBar.constants.ts
@@ -2,4 +2,4 @@ export const BUY_BASKET_TOKENS_TOOLTIP =
   'NCT represents the first IBC-compatible carbon token to the Interchain economy, and launches via NCT: Regen and NCT: Osmo pools on the Osmosis exchange.';
 export const BUY_BASKET_TOKENS = 'buy NCT';
 export const BUY_BUTTON_LINK =
-  'https://guides.regen.network/guides/regen-marketplace/bridging/polygon-to-regen-ledger';
+  'https://app.osmosis.zone/?from=REGEN&to=NCT';

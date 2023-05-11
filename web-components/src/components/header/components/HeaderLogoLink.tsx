fdd7528865ea8268cec338358e4317b97dc11aa0
Florent <florent.lagrede@gmail.com>
2023-05-11 11:29:18 +0200

    fix: remove safari hack

--- web-components/src/components/header/components/HeaderLogoLink.tsx
+++ web-components/src/components/header/components/HeaderLogoLink.tsx
@@ -1,18 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { makeStyles } from 'tss-react/mui';
 
 import RegenIcon from '../../icons/RegenIcon';
 
-type UseStylesParams = {
-  isLoaded: boolean;
-};
-
-const useStyles = makeStyles<UseStylesParams>()((theme, { isLoaded }) => {
+const useStyles = makeStyles()(theme => {
   const { pxToRem } = theme.typography;
 
   return {
     icon: {
-      display: isLoaded ? 'block' : 'inline-block',
+      display: 'block',
       height: 'auto',
       width: pxToRem(186),
       [theme.breakpoints.down('md')]: {
@@ -28,21 +24,7 @@ const useStyles = makeStyles<UseStylesParams>()((theme, { isLoaded }) => {
 export const HeaderLogoLink: React.FC<
   React.PropsWithChildren<{ color: string }>
 > = ({ color }) => {
-  const [isLoaded, setIsLoaded] = useState(false);
-  const { classes: styles } = useStyles({ isLoaded });
-
-  // TODO: this is a hack to make the SVG render properly in safari - it's not a
-  // perfect solution but should work until we can dedicate time to find
-  // something more long-term. You can see the bug by visiting the /community
-  // page in safari
-  useEffect(() => {
-    if (
-      typeof window !== 'undefined' &&
-      window.location &&
-      window.location.href
-    )
-      setIsLoaded(i => !i);
-  }, []);
+  const { classes: styles } = useStyles();
 
   return (
     <a href="/">

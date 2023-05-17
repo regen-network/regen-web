a175947bd4b9d4186b50003c3fa060d9b73f8f3b
blushi <marie.gauthier63@gmail.com>
2023-05-17 16:40:52 +0200

    chore: rm media query check on credibility card

--- web-components/src/components/cards/CredibilityCard/CredibilityCard.tsx
+++ web-components/src/components/cards/CredibilityCard/CredibilityCard.tsx
@@ -1,10 +1,11 @@
-import { Box, useMediaQuery, useTheme } from '@mui/material';
+import { Box } from '@mui/material';
 
 import { BlockContent } from '../../block-content';
 import OutlinedCheckIcon from '../../icons/OutlinedCheckIcon';
 import { Body, Title } from '../../typography';
 import Card from '../Card';
 import { CredibilityCardProps } from './CredibilityCard.types';
+import { getBackground } from './CredibilityCard.utils';
 
 export const CredibilityCard: React.FC<CredibilityCardProps> = ({
   index = 0,
@@ -12,51 +13,49 @@ export const CredibilityCard: React.FC<CredibilityCardProps> = ({
   descriptionRaw,
   icon,
   claims,
-}) => {
-  const theme = useTheme();
-  const lg = useMediaQuery(theme.breakpoints.up('lg'));
-  const background = `${Math.min(100, 55 + index * (lg ? 12 : 8))}%`;
-  return (
-    <Card
-      borderColor="grey.100"
-      sx={{
-        pt: [7.25, 10],
-        px: [4.75, 3.75],
-        pb: [1.25, 3],
-        background: `linear-gradient(-8deg, #F1F7F6 ${background}, transparent ${background})`,
-      }}
-    >
-      {icon && (
-        <Box
-          component="img"
-          alt={title}
-          src={icon}
-          height={[100, 126]}
-          sx={{ float: 'right', mr: [1.75, 4.5] }}
-        />
-      )}
-      <Title variant="h4" pt={[7.75, 8.23]} sx={{ clear: 'right' }}>
-        {title}
-      </Title>
-      <Body size="lg" sx={{ pt: [2.5], pb: [7.5, 12.5] }}>
-        <BlockContent content={descriptionRaw} />
-      </Body>
-      <Box>
-        {claims.map(claim => (
-          <Body
-            size="md"
-            mobileSize="xs"
-            color="primary.light"
-            fontWeight={[700]}
-            pb={4}
-            display="flex"
-            alignItems="center"
-          >
-            <OutlinedCheckIcon sx={{ pr: 1.25 }} />
-            {claim.description}
-          </Body>
-        ))}
-      </Box>
-    </Card>
-  );
-};
+}) => (
+  <Card
+    borderColor="grey.100"
+    sx={{
+      pt: [7.25, 10],
+      px: [4.75, 3.75],
+      pb: [1.25, 3],
+      background: {
+        xs: getBackground(index, 8),
+        lg: getBackground(index, 12),
+      },
+    }}
+  >
+    {icon && (
+      <Box
+        component="img"
+        alt={title}
+        src={icon}
+        height={[100, 126]}
+        sx={{ float: 'right', mr: [1.75, 4.5] }}
+      />
+    )}
+    <Title variant="h4" pt={[7.75, 8.23]} sx={{ clear: 'right' }}>
+      {title}
+    </Title>
+    <Body size="lg" sx={{ pt: [2.5], pb: [7.5, 12.5] }}>
+      <BlockContent content={descriptionRaw} />
+    </Body>
+    <Box>
+      {claims.map(claim => (
+        <Body
+          size="md"
+          mobileSize="xs"
+          color="primary.light"
+          fontWeight={[700]}
+          pb={4}
+          display="flex"
+          alignItems="center"
+        >
+          <OutlinedCheckIcon sx={{ pr: 1.25 }} />
+          {claim.description}
+        </Body>
+      ))}
+    </Box>
+  </Card>
+);

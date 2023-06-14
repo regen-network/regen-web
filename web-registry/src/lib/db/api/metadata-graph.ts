fe952f1e1e140114c18ea7f71ae0830b45ed6a13
blushi <marie.gauthier63@gmail.com>
2023-06-14 13:12:36 +0200

    feat: update getMetadata to fetch from data module resolvers

--- web-registry/src/lib/db/api/metadata-graph.ts
+++ web-registry/src/lib/db/api/metadata-graph.ts
@@ -1,3 +1,4 @@
+import { QueryClientImpl } from '@regen-network/api/lib/generated/regen/data/v1/query';
 import axios from 'axios';
 
 import { apiUri } from 'lib/apiUri';
@@ -6,14 +7,42 @@ import { jsonLdCompact } from 'lib/rdf';
 export const getMetadata = async (
   iri?: string,
   context?: object,
+  client?: QueryClientImpl,
 ): Promise<any> => {
   if (!iri) return null;
+
+  // First try to get metadata from registry-server
+  let res;
   try {
-    const { data } = await axios.get(`${apiUri}/metadata-graph/${iri}`);
-    return await jsonLdCompact(data, context);
+    res = await fetch(`${apiUri}/metadata-graph/${iri}`);
+    if (res.ok) {
+      const data = await res.json();
+      return await jsonLdCompact(data, context);
+    }
   } catch (err) {
     return null;
   }
+
+  // Fallback to data module resolvers if metadata can't be found on registry-server
+  if (res.status === 404 && client) {
+    const resolversRes = await client.ResolversByIRI({ iri });
+    for (let i = 0; i < resolversRes.resolvers.length; i++) {
+      const resolverUrl = resolversRes.resolvers[i].url;
+      try {
+        const resolverRes = await fetch(`${resolverUrl}/${iri}`);
+        if (resolverRes.ok) {
+          const data = await resolverRes.json();
+          return await jsonLdCompact(data, context);
+        }
+      } catch (e) {
+        // We've tried with the last resolver so we can return
+        if (i === resolversRes.resolvers.length - 1) {
+          return null;
+        }
+      }
+    }
+  }
+  return null;
 };
 
 /**

d7647f0506675d27b808f625bff6dedd87430005
Florent Lagrede <florent.lagrede@gmail.com>
2023-06-26 17:39:05 +0200

    feat: move additional info section to table (#1937)
    * feat: move additional info section to table
    
    * feat: remove offsetGeneration and projectActivity from metadata tab
    
    OffsetGeneration is now displayed in the credit class card and projectActivity in ProjectTopCard as a tag.
    
    * feat: remove offset protocol from project metadata
    
    * fix: lint
    
    * feat: remove toucan approved methodologies

---
+++ web-registry/src/components/molecules/ProjectPageMetadata/ProjectPageMetadata.constants.ts
@@ -0,0 +1,5 @@
+export const PROJECT_PAGE_METADATA_HELPER_TEXT =
+  'This data is onchain or anchored onchain via a content hash.';
+
+export const PROJECT_PAGE_METADATA_ID_TOOLTIP =
+  'Project ID: unique identifier of the project on Regen Ledger.';
--- web-registry/src/components/molecules/ProjectPageMetadata/ProjectPageMetadata.tsx
+++ web-registry/src/components/molecules/ProjectPageMetadata/ProjectPageMetadata.tsx
@@ -1,34 +1,33 @@
-import React, { useState } from 'react';
-import { Box, Collapse, Grid, useTheme } from '@mui/material';
+import React from 'react';
+import { Box, Grid } from '@mui/material';
 
-import { ExpandButton } from 'web-components/lib/components/buttons/ExpandButton';
-import { Body, Title } from 'web-components/lib/components/typography';
+import InfoTooltipWithIcon from 'web-components/lib/components/tooltip/InfoTooltipWithIcon';
+import { Body, Label } from 'web-components/lib/components/typography';
 import { formatDate } from 'web-components/lib/utils/format';
-import { pluralize } from 'web-components/lib/utils/pluralize';
 
 import { AnchoredProjectMetadataLD } from 'lib/db/types/json-ld';
 
 import { ArrowLink } from '../../atoms/MetadataArrowLink';
 import { MetaDetail } from '../MetaDetail';
+import {
+  PROJECT_PAGE_METADATA_HELPER_TEXT,
+  PROJECT_PAGE_METADATA_ID_TOOLTIP,
+} from './ProjectPageMetadata.constants';
 
 interface MetadataProps {
   metadata?: AnchoredProjectMetadataLD;
+  onChainProjectId?: string;
 }
 
 const ProjectPageMetadata: React.FC<React.PropsWithChildren<MetadataProps>> = ({
   metadata,
+  onChainProjectId,
 }) => {
-  const theme = useTheme();
-  const [expanded, setExpanded] = useState(false);
-
   if (!metadata) return null;
 
   // Common
   const startDate = metadata?.['regen:projectStartDate'];
   const endDate = metadata?.['regen:projectEndDate'];
-  const offsetGenerationMethods = metadata?.['regen:offsetGenerationMethod'];
-  const methodsCount = offsetGenerationMethods?.length;
-  const projectActivity = metadata?.['regen:projectActivity'];
   const projectType = metadata?.['regen:projectType'];
 
   // VCS
@@ -38,155 +37,107 @@ const ProjectPageMetadata: React.FC<React.PropsWithChildren<MetadataProps>> = ({
   // CFC
   const cfcProjectId = metadata?.['regen:cfcProjectId'];
   const projectDesignDocument = metadata?.['regen:projectDesignDocument'];
-  const offsetProtocol = metadata?.['regen:offsetProtocol'];
 
   // Toucan
   const toucanProjectTokenId = metadata?.['regen:toucanProjectTokenId'];
-  const approvedMethodologies =
-    metadata?.['regen:approvedMethodologies']?.['schema:itemListElement'];
-  const methodologyCount = approvedMethodologies?.length;
 
   return (
-    <Box sx={{ pt: 8 }}>
-      <Title variant="h5">Additional Info</Title>
-      <Collapse collapsedSize={theme.spacing(24)} in={expanded}>
-        <Box sx={{ py: 8 }}>
-          <Grid container spacing={8}>
-            {methodology && (
-              <MetaDetail
-                label="methodology"
-                data={
-                  <ArrowLink
-                    label={methodology?.['schema:name']}
-                    href={methodology?.['schema:url'] || ''}
-                  />
-                }
+    <Box
+      sx={{
+        p: 8,
+        backgroundColor: 'primary.main',
+        border: '1px solid',
+        borderColor: 'info.light',
+        borderRadius: '0 0 8px 8px',
+      }}
+    >
+      <Body size="lg">{PROJECT_PAGE_METADATA_HELPER_TEXT}</Body>
+      <Box sx={{ pt: 7 }}>
+        <Grid container spacing={8}>
+          <Grid item xs={12} sm={6} sx={{ flexDirection: 'column' }}>
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <Label size="xs" sx={{ mr: 1 }}>
+                project id:
+              </Label>
+              <InfoTooltipWithIcon
+                title={PROJECT_PAGE_METADATA_ID_TOOLTIP}
+                outlined
               />
-            )}
-            {offsetGenerationMethods && methodsCount && methodsCount > 0 && (
-              <MetaDetail
-                label={pluralize(methodsCount, 'offset generation method')}
-                data={
-                  <>
-                    {offsetGenerationMethods.map((method, i) => (
-                      <Body key={i} size="xl">
-                        {method}
-                      </Body>
-                    ))}
-                  </>
-                }
-              />
-            )}
-            {projectActivity?.['schema:name'] && (
-              <MetaDetail
-                label="project activity"
-                data={
-                  <ArrowLink
-                    href={projectActivity?.['schema:url'] || ''}
-                    label={projectActivity?.['schema:name']}
-                  />
-                }
-              />
-            )}
-            {approvedMethodologies && methodologyCount && methodologyCount > 0 && (
-              <MetaDetail
-                label={`methodolog${methodologyCount > 1 ? 'ies' : 'y'}`}
-                data={
-                  <>
-                    {approvedMethodologies.map(methodology => (
-                      <ArrowLink
-                        label={methodology?.['schema:name']}
-                        href={methodology?.['schema:url'] || ''}
-                      />
-                    ))}
-                  </>
-                }
-              />
-            )}
-            {toucanProjectTokenId && (
-              <MetaDetail
-                label="toucan project token id"
-                data={
-                  <ArrowLink
-                    label={toucanProjectTokenId.toString()}
-                    href={metadata?.['regen:toucanURI'] || ''}
-                  />
-                }
-              />
-            )}
-            {vcsProjectId && (
-              <MetaDetail
-                label="vcs project id"
-                data={
-                  <ArrowLink
-                    label={vcsProjectId.toString()}
-                    href={metadata?.['regen:vcsProjectPage'] || ''}
-                  />
-                }
-              />
-            )}
-            {projectType && (
-              <MetaDetail
-                label={`${vcsProjectId ? 'vcs ' : ''}project type`}
-                data={projectType}
-              />
-            )}
-            {projectDesignDocument && (
-              <MetaDetail
-                label="documents"
-                data={
-                  <ArrowLink
-                    label="Project Design Document"
-                    href={projectDesignDocument}
-                  />
-                }
-              />
-            )}
-            {cfcProjectId && (
-              <MetaDetail
-                label="reference id (cfc project id)"
-                data={
-                  <ArrowLink
-                    label={cfcProjectId}
-                    href={metadata?.['regen:cfcProjectPage'] || ''}
-                  />
-                }
-              />
-            )}
-            {offsetProtocol && (
-              <MetaDetail
-                label="offset protocol"
-                data={
-                  <ArrowLink
-                    label={offsetProtocol?.['schema:name']}
-                    href={offsetProtocol?.['schema:url'] || ''}
-                  />
-                }
-              />
-            )}
-            {startDate && (
-              <MetaDetail
-                label="project start date"
-                data={formatDate(startDate)}
-              />
-            )}
-            {endDate && (
-              <MetaDetail label="project end date" data={formatDate(endDate)} />
-            )}
+            </Box>
+            <Body size="xl">{onChainProjectId}</Body>
           </Grid>
-        </Box>
-      </Collapse>
-      <ExpandButton
-        sx={{
-          mt: 2,
-          px: [0],
-          ':hover': { bgcolor: 'transparent !important' },
-        }}
-        onClick={() => setExpanded(!expanded)}
-        text="see"
-        size="small"
-        expanded={expanded}
-      />
+          {methodology && (
+            <MetaDetail
+              label="methodology"
+              data={
+                <ArrowLink
+                  label={methodology?.['schema:name']}
+                  href={methodology?.['schema:url'] || ''}
+                />
+              }
+            />
+          )}
+          {toucanProjectTokenId && (
+            <MetaDetail
+              label="toucan project token id"
+              data={
+                <ArrowLink
+                  label={toucanProjectTokenId.toString()}
+                  href={metadata?.['regen:toucanURI'] || ''}
+                />
+              }
+            />
+          )}
+          {vcsProjectId && (
+            <MetaDetail
+              label="vcs project id"
+              data={
+                <ArrowLink
+                  label={vcsProjectId.toString()}
+                  href={metadata?.['regen:vcsProjectPage'] || ''}
+                />
+              }
+            />
+          )}
+          {projectType && (
+            <MetaDetail
+              label={`${vcsProjectId ? 'vcs ' : ''}project type`}
+              data={projectType}
+            />
+          )}
+          {projectDesignDocument && (
+            <MetaDetail
+              label="documents"
+              data={
+                <ArrowLink
+                  label="Project Design Document"
+                  href={projectDesignDocument}
+                />
+              }
+            />
+          )}
+          {cfcProjectId && (
+            <MetaDetail
+              label="reference id (cfc project id)"
+              data={
+                <ArrowLink
+                  label={cfcProjectId}
+                  href={metadata?.['regen:cfcProjectPage'] || ''}
+                />
+              }
+            />
+          )}
+          {startDate && (
+            <MetaDetail
+              label="project start date"
+              data={formatDate(startDate)}
+            />
+          )}
+          {endDate && (
+            <MetaDetail label="project end date" data={formatDate(endDate)} />
+          )}
+        </Grid>
+      </Box>
     </Box>
   );
 };

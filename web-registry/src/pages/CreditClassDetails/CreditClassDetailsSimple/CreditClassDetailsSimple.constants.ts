608a92d4253a9b3aadc5be40ece47be7a9a680e2
Florent <florent.lagrede@gmail.com>
2023-07-11 17:36:15 +0200

    feat: add credit type, offset generation methods and tooltip

--- web-registry/src/pages/CreditClassDetails/CreditClassDetailsSimple/CreditClassDetailsSimple.constants.ts
+++ web-registry/src/pages/CreditClassDetails/CreditClassDetailsSimple/CreditClassDetailsSimple.constants.ts
@@ -1,1 +1,3 @@
 export const ELIGIBLE_ACTIVITIES = 'eligible activities';
+export const CREDIT_CLASS_TOOLTIP =
+  'The structure, procedures and requirements for project registration, the quantification, monitoring, reporting and verification (MRV), and issuance of credits related to a certain credit type.';

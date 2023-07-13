71a56be44b2062de8e2e47bcd0f836bac45264d7
blushi <marie.gauthier63@gmail.com>
2023-07-13 15:10:03 +0200

    fix: class details section layout

--- web-registry/src/components/organisms/DetailsSection/DetailsSection.tsx
+++ web-registry/src/components/organisms/DetailsSection/DetailsSection.tsx
@@ -35,102 +35,109 @@ export const DetailsSection: React.FC<
   const hasClassInfo = header && (methodology || creditClassDoc);
 
   return (
-    <Section visibleOverflow sx={{ root: { pb: [20, 21.25] } }}>
-      {(hasCredibilityCards || hasClassInfo) && (
-        <Grid
-          container
-          sx={{ pb: { xs: 7.5, sm: 12.5 } }}
-          wrap="nowrap"
-          flexDirection={{ xs: 'column', tablet: 'row' }}
-        >
-          <Grid item>
-            <Label size="sm" mobileSize="sm" color="info.main">
-              {header.label}
-            </Label>
-            <Title variant="h2" py={3}>
-              {header.title}
-            </Title>
-            <Body size="lg" mobileSize="md" maxWidth={718}>
-              <BlockContent content={header.descriptionRaw} />
-            </Body>
-            {hasClassInfo && (
-              <Grid container pt={5} pr={{ xs: 11.25, tablet: 0 }} spacing={3}>
-                {creditClassDoc && (
-                  <DetailsSectionButton
-                    href={creditClassDoc['schema:url']}
-                    label={VIEW_CREDIT_CLASS_DOC}
-                  />
-                )}
-                {methodology && (
-                  <DetailsSectionButton
-                    href={methodology['schema:url']}
-                    label={VIEW_METHODOLOGY}
-                  />
-                )}
-              </Grid>
-            )}
-          </Grid>
-          {credit?.creditImage &&
-            credit?.creditTypeImage &&
-            credit?.creditTypeUnit && (
-              <Grid item pt={{ xs: 7.5, tablet: 11.25 }}>
-                <Grid container justifyContent="space-evenly" wrap="nowrap">
-                  <DetailsSectionCredit
-                    src={credit.creditImage}
-                    label={CREDIT}
-                  />
-                  <Grid
-                    item
-                    sx={{
-                      background:
-                        'linear-gradient(206deg, #7D9AA2 0%, #9AD3BE 50%, #D1E2C7 100%)',
-                      '-webkit-background-clip': 'text',
-                      '-webkit-text-fill-color': 'transparent',
-                      '-moz-background-clip': 'text',
-                      '-moz-text-fill-color': 'transparent',
-                    }}
-                    pt={{ xs: 4, sm: 4.5 }}
-                    px={7.5}
-                    fontSize={40}
-                    fontFamily={headerFontFamily}
-                  >
-                    =
-                  </Grid>
-                  <DetailsSectionCredit
-                    src={credit.creditTypeImage}
-                    label={credit.creditTypeUnit}
-                  />
+    <div>
+      <Section visibleOverflow sx={{ root: { pb: [20, 21.25] } }}>
+        {(hasCredibilityCards || hasClassInfo) && (
+          <Grid
+            container
+            sx={{ pb: { xs: 7.5, sm: 12.5 } }}
+            wrap="nowrap"
+            flexDirection={{ xs: 'column', tablet: 'row' }}
+          >
+            <Grid item>
+              <Label size="sm" mobileSize="sm" color="info.main">
+                {header.label}
+              </Label>
+              <Title variant="h2" py={3}>
+                {header.title}
+              </Title>
+              <Body size="lg" mobileSize="md" maxWidth={718}>
+                <BlockContent content={header.descriptionRaw} />
+              </Body>
+              {hasClassInfo && (
+                <Grid
+                  container
+                  pt={5}
+                  pr={{ xs: 11.25, tablet: 0 }}
+                  spacing={3}
+                >
+                  {creditClassDoc && (
+                    <DetailsSectionButton
+                      href={creditClassDoc['schema:url']}
+                      label={VIEW_CREDIT_CLASS_DOC}
+                    />
+                  )}
+                  {methodology && (
+                    <DetailsSectionButton
+                      href={methodology['schema:url']}
+                      label={VIEW_METHODOLOGY}
+                    />
+                  )}
                 </Grid>
-              </Grid>
-            )}
-        </Grid>
-      )}
-      {children}
-      {hasCredibilityCards && (
-        <ResponsiveSlider
-          visibleOverflow
-          arrows
-          mobileItemWidth="90%"
-          itemWidth="85%"
-          infinite={false}
-          slidesToShow={isMobile ? 1 : 2}
-          classes={{ headerWrap: classes.headerWrap }}
-          padding={theme.spacing(2.5)}
-          items={credibilityCards.map((card, index) => (
-            <CredibilityCard
-              index={index}
-              title={card?.credibilityCard?.title as string}
-              descriptionRaw={card?.credibilityCard?.descriptionRaw}
-              icon={card?.credibilityCard?.icon?.asset?.url}
-              claims={
-                card?.claims?.map(claim => ({
-                  description: claim?.description as string,
-                })) || []
-              }
-            />
-          ))}
-        />
-      )}
-    </Section>
+              )}
+            </Grid>
+            {credit?.creditImage &&
+              credit?.creditTypeImage &&
+              credit?.creditTypeUnit && (
+                <Grid item pt={{ xs: 7.5, tablet: 11.25 }}>
+                  <Grid container justifyContent="space-evenly" wrap="nowrap">
+                    <DetailsSectionCredit
+                      src={credit.creditImage}
+                      label={CREDIT}
+                    />
+                    <Grid
+                      item
+                      sx={{
+                        background:
+                          'linear-gradient(206deg, #7D9AA2 0%, #9AD3BE 50%, #D1E2C7 100%)',
+                        '-webkit-background-clip': 'text',
+                        '-webkit-text-fill-color': 'transparent',
+                        '-moz-background-clip': 'text',
+                        '-moz-text-fill-color': 'transparent',
+                      }}
+                      pt={{ xs: 4, sm: 4.5 }}
+                      px={7.5}
+                      fontSize={40}
+                      fontFamily={headerFontFamily}
+                    >
+                      =
+                    </Grid>
+                    <DetailsSectionCredit
+                      src={credit.creditTypeImage}
+                      label={credit.creditTypeUnit}
+                    />
+                  </Grid>
+                </Grid>
+              )}
+          </Grid>
+        )}
+        {children}
+        {hasCredibilityCards && (
+          <ResponsiveSlider
+            visibleOverflow
+            arrows
+            mobileItemWidth="90%"
+            itemWidth="85%"
+            infinite={false}
+            slidesToShow={isMobile ? 1 : 2}
+            classes={{ headerWrap: classes.headerWrap }}
+            padding={theme.spacing(2.5)}
+            items={credibilityCards.map((card, index) => (
+              <CredibilityCard
+                index={index}
+                title={card?.credibilityCard?.title as string}
+                descriptionRaw={card?.credibilityCard?.descriptionRaw}
+                icon={card?.credibilityCard?.icon?.asset?.url}
+                claims={
+                  card?.claims?.map(claim => ({
+                    description: claim?.description as string,
+                  })) || []
+                }
+              />
+            ))}
+          />
+        )}
+      </Section>
+    </div>
   );
 };

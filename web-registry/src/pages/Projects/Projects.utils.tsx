f8365f946b21f85ca8e2ff5628b5aec13c515921
Florent <florent.lagrede@gmail.com>
2023-08-09 09:50:11 +0200

    feat: hide skipped credit class in filter

--- web-registry/src/pages/Projects/Projects.utils.tsx
+++ web-registry/src/pages/Projects/Projects.utils.tsx
@@ -5,6 +5,7 @@ import { ReactElement } from 'react';
 import { BlockContent } from 'web-components/lib/components/block-content';
 
 import { AllCreditClassQuery } from 'generated/sanity-graphql';
+import { SKIPPED_CLASS_ID } from 'lib/env';
 
 type GetCreditClassesMappingProps = {
   creditClassesData?: AllCreditClassQuery;
@@ -13,7 +14,11 @@ type GetCreditClassesMappingProps = {
 export const getCreditClassesMapping = ({
   creditClassesData,
 }: GetCreditClassesMappingProps) => {
-  const creditClassesMapping = creditClassesData?.allCreditClass.reduce(
+  const creditClassesFiltered = creditClassesData?.allCreditClass?.filter(
+    creditClass => creditClass.path !== SKIPPED_CLASS_ID,
+  );
+
+  const creditClassesMapping = creditClassesFiltered?.reduce(
     (acc, creditClass) => {
       acc[creditClass.path ?? ''] = (
         <BlockContent content={creditClass.nameRaw} />

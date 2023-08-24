bac0a0c003a0b55c7afa76647fe77e4d33a0d779
Florent Lagrede <florent.lagrede@gmail.com>
2023-08-24 09:42:31 +0200

    fix: spacing credit class + handle empty sections (#2028)
    * fix: hide projects section if no project
    
    * feat: add empty state for credit issuance tab
    
    * fix: adjust description spacing
    
    * feat: adjust credit class impacts and tags spacing
    
    * feat: consistent spacing and table border

--- web-marketplace/src/components/molecules/NoCredits.tsx
+++ web-marketplace/src/components/molecules/NoCredits.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Box } from '@mui/material';
+import { Box, SxProps } from '@mui/material';
+import { sxToArray } from 'utils/mui/sxToArray';
 
 import { StyledTableContainer } from 'web-components/lib/components/table';
 import { Title } from 'web-components/lib/components/typography';
+import { Theme } from 'web-components/lib/theme/muiTheme';
 
 import { ReactComponent as CloudData } from '../../assets/svgs/cloud-data.svg';
 
@@ -10,18 +12,22 @@ const NoCredits: React.FC<
   React.PropsWithChildren<{
     title: string;
     icon?: JSX.Element;
+    sx?: SxProps<Theme>;
   }>
-> = ({ title, icon }) => {
+> = ({ title, icon, sx = [] }) => {
   return (
     <StyledTableContainer>
       <Box
-        sx={{
-          minHeight: 230,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
+        sx={[
+          {
+            minHeight: 230,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          },
+          ...sxToArray(sx),
+        ]}
       >
         {icon ?? <CloudData />}
         <Title variant="h4" sx={{ mt: 5 }}>
--- web-marketplace/src/pages/CreditClassDetails/CreditClassDetails.Projects.tsx
+++ web-marketplace/src/pages/CreditClassDetails/CreditClassDetails.Projects.tsx
@@ -22,27 +22,35 @@ function ProjectsSection({ classId }: Props): JSX.Element {
     useState<ProjectWithOrderData | null>(null);
   const [isBuyFlowStarted, setIsBuyFlowStarted] = useState(false);
   const { isKeplrMobileWeb } = useWallet();
+  const hasProjects = projectsWithOrderData.length > 0;
 
   return (
-    <Box
-      className={cx('topo-background-alternate', isKeplrMobileWeb && 'dark')}
-      sx={{ pb: 9 }}
-    >
-      <ProjectCardsSection
-        title="Projects"
-        projects={projectsWithOrderData}
-        loading={loading}
-        onButtonClick={({ project }) => {
-          setSelectedProject(project);
-          setIsBuyFlowStarted(true);
-        }}
-      />
-      <BuySellOrderFlow
-        isFlowStarted={isBuyFlowStarted}
-        setIsFlowStarted={setIsBuyFlowStarted}
-        projects={selectedProject && [selectedProject]}
-      />
-    </Box>
+    <>
+      {hasProjects && (
+        <Box
+          className={cx(
+            'topo-background-alternate',
+            isKeplrMobileWeb && 'dark',
+          )}
+          sx={{ pb: 9 }}
+        >
+          <ProjectCardsSection
+            title="Projects"
+            projects={projectsWithOrderData}
+            loading={loading}
+            onButtonClick={({ project }) => {
+              setSelectedProject(project);
+              setIsBuyFlowStarted(true);
+            }}
+          />
+          <BuySellOrderFlow
+            isFlowStarted={isBuyFlowStarted}
+            setIsFlowStarted={setIsBuyFlowStarted}
+            projects={selectedProject && [selectedProject]}
+          />
+        </Box>
+      )}
+    </>
   );
 }
 

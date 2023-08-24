2ff749506db314c910e7732a75f80e142568dafc
Ryan Christoffersen <12519942+ryanchristo@users.noreply.github.com>
2023-08-24 08:16:48 -0700

    fix: unable to load project with initial metadata (#2043)
    * fix: unable to load project with initial metadata
    
    * add remaining prexixes

--- web-marketplace/src/lib/queries/react-query/registry-server/graphql/getProjectByIdQuery/getProjectByIdQuery.ts
+++ web-marketplace/src/lib/queries/react-query/registry-server/graphql/getProjectByIdQuery/getProjectByIdQuery.ts
@@ -29,13 +29,25 @@ export const getProjectByIdQuery = ({
       });
 
       if (projectById.data?.projectById?.metadata) {
-        projectById.data.projectById.metadata = await jsonLdCompact(
-          projectById.data.projectById.metadata,
-        );
+        projectById.data.projectById.metadata = await jsonLdCompact({
+          // NOTE: We set the context here to prevent a prefix confusion
+          // error when no context is provided and field values with the
+          // prefixes are present within the project metadata.
+          '@context': {
+            schema: 'http://schema.org/',
+            regen: 'https://schema.regen.network#',
+            qudt: 'http://qudt.org/schema/qudt/',
+            unit: 'http://qudt.org/vocab/unit/',
+            xsd: 'http://www.w3.org/2001/XMLSchema#',
+          },
+          ...projectById.data.projectById.metadata,
+        });
       }
 
       return projectById;
     } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(e);
       return null;
     }
   },

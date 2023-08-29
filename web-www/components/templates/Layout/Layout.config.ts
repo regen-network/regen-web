ea379f9d52f73dc31ea4ab3d5fbd7d8e63c75791
Florent <florent.lagrede@gmail.com>
2023-08-29 09:45:55 +0200

    fix: remove timezone in link

--- web-www/components/templates/Layout/Layout.config.ts
+++ web-www/components/templates/Layout/Layout.config.ts
@@ -100,7 +100,7 @@ export const footerItems: [FooterItem, FooterItem, FooterItem, FooterItem] = [
       },
       {
         title: 'Community Calendar',
-        href: 'https://calendar.google.com/calendar/u/0/embed?src=c_1250640b0093c1453ac648937f168c97e48175e7862fb02d38f45e0639df2b25@group.calendar.google.com&ctz=America/New_York',
+        href: 'https://calendar.google.com/calendar/u/0/embed?src=c_1250640b0093c1453ac648937f168c97e48175e7862fb02d38f45e0639df2b25@group.calendar.google.com',
         target: '_blank',
       },
     ],

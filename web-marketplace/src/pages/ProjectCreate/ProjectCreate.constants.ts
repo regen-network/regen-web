acc7cfc81748690c155d1ff06584c1c845941581
Florent <florent.lagrede@gmail.com>
2023-09-06 15:33:17 +0200

    feat: add percentage on project creation

---
+++ web-marketplace/src/pages/ProjectCreate/ProjectCreate.constants.ts
@@ -0,0 +1,11 @@
+export const PROJECT_CREATION_STEPS = [
+  'basic-info',
+  'location',
+  'roles',
+  'description',
+  'media',
+  'metadata',
+  'review',
+];
+
+export const PROJECT_STEPS_COUNT = PROJECT_CREATION_STEPS.length;

21a40f9ee91c18e206cdf3405ab132cd9c4a6d90
Florent Lagrede <florent.lagrede@gmail.com>
2023-09-27 11:08:06 +0200

    chore: remove old fetching hooks (#2137)

--- web-marketplace/src/hooks/index.ts
+++ web-marketplace/src/hooks/index.ts
@@ -1,16 +1,7 @@
-export { default as useBankQuery } from './useBankQuery';
-export { default as useBasketQuery } from './useBasketQuery';
-export { default as useBasketsWithClasses } from './useBasketsWithClasses';
 export { default as useBasketTokens } from './useBasketTokens';
 export { default as useEcocreditQuery } from './useEcocreditQuery';
 export { default as useEcocredits } from './useEcocredits';
 export { default as useLocalStorage } from './useLocalStorage';
 export { default as useMsgClient } from './useMsgClient';
 export { default as useQueryBalance } from './useQueryBalance';
-export { default as useQueryBasket } from './useQueryBasket';
-export { default as useQueryBaskets } from './useQueryBaskets';
 export { default as useQueryDenomMetadata } from './useQueryDenomMetadata';
-export { default as useQueryListBatchInfo } from './useQueryListBatchInfo';
-export { default as useQueryListClasses } from './useQueryListClasses';
-export { default as useQueryListClassesWithMetadata } from './useQueryListClassesWithMetadata';
-export { default as useQueryListClassInfo } from './useQueryListClassInfo';

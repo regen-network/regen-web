4beeac8abfd9ae842a31f5242caf7555efd4d079
Aaron Craelius <aaronc@users.noreply.github.com>
2023-10-04 13:01:26 -0400

    add basic content

--- web-marketplace/src/components/organisms/UserAccountSettings/UserAccountSettings.tsx
+++ web-marketplace/src/components/organisms/UserAccountSettings/UserAccountSettings.tsx
@@ -1,9 +1,23 @@
 import {Title, Subtitle, Body, Label} from 'web-components/lib/components/typography';
 import ContainedButton from "web-components/lib/components/buttons/ContainedButton";
+// import {TextField} from "web-components/lib/components/new/TextField";
 
 export const UserAccountSettings = () => {
   return <div>
-    <ConnectField />
+    <div>
+      <Subtitle>Login Email</Subtitle>
+      {/*<TextField />*/}
+    </div>
+    <div>
+      <Subtitle>Social Accounts</Subtitle>
+      <div>Use your social account to log in to Regen Marketplace. <div>Learn more</div></div>
+      <ConnectField provider="Google" connected={false} />
+    </div>
+    <div>
+      <Subtitle>Wallet integration</Subtitle>
+      <div>Connect a wallet address to be able to perform on chain tasks such as buying and selling credits, and creating projects on Regen Ledger. <div>Learn more</div></div>
+      <ConnectField provider="Keplr or Wallet Connect" connected={false} />
+    </div>
   </div>
   // typography
   // contained button
@@ -11,9 +25,17 @@ export const UserAccountSettings = () => {
   // components/new/TextField
 }
 
-const ConnectField = () => {
+interface ConnectFieldProps {
+  provider: string
+  connected: boolean
+}
+
+const ConnectField = ({provider, connected}: ConnectFieldProps) => {
   return <div className="flex flex-row">
-    <div>Google</div>
+    <div>
+      <div>{provider}</div>
+      <div>{connected ? 'Connected' : 'Not connected'}</div>
+    </div>
     <ContainedButton>Connect</ContainedButton>
   </div>
 }

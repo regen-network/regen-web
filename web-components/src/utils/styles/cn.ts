52ce3b76d43379c1d034d064e7c229dab80ad0b1
Florent <florent.lagrede@gmail.com>
2023-10-16 09:57:07 +0200

    doc: add documentation to cn util function

--- web-components/src/utils/styles/cn.ts
+++ web-components/src/utils/styles/cn.ts
@@ -1,6 +1,13 @@
 import { ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
+/**
+ * Merges class names and applies Tailwind CSS styles.
+ * clsx(...inputs) allows to use of various syntaxes to apply style (object, array, condition...).
+ * twMerge(...) ensures that the classes applied at the end will override previous ones in case of conflict.
+ * @param inputs - The class names to merge.
+ * @returns The merged class names with Tailwind CSS styles applied.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(...inputs));
 }

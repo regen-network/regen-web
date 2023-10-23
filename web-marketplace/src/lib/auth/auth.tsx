526a40a8e3e897ec14071970dbb1025a49453fda
blushi <marie.gauthier63@gmail.com>
2023-10-23 16:26:49 +0200

    chore: add AuthProvider skeleton

---
+++ web-marketplace/src/lib/auth/auth.tsx
@@ -0,0 +1,16 @@
+import { createContext, useContext } from 'react';
+
+export type AuthContextType = {
+  error?: unknown;
+  accountId?: string;
+};
+
+const AuthContext = createContext<AuthContextType>({});
+
+export const AuthProvider: React.FC<React.PropsWithChildren<unknown>> = ({
+  children,
+}) => {
+  return <AuthContext.Provider value={{}}>{children}</AuthContext.Provider>;
+};
+
+export const useAuth = (): AuthContextType => useContext(AuthContext);

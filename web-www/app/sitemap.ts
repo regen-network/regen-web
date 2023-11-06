3f0e533a107fcf3d106a5090e38b282beec80fef
Florent <florent.lagrede@gmail.com>
2023-11-06 14:41:10 +0100

    chore: update frequencies to daily

--- web-www/app/sitemap.ts
+++ web-www/app/sitemap.ts
@@ -4,47 +4,47 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: 'https://www.regen.network/',
-      changeFrequency: 'yearly',
+      changeFrequency: 'daily',
       priority: 1,
     },
     {
       url: 'https://www.regen.network/buyers',
-      changeFrequency: 'monthly',
+      changeFrequency: 'daily',
       priority: 0.9,
     },
     {
       url: 'https://www.regen.network/media',
-      changeFrequency: 'monthly',
+      changeFrequency: 'daily',
       priority: 0.8,
     },
     {
       url: 'https://www.regen.network/resources',
-      changeFrequency: 'monthly',
+      changeFrequency: 'daily',
       priority: 0.8,
     },
     {
       url: 'https://www.regen.network/faq',
-      changeFrequency: 'yearly',
+      changeFrequency: 'daily',
       priority: 0.7,
     },
     {
       url: 'https://www.regen.network/contact',
-      changeFrequency: 'yearly',
+      changeFrequency: 'daily',
       priority: 0.7,
     },
     {
       url: 'https://www.regen.network/press-kit',
-      changeFrequency: 'yearly',
+      changeFrequency: 'daily',
       priority: 0.6,
     },
     {
       url: 'https://www.regen.network/privacy-policy',
-      changeFrequency: 'yearly',
+      changeFrequency: 'daily',
       priority: 0.5,
     },
     {
       url: 'https://www.regen.network/terms-service',
-      changeFrequency: 'yearly',
+      changeFrequency: 'daily',
       priority: 0.5,
     },
   ];

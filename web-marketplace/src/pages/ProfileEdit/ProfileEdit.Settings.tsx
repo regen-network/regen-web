07f6fbb3a7f57767e0fbd30234fba74940508f60
Florent <florent.lagrede@gmail.com>
2023-11-07 17:43:01 +0100

    feat: wire google connect

--- web-marketplace/src/pages/ProfileEdit/ProfileEdit.Settings.tsx
+++ web-marketplace/src/pages/ProfileEdit/ProfileEdit.Settings.tsx
@@ -1,14 +1,30 @@
+import { useNavigate } from 'react-router-dom';
+
+import { useAuth } from 'lib/auth/auth';
+import { apiServerUrl } from 'lib/env';
+
 import { UserAccountSettings } from 'components/organisms/UserAccountSettings/UserAccountSettings';
 
 export const ProfileEditSettings = () => {
+  const { authenticatedAccounts } = useAuth();
+  const googleAccounts = authenticatedAccounts?.filter(
+    account => !!account?.email,
+  );
+  const connectGoogleAccount = () =>
+    (window.location.href = `${apiServerUrl}/marketplace/v1/auth/google`);
+
   return (
     <UserAccountSettings
       email="joemcnab@gmail.com"
       socialProviders={[
         {
           providerName: 'Google',
-          connect: () => undefined,
+          connect: connectGoogleAccount,
         },
+        ...(googleAccounts?.map(account => ({
+          providerName: 'Google',
+          disconnect: () => undefined,
+        })) ?? []),
       ]}
       walletProvider={{
         address: 'regenfoobar3792723djghsdg',

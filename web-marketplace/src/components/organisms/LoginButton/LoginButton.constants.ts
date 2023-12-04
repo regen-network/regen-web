335d1c5c3e6ca27aac9f2774fc5832691e388de4
Florent <florent.lagrede@gmail.com>
2023-12-04 11:07:27 +0100

    fix: add dot after resend email

--- web-marketplace/src/components/organisms/LoginButton/LoginButton.constants.ts
+++ web-marketplace/src/components/organisms/LoginButton/LoginButton.constants.ts
@@ -14,7 +14,7 @@ export const socialProviders = [
 
 export const RESEND_TEXT = 'Don’t see anything?';
 export const RESEND_SUCCES = 'New confirmation code sent';
-export const RESEND_BUTTON_TEXT = 'Resend email';
+export const RESEND_BUTTON_TEXT = 'Resend email.';
 export const EMAIL_CONFIRMATION_CANCEL = 'cancel';
 export const EMAIL_CONFIRMATION_SUBMIT = 'log in';
 export const EMAIL_CONFIRMATION_SUCCES = 'Sign in successful';

ea04ac70fba1c12038dacfb98369f6129a53c3d7
blushi <marie.gauthier63@gmail.com>
2024-02-12 16:57:56 +0100

    feat: add pdf viewer to gallery

--- web-components/src/components/organisms/PostFiles/PostFiles.PrivateLocations.tsx
+++ web-components/src/components/organisms/PostFiles/PostFiles.PrivateLocations.tsx
@@ -7,14 +7,18 @@ type Props = Pick<PostFilesProps, 'files'>;
 
 const PostFilesPrivateLocations = ({ files }: Props) => {
   return (
-    <div className="relative">
+    <div className="h-[100%] relative">
       <Tag
         className="top-20 left-20 absolute"
         label="Location data available on request"
         icon={<PinIcon className="w-[16px] h-[17px]" />}
       />
 
-      <Gallery items={files} className={{ root: 'h-[100%]' }} />
+      <Gallery
+        items={files}
+        className={{ root: 'h-[100%]' }}
+        pdfPageHeight={550}
+      />
     </div>
   );
 };
--- web-components/src/components/organisms/PostFiles/PostFiles.tsx
+++ web-components/src/components/organisms/PostFiles/PostFiles.tsx
@@ -1,5 +1,6 @@
 import { Point } from 'geojson';
 
+import { cn } from '../../../utils/styles/cn';
 import { PostFilesPrivateFiles } from './PostFiles.PrivateFiles';
 import { PostFilesPrivateLocations } from './PostFiles.PrivateLocations';
 import { PostFilesPublic } from './PostFiles.Public';
@@ -17,20 +18,28 @@ export type PostFilesProps = {
   privacyType: PostPrivacyType;
   files: Array<PostFile>;
   mapboxToken?: string;
+  isAdmin: boolean;
 };
 
-const PostFiles = ({ privacyType, files, mapboxToken }: PostFilesProps) => {
+const PostFiles = ({
+  privacyType,
+  files,
+  mapboxToken,
+  isAdmin = false,
+}: PostFilesProps) => {
+  const isPublic = privacyType === 'public';
+  const privateLocations = privacyType === 'private_locations';
+  const privateFiles = privacyType === 'private_files';
   return (
-    <div className="w-[100%] sm:h-[550px]">
-      {privacyType === 'public' && (
-        <PostFilesPublic files={files} mapboxToken={mapboxToken} />
+    <div
+      className={cn(
+        'w-[100%]',
+        (isPublic || privateLocations || isAdmin) && 'sm:h-[550px]',
       )}
-      {privacyType === 'private_locations' && (
-        <PostFilesPrivateLocations files={files} />
-      )}
-      {privacyType === 'private_files' && (
-        <PostFilesPrivateFiles files={files} />
-      )}
+    >
+      {isPublic && <PostFilesPublic files={files} mapboxToken={mapboxToken} />}
+      {privateLocations && <PostFilesPrivateLocations files={files} />}
+      {privateFiles && <PostFilesPrivateFiles files={files} />}
     </div>
   );
 };

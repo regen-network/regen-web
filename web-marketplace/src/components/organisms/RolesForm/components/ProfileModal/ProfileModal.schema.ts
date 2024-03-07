e6d7aa2beb3a8ce5b8c044c0660c3400c63f0a15
blushi <marie.gauthier63@gmail.com>
2024-03-07 13:37:32 +0100

    fix: set min lenfth of 1 for profile name in roles form

--- web-marketplace/src/components/organisms/RolesForm/components/ProfileModal/ProfileModal.schema.ts
+++ web-marketplace/src/components/organisms/RolesForm/components/ProfileModal/ProfileModal.schema.ts
@@ -8,7 +8,7 @@ export const profileModalSchema = z.object({
   id: z.string().optional(),
   creatorId: z.string().nullable().optional(),
   profileType: z.custom<AccountType>(),
-  name: z.string(),
+  name: z.string().min(1),
   profileImage: z.string(),
   description: z.string().max(160).optional(),
   address: optionalAddressSchema,

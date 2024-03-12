350b55a1d216e3cf8f3ac70800a9f4c7c33d3b56
blushi <marie.gauthier63@gmail.com>
2024-03-12 13:04:45 +0100

    chore: rename project location field

--- web-marketplace/src/components/organisms/LocationForm/LocationForm.constants.ts
+++ web-marketplace/src/components/organisms/LocationForm/LocationForm.constants.ts
@@ -1,4 +1,4 @@
-export const LOCATION_LABEL = 'Location';
+export const LOCATION_LABEL = 'Project location';
 export const LOCATION_DESCRIPTION =
   'Type an address or latitude/longitude coordinates. This is the location that will appear in the project contracts, and on your project page.';
 export const LOCATION_PLACEHOLDER = 'Start typing the location';

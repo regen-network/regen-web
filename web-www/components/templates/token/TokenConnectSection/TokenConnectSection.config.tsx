72f97dce395a80ff4c7677594fa068dace4760e1
blushi <marie.gauthier63@gmail.com>
2024-04-29 10:38:30 +0200

    fix: web-www build

--- web-www/components/templates/token/TokenConnectSection/TokenConnectSection.config.tsx
+++ web-www/components/templates/token/TokenConnectSection/TokenConnectSection.config.tsx
@@ -1,8 +1,8 @@
 import DiscordIcon from 'web-components/src/components/icons/social/DiscordIcon';
 import TelegramIcon from 'web-components/src/components/icons/social/TelegramIcon';
 import TwitterIcon from 'web-components/src/components/icons/social/TwitterIcon';
-
 import { Theme } from 'web-components/src/theme/muiTheme';
+
 import { IconLabelProps } from '@/components/organisms/ConnectSection/ConnectSection.types';
 
 type Params = {

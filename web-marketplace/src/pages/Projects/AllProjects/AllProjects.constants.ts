611872d5a4ec446c52414949266c8c1e02a7c9c1
Will Gardiner <gardiner.w@gmail.com>
2024-05-20 10:47:02 -0700

    feat: change default filters on project list page.  (#2365)

--- web-marketplace/src/pages/Projects/AllProjects/AllProjects.constants.ts
+++ web-marketplace/src/pages/Projects/AllProjects/AllProjects.constants.ts
@@ -4,7 +4,7 @@ export const NO_CREDITS_TOOLTIP =
   'There are currently no open sell orders for this project.';
 export const COMMUNITY_FILTER = 'Show community credits';
 export const OFFCHAIN_FILTER = 'Show off-chain projects';
-export const SIDE_FILTERS_BUTTON = 'Add filters';
+export const SIDE_FILTERS_BUTTON = 'Edit filters';
 export const FILTERS_LABEL = 'Filters';
 export const RESET_FILTERS_LABEL = 'Reset filters';
 export const CREDIT_CLASS_FILTER_LABEL = 'Credit Class';

87858e7f92b9847851141ac9a72ef5b0dd1d971b
blushi <marie.gauthier63@gmail.com>
2024-06-12 11:17:47 +0200

    chore: add DataStream component skeleton

---
+++ web-marketplace/src/components/templates/ProjectDetails/ProjectDetails.DataStream.tsx
@@ -0,0 +1,7 @@
+import Section from 'web-components/src/components/section';
+
+import { DATA_STREAM } from './ProjectDetails.constant';
+
+export const DataStream = () => {
+  return <Section title={DATA_STREAM}></Section>;
+};

f7b2b08a0811286a3ab514ea365c26778e84a3e5
blushi <marie.gauthier63@gmail.com>
2024-06-12 11:30:13 +0200

    feat: get community projects filter default value from env

--- web-marketplace/src/lib/atoms/projects.atoms.ts
+++ web-marketplace/src/lib/atoms/projects.atoms.ts
@@ -1,7 +1,11 @@
 import { atom } from 'jotai';
 
+import { DEFAULT_COMMUNITY_PROJECTS_FILTER } from 'lib/env';
+
 import { sortOptions } from 'pages/Projects/AllProjects/AllProjects.config';
 
-export const useCommunityProjectsAtom = atom<boolean | undefined>(false);
+export const useCommunityProjectsAtom = atom<boolean | undefined>(
+  DEFAULT_COMMUNITY_PROJECTS_FILTER,
+);
 export const creditClassSelectedFiltersAtom = atom<Record<string, boolean>>({});
 export const projectsSortAtom = atom(sortOptions[0].value);

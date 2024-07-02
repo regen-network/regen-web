2bb89d93aff5bd99a7377327b9136f662a01c7dd
r41ph <ralph.galan@regen.network>
2024-07-02 13:35:03 +0100

    fix: APP-135 update Save button disabled functionality and styles (#2396)
    Co-authored-by: “Ralph“ <“ralph.galan@regen.network>

---
+++ web-components/src/components/buttons/SaveButton.tsx
@@ -0,0 +1,52 @@
+import { ButtonProps } from '@mui/material';
+import { makeStyles } from 'tss-react/mui';
+import ContainedButton from 'web-components/src/components/buttons/ContainedButton';
+import { SaveIcon } from 'web-components/src/components/icons/SaveIcon';
+import { Theme } from 'web-components/src/theme/muiTheme';
+import { cn } from 'web-components/src/utils/styles/cn';
+
+interface SaveButtonProps extends ButtonProps {
+  buttonText: string;
+}
+
+const useStyles = makeStyles()((theme: Theme) => ({
+  btn: {
+    display: 'flex',
+    minWidth: 0,
+    justifyContent: 'center',
+    [theme.breakpoints.up('sm')]: {
+      fontSize: theme.typography.pxToRem(21),
+      padding: theme.spacing(2, 4),
+      height: theme.typography.pxToRem(60),
+    },
+    [theme.breakpoints.down('sm')]: {
+      fontSize: theme.typography.pxToRem(18),
+      height: theme.typography.pxToRem(50),
+      padding: theme.spacing(2, 3.5),
+    },
+  },
+  saveIcon: {
+    marginRight: theme.spacing(1),
+    height: theme.typography.pxToRem(15),
+    [theme.breakpoints.down('sm')]: {
+      marginBottom: theme.spacing(0.25),
+    },
+  },
+}));
+
+export function SaveButton(props: SaveButtonProps) {
+  const { buttonText, disabled, onClick, className } = props;
+  const { classes: styles } = useStyles();
+  return (
+    <ContainedButton
+      type="submit"
+      className={cn(styles.btn, className)}
+      disabled={disabled}
+      onClick={onClick}
+      {...props}
+    >
+      <SaveIcon className={styles.saveIcon} />
+      {buttonText}
+    </ContainedButton>
+  );
+}
--- web-marketplace/src/components/organisms/EditProfileForm/EditProfileForm.ActionBar.tsx
+++ web-marketplace/src/components/organisms/EditProfileForm/EditProfileForm.ActionBar.tsx
@@ -1,22 +1,21 @@
 import { useFormState } from 'react-hook-form';
 import { Box } from '@mui/material';
 
-import ContainedButton from 'web-components/src/components/buttons/ContainedButton';
+import { SaveButton } from 'web-components/src/components/buttons/SaveButton';
 import { StickyBar } from 'web-components/src/components/sticky-bar/StickyBar';
 
 import { SAVE } from './EditProfileForm.constants';
 
 export const EditProfileFormActionBar = () => {
-  const { isSubmitting, submitCount, isValid } = useFormState();
+  const { isSubmitting, submitCount, isValid, dirtyFields } = useFormState();
+  const isFormDirty = !Object.keys(dirtyFields).length;
+  const isSaveButtonDisabled =
+    (submitCount > 0 && !isValid) || isSubmitting || isFormDirty || !isValid;
+
   return (
     <StickyBar>
       <Box sx={{ display: 'flex', justifyContent: 'flex-end', width: '100%' }}>
-        <ContainedButton
-          type="submit"
-          disabled={(submitCount > 0 && !isValid) || isSubmitting}
-        >
-          {SAVE}
-        </ContainedButton>
+        <SaveButton buttonText={SAVE} disabled={isSaveButtonDisabled} />
       </Box>
     </StickyBar>
   );

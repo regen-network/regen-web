12ef5168fc71ce32abd798f79efae01c4545ddc5
“Ralph“ <ralph.galan@regen.network>
2024-07-02 12:28:59 +0100

    fix: hide project create modal for project drafts

--- web-marketplace/src/pages/ProjectCreate/ProjectCreate.store.tsx
+++ web-marketplace/src/pages/ProjectCreate/ProjectCreate.store.tsx
@@ -1,10 +1,18 @@
+import { atom } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
 
 type ProjectsCurrentStep = {
   [id: string]: string;
 };
+type ProjectDraftStatus = {
+  id: string;
+  draft: boolean | undefined;
+};
+export type ProjectsDraftStatus = ProjectDraftStatus[] | [];
 
 export const projectsCurrentStepAtom = atomWithStorage(
   'projectsCurrentStep',
   {} as ProjectsCurrentStep,
 );
+
+export const projectsDraftState = atom<ProjectsDraftStatus>([]);

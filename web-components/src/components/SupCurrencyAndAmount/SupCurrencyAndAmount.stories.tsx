9f67a02d56f762f9e104891b3364f9ad6ac0fffd
“Ralph“ <ralph.galan@regen.network>
2024-07-12 16:21:21 +0100

    refactor: implement Intl.NumberFormat

---
+++ web-components/src/components/SupCurrencyAndAmount/SupCurrencyAndAmount.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import { SupCurrencyAndAmount } from './SupCurrencyAndAmount';
+
+export default {
+  title: 'SupCurrencyAndAmount',
+  component: SupCurrencyAndAmount,
+} as Meta<typeof SupCurrencyAndAmount>;
+
+type Story = StoryObj<typeof SupCurrencyAndAmount>;
+
+export const Default: Story = {
+  render: args => <SupCurrencyAndAmount {...args} />,
+};
+
+Default.args = {
+  price: 5,
+  currencyCode: 'USD',
+};

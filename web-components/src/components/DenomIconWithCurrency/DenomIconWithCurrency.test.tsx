a44df5b983cb43d5b8e6ccda49d52004501b5a4b
“Ralph“ <ralph.galan@regen.network>
2024-07-16 10:31:50 +0200

    refactor: remove CurrencyFlag and create DenomIconWithCurrency

---
+++ web-components/src/components/DenomIconWithCurrency/DenomIconWithCurrency.test.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import { USD_CURRENCY_DENOM } from 'web-marketplace/src/config/allowedBaseDenoms';
+
+import { DenomIconWithCurrency } from './DenomIconWithCurrency';
+
+describe('DenomIconWithCurrency', () => {
+  const currency = USD_CURRENCY_DENOM;
+
+  it('renders the denom icon and currency code', () => {
+    render(<DenomIconWithCurrency currency={currency} />);
+
+    const flagIcon = screen.getByTestId('USFlagIcon');
+    const currencyCode = screen.getByText(currency);
+
+    expect(flagIcon).toBeInTheDocument();
+    expect(currencyCode).toBeInTheDocument();
+  });
+
+  it('renders info icon and tooltip', async () => {
+    render(
+      <DenomIconWithCurrency currency={currency} tooltipText="tooltip text" />,
+    );
+
+    const tooltipIcon = screen.getByTestId('info-tooltip');
+    expect(tooltipIcon).toBeInTheDocument();
+
+    fireEvent.mouseEnter(tooltipIcon);
+    const currencyCode = await screen.findByText(/tooltip text/i);
+    expect(currencyCode).toBeInTheDocument();
+  });
+});

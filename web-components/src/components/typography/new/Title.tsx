a41dbb3d50e1a4dd347308d7e5c61d02d1810c16
Ralph <ralph.galan@regen.network>
2024-07-17 23:14:13 +0100

    feat: add new Title component

---
+++ web-components/src/components/typography/new/Title.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react';
+
+type TitleProps = {
+  as: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+  children: ReactNode;
+  className?: string;
+};
+
+const Title = ({ as, children, className = '' }: TitleProps) => {
+  const Tag = as;
+
+  return (
+    <Tag className={`font-[muli] font-black ${className}`}>{children}</Tag>
+  );
+};
+
+export default Title;

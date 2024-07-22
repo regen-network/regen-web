a43c30670f0789eaaa92f93e82e8630030a3b15b
Ralph <ralph.galan@regen.network>
2024-07-22 20:26:31 +0100

    fix: import path

--- web-components/src/components/cards/ProjectCard/ProjectCard.tsx
+++ web-components/src/components/cards/ProjectCard/ProjectCard.tsx
@@ -2,9 +2,8 @@ import { useState } from 'react';
 import { SxProps, Theme, useTheme } from '@mui/material';
 import clsx from 'clsx';
 import InfoTooltip from 'web-components/src/components/tooltip/InfoTooltip';
+import { PrefinanceTag } from 'web-components/src/components/PrefinanceTag/PrefinanceTag';
 import { Track } from 'web-marketplace/src/lib/tracker/types';
-
-import { PrefinanceTag } from 'src/components/PrefinanceTag/PrefinanceTag';
 
 import { ButtonType } from '../../../types/shared/buttonType';
 import { formatStandardInfo } from '../../../utils/format';

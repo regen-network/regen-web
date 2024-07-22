e2bcfa527f9a5d064ffea69c8f4c977b52de49ef
Ralph <ralph.galan@regen.network>
2024-07-22 19:21:36 +0100

    feat: add type attribute to SetMaxButton component

--- web-components/src/components/buttons/SetMaxButton.tsx
+++ web-components/src/components/buttons/SetMaxButton.tsx
@@ -7,6 +7,7 @@ export function SetMaxButton({
 }) {
   return (
     <button
+      type="button"
       onClick={onClick}
       aria-label="Set max credits"
       className="ml-3 sm:ml-10 border-none w-[46px] h-[30px] py-[5px] bg-grey-500 hover:bg-grey-400 rounded text-white text-sm font-bold font-['Lato'] hover:cursor-pointer"

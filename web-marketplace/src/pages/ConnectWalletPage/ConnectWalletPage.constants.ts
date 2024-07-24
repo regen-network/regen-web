3ff0c2598e5e924651b396b36215a971576c5087
Florent <florent.lagrede@gmail.com>
2024-07-24 09:20:44 +0200

    feat: translate ConnectWalletPage

--- web-marketplace/src/pages/ConnectWalletPage/ConnectWalletPage.constants.ts
+++ web-marketplace/src/pages/ConnectWalletPage/ConnectWalletPage.constants.ts
@@ -1,2 +1,3 @@
-export const CONNECT_WALLET_PAGE_TITLE =
-  'You must connect to Keplr in order to view the content of this page.';
+import { msg } from '@lingui/macro';
+
+export const CONNECT_WALLET_PAGE_TITLE = msg`You must connect to Keplr in order to view the content of this page.`;

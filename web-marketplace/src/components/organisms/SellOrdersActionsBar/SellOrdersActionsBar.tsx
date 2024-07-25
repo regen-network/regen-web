2c2faf450ff33c0d4333c0981e9dede3f9b9b33d
blushi <marie.gauthier63@gmail.com>
2024-07-25 13:32:44 +0200

    fix: hide price on sold out prefinance project page

--- web-marketplace/src/components/organisms/SellOrdersActionsBar/SellOrdersActionsBar.tsx
+++ web-marketplace/src/components/organisms/SellOrdersActionsBar/SellOrdersActionsBar.tsx
@@ -93,7 +93,7 @@ export const SellOrdersActionsBar = ({
           </OutlinedButton>
         ) : (
           <>
-            {(!isBuyButtonDisabled || !isSoldOut) &&
+            {((!isPrefinanceProject && !isBuyButtonDisabled) || !isSoldOut) &&
               (prefinancePrice ||
                 (avgPricePerTonLabel && !!onChainProjectId)) && (
                 <Box

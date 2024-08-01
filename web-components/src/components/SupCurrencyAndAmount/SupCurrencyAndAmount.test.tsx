9ffb53c72a1710c0e5883da6a3abdf162243de9b
Ralph <ralph.galan@regen.network>
2024-08-01 13:39:24 +0100

    fix: SupCurrencyAndAmount test

--- web-components/src/components/SupCurrencyAndAmount/SupCurrencyAndAmount.test.tsx
+++ web-components/src/components/SupCurrencyAndAmount/SupCurrencyAndAmount.test.tsx
@@ -3,9 +3,11 @@ import { render } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import { SupCurrencyAndAmount } from 'web-components/src/components/SupCurrencyAndAmount/SupCurrencyAndAmount';
 
+import { CURRENCIES } from '../DenomIconWithCurrency/DenomIconWithCurrency.constants';
+
 describe('SupCurrencyAndAmount', () => {
   it('renders the currency symbol and amount', () => {
-    const currency = 'USD';
+    const currency = CURRENCIES.usd;
     const amount = '100.00';
 
     const { getByText } = render(

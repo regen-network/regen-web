fdc4989d454fd49b9f3be20a5786078a0d8aa754
Ralph <ralph.galan@regen.network>
2024-08-01 13:16:07 +0100

    refactor: Update DenomIconWithCurrency component to use Body comp

--- web-components/src/components/DenomIconWithCurrency/DenomIconWithCurrency.tsx
+++ web-components/src/components/DenomIconWithCurrency/DenomIconWithCurrency.tsx
@@ -1,6 +1,7 @@
 import { DenomIcon } from 'web-marketplace/src/components/molecules/DenomIcon/DenomIcon';
 
 import InfoTooltipWithIcon from '../tooltip/InfoTooltipWithIcon';
+import { Body } from '../typography';
 import { Currency } from './DenomIconWithCurrency.constants';
 
 export function DenomIconWithCurrency({
@@ -11,8 +12,8 @@ export function DenomIconWithCurrency({
   tooltipText?: string;
 }) {
   return (
-    <span className="text-grey-600 text-sm font-normal font-['Lato'] flex gap-[5px] items-center">
-      <DenomIcon baseDenom={currency} className="h-[24px] block" />
+    <Body size="sm" className="flex gap-5 items-center">
+      <DenomIcon baseDenom={currency} className="h-[24px] inline-block" />
       <span>{currency.toUpperCase()}</span>
       {tooltipText && (
         <InfoTooltipWithIcon
@@ -22,6 +23,6 @@ export function DenomIconWithCurrency({
           className="fill-current text-grey-600 fill-grey-600"
         />
       )}
-    </span>
+    </Body>
   );
 }

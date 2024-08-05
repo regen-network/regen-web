17a55dd0fc0ab19e523f01680db316536d37354d
Ralph <ralph.galan@regen.network>
2024-08-05 14:54:31 +0100

    fix: make Buffer available in browser env

--- web-storybook/.storybook/preview.jsx
+++ web-storybook/.storybook/preview.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { CssBaseline } from '@mui/material';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Buffer } from 'buffer';
 
 import ThemeProvider from 'web-components/src/theme/RegenThemeProvider';
 import 'web-components/src/theme/fonts.css';
 import '../../tailwind.css';
+
+window.Buffer = Buffer;
 
 export const decorators = [
   Story => (

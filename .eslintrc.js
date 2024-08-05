e52d8402307d7e3ca600a44a4b0a889c78551d47
Ralph <ralph.galan@regen.network>
2024-08-05 13:01:43 +0100

    chore: disable lingui/no-unlocalized-strings rule for test files

--- .eslintrc.js
+++ .eslintrc.js
@@ -65,5 +65,11 @@ module.exports = {
         'import/no-anonymous-default-export': 'off',
       },
     },
+    {
+      files: ['*.test.ts', '*.spec.ts', '*.test.tsx', '*.spec.tsx'],
+      rules: {
+        'lingui/no-unlocalized-strings': 'off',
+      },
+    },
   ],
 };

729057fc2441c8673e103ea4168bb07bf307dec0
Ralph <ralph.galan@regen.network>
2024-08-08 09:58:19 +0100

    refactor: update ChooseCreditsForm tests and stories

--- web-marketplace/src/components/organisms/ChooseCreditsForm/ChooseCreditsForm.stories.tsx
+++ web-marketplace/src/components/organisms/ChooseCreditsForm/ChooseCreditsForm.stories.tsx
@@ -1,6 +1,8 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { creditVintages } from 'web-marketplace/src/components/molecules/CreditsAmount/CreditsAmount.mock';
 
+import { CURRENCIES } from 'web-components/src/components/DenomIconWithCurrency/DenomIconWithCurrency.constants';
+
 import { ChooseCreditsForm } from './ChooseCreditsForm';
 
 export default {
@@ -10,10 +12,30 @@ export default {
 
 type Story = StoryObj<typeof ChooseCreditsForm>;
 
+const creditAvailability = [
+  {
+    credits: 1000,
+    currency: CURRENCIES.usd,
+  },
+  {
+    credits: 2000,
+    currency: CURRENCIES.uregen,
+  },
+  {
+    credits: 3000,
+    currency: CURRENCIES.usdc,
+  },
+  {
+    credits: 4000,
+    currency: CURRENCIES.usdcaxl,
+  },
+];
+
 export const ChooseCreditsStep1: Story = {
   render: args => <ChooseCreditsForm {...args} />,
 };
 
 ChooseCreditsStep1.args = {
   creditVintages,
+  creditsAvailable: creditAvailability,
 };

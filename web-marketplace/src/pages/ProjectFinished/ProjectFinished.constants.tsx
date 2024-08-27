01a87c3eb2aa9c05bcb890706408d7776ea94d56
Florent <florent.lagrede@gmail.com>
2024-08-27 09:28:41 +0200

    feat: translate ProjectFinished page

--- web-marketplace/src/pages/ProjectFinished/ProjectFinished.constants.tsx
+++ web-marketplace/src/pages/ProjectFinished/ProjectFinished.constants.tsx
@@ -1,2 +1,3 @@
-export const PROJECT_OFFCHAIN_REMINDER =
-  'Reminder: this project is stored in the Regen Marketplace database. If you want to make a project on Regen Ledger, please log in with a keplr wallet.';
+import { msg } from '@lingui/macro';
+
+export const PROJECT_OFFCHAIN_REMINDER = msg`Reminder: this project is stored in the Regen Marketplace database. If you want to make a project on Regen Ledger, please log in with a keplr wallet.`;

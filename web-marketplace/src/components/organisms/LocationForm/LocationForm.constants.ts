9fb74204291f6157840902161f112df7e9a5f0b7
blushi <marie.gauthier63@gmail.com>
2024-08-29 15:50:30 +0200

    chore: address review comments

--- web-marketplace/src/components/organisms/LocationForm/LocationForm.constants.ts
+++ web-marketplace/src/components/organisms/LocationForm/LocationForm.constants.ts
@@ -1,4 +1,4 @@
 export const LOCATION_LABEL = 'Project location';
 export const LOCATION_DESCRIPTION =
-  'Type an address or latitude, longitude coordinates in decimal degrees format. This is the location that will appear in the project contracts, and on your project page.';
+  'Type an address or latitude, longitude coordinates in decimal degree format (i.e. 41.40338, 2.17403). Note: if you enter rural lat/long coordinates, select the closest town (the map marker on your project page will still appear in the correct location).';
 export const LOCATION_PLACEHOLDER = 'Start typing the location';

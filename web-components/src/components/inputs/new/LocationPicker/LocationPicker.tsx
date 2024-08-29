317f62786e2942194eba645770f803178c7c4e89
blushi <marie.gauthier63@gmail.com>
2024-08-29 10:15:20 +0200

    chore: address review comments

--- web-components/src/components/inputs/new/LocationPicker/LocationPicker.tsx
+++ web-components/src/components/inputs/new/LocationPicker/LocationPicker.tsx
@@ -92,7 +92,7 @@ export const LocationPicker = ({
         attributionControl={false}
         scrollZoom={{ around: 'center' }}
         touchZoomRotate={{ around: 'center' }}
-        cursor={'default'}
+        cursor="default"
       >
         <div className="absolute top-0 w-[100%]">
           {disabled ? (
@@ -125,7 +125,7 @@ export const LocationPicker = ({
         </div>
         {/* We simply absolutely position the location pin at the center,
           without using a react-map-gl Marker, so the map moves around the pin */}
-        <div className="absolute top-[50%] left-[50%] -ml-[18.5px] -mt-50">
+        <div className="absolute top-1/2 left-1/2 -ml-[18.5px] -mt-50">
           <GreenPinIcon />
           <Body
             className="mt-15 text-grey-0 bg-grey-700 py-5 px-10 rounded-[30px] -ml-[78px]"

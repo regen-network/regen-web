1de0b63ab1b6b10576ca2c82a0ecac36b157a2bf
blushi <marie.gauthier63@gmail.com>
2024-09-03 10:41:35 +0200

    chore: apply review comments

--- web-components/src/components/inputs/new/LocationField/LocationField.utils.ts
+++ web-components/src/components/inputs/new/LocationField/LocationField.utils.ts
@@ -1,9 +1,11 @@
+const COORDINATES_REGEX =
+  /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/;
+
+const isValidGeoCoordinates = (value: string): boolean =>
+  COORDINATES_REGEX.test(value);
+
 export const getLongLat = (value: string) => {
-  const isCoordinates =
-    /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/.test(
-      value,
-    );
-  if (isCoordinates) {
+  if (isValidGeoCoordinates(value)) {
     const [latitude, longitude] = value.split(',').map(Number) as [
       number,
       number,

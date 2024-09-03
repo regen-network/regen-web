3ba2736d357fccff0510bb54fc6c5aff28f6320f
Florent <florent.lagrede@gmail.com>
2024-09-03 16:12:31 +0200

    fix: storybook build with i18n

--- web-storybook/.storybook/preview.jsx
+++ web-storybook/.storybook/preview.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 import { CssBaseline } from '@mui/material';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -11,18 +11,30 @@ import 'web-components/src/theme/fonts.css';
 import '../../tailwind.css';
 
 window.Buffer = Buffer;
-i18n.activate('en');
+
+const StorybookI18nProvider = ({ children }) => {
+  useEffect(async () => {
+    const { messages } = await import(
+      `web-marketplace/src/lib/i18n/locales/en.po`
+    );
+
+    i18n.load('en', messages);
+    i18n.activate('en');
+  }, []);
+
+  return <I18nProvider i18n={i18n}>{children}</I18nProvider>;
+};
 
 export const decorators = [
   Story => (
-    <I18nProvider i18n={i18n}>
+    <StorybookI18nProvider>
       <ThemeProvider injectFonts>
         <CssBaseline />
         <Router>
           <Story />
         </Router>
       </ThemeProvider>
-    </I18nProvider>
+    </StorybookI18nProvider>
   ),
 ];
 

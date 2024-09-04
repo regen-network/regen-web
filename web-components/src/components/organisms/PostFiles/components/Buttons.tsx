e38f8cdeab2e3c8b681f7e38aa628e9d64097c0c
blushi <marie.gauthier63@gmail.com>
2024-09-04 10:45:17 +0200

    fix: update buttons z-index

--- web-components/src/components/organisms/PostFiles/components/Buttons.tsx
+++ web-components/src/components/organisms/PostFiles/components/Buttons.tsx
@@ -10,7 +10,7 @@ const Buttons = ({ onClose, selectedUrl }: Props) => (
   <>
     <div
       onClick={onClose}
-      className="cursor-pointer absolute top-[13px] right-[13px]"
+      className="cursor-pointer absolute top-[13px] right-[13px] z-10"
     >
       <CloseIcon className="h-[24px] w-[24px] rounded-[50%] text-grey-0 bg-grey-700/[.6] p-3" />
     </div>
@@ -18,7 +18,7 @@ const Buttons = ({ onClose, selectedUrl }: Props) => (
       target="_blank"
       rel="noopener noreferrer"
       href={selectedUrl}
-      className="outline-none cursor-pointer absolute top-[13px] right-[47px]"
+      className="outline-none cursor-pointer absolute top-[13px] right-[47px] z-10"
     >
       <OpenInNewIcon className="h-[24px] w-[24px] rounded-[50%] text-grey-0 bg-grey-700/[.6] p-3" />
     </a>

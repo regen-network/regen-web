dc1bc3dd782bc961697abf9f7b4c93655f552002
Florent <florent.lagrede@gmail.com>
2024-09-16 16:53:39 +0200

    fix: web-www

--- web-www/pages/404.tsx
+++ web-www/pages/404.tsx
@@ -1,5 +1,18 @@
+import { Box } from '@mui/material';
+
 import NotFound from 'web-components/src/components/views/NotFoundView';
 
 export default function Custom404() {
-  return <NotFound />;
+  return (
+    <NotFound
+      title="Oops! Page not found."
+      bodyText="The page you are looking for might have been temporarily removed or had its name changed."
+      buttonChildren={
+        <>
+          Visit Our Homepage{' '}
+          <Box display={{ xs: 'none', sm: 'inline' }}>{'\u00A0'}Instead</Box>
+        </>
+      }
+    />
+  );
 }

598d8634969ba7474231d0164cb45f7ae1ff3549
blushi <marie.gauthier63@gmail.com>
2024-09-19 12:09:43 +0200

    feat: add more props to PaymentInfoForm, payment methods query  and some refactoring

--- web-marketplace/src/components/molecules/CreditsAmount/CreditsAmount.utils.tsx
+++ web-marketplace/src/components/molecules/CreditsAmount/CreditsAmount.utils.tsx
@@ -1,8 +1,12 @@
 import { CardSellOrder } from 'web-marketplace/src/components/organisms/ChooseCreditsForm/ChooseCreditsForm.types';
 
+import { denomToMicro, microToDenom } from 'lib/denom.utils';
+
 import { PAYMENT_OPTIONS } from 'pages/BuyCredits/BuyCredits.constants';
 import { PaymentOptionsType } from 'pages/BuyCredits/BuyCredits.types';
 import { UISellOrderInfo } from 'pages/Projects/AllProjects/AllProjects.types';
+
+import { CURRENCY_AMOUNT } from './CreditsAmount.constants';
 
 export const getCreditsAvailablePerCurrency = (
   paymentOption: PaymentOptionsType,
@@ -34,3 +38,116 @@ export function getSpendingCap(
         0,
       ) || 0;
 }
+
+type GetCreditsAmountParams = {
+  value: number;
+  card: boolean;
+  orderedSellOrders: UISellOrderInfo[];
+};
+export const getCreditsAmount = ({
+  value,
+  card,
+  orderedSellOrders,
+}: GetCreditsAmountParams) => {
+  const currentCurrencyAmount = card ? value : denomToMicro(value);
+  let currentCreditsAmount = 0;
+  let currencyAmountLeft = currentCurrencyAmount;
+  const sellOrders = [];
+
+  for (let i = 0; i < orderedSellOrders.length; i++) {
+    const order = orderedSellOrders[i];
+    const price = getSellOrderPrice({ order, card });
+    const quantity = Number(order.quantity);
+    const orderTotalAmount = quantity * price;
+
+    if (currencyAmountLeft >= orderTotalAmount) {
+      currencyAmountLeft -= orderTotalAmount;
+      currentCreditsAmount += quantity;
+      sellOrders.push(formatFullSellOrder({ order, card, price }));
+      if (currencyAmountLeft === 0) break;
+    } else {
+      currentCreditsAmount += currencyAmountLeft / price;
+      sellOrders.push({
+        sellOrderId: order.id,
+        quantity: String(currencyAmountLeft / price),
+        bidPrice: !card
+          ? { amount: String(price), denom: order.askDenom }
+          : undefined,
+        price: card ? price : undefined,
+      });
+      break;
+    }
+  }
+  return { currentCreditsAmount, sellOrders };
+};
+
+type GetCurrencyAmountParams = {
+  currentCreditsAmount: number;
+  card: boolean;
+  orderedSellOrders: UISellOrderInfo[];
+};
+export const getCurrencyAmount = ({
+  currentCreditsAmount,
+  card,
+  orderedSellOrders,
+}: GetCurrencyAmountParams) => {
+  let currentCurrencyAmount = 0;
+  let creditsAmountLeft = currentCreditsAmount;
+  const sellOrders = [];
+
+  for (let i = 0; i < orderedSellOrders.length; i++) {
+    const order = orderedSellOrders[i];
+    const price = getSellOrderPrice({ order, card });
+    const quantity = Number(order.quantity);
+
+    // Take all credits from this sell order
+    if (creditsAmountLeft >= quantity) {
+      creditsAmountLeft -= quantity;
+      currentCurrencyAmount += quantity * price;
+      sellOrders.push(formatFullSellOrder({ order, card, price }));
+
+      if (creditsAmountLeft === 0) break;
+    } else {
+      // Take only remaining credits
+      currentCurrencyAmount += creditsAmountLeft * price;
+      sellOrders.push({
+        sellOrderId: order.id,
+        quantity: String(creditsAmountLeft),
+        bidPrice: !card
+          ? { amount: String(price), denom: order.askDenom }
+          : undefined,
+        price: card ? price : undefined,
+      });
+      break;
+    }
+  }
+  return {
+    [CURRENCY_AMOUNT]: card
+      ? currentCurrencyAmount
+      : microToDenom(currentCurrencyAmount),
+    sellOrders,
+  };
+};
+
+type GetSellOrderPriceParams = {
+  order: UISellOrderInfo;
+  card: boolean;
+};
+export const getSellOrderPrice = ({ order, card }: GetSellOrderPriceParams) =>
+  card ? (order as CardSellOrder).usdPrice : Number(order.askAmount);
+
+type FormatFullSellOrderParams = { price: number } & GetSellOrderPriceParams;
+export const formatFullSellOrder = ({
+  order,
+  card,
+  price,
+}: FormatFullSellOrderParams) => {
+  return {
+    sellOrderId: order.id,
+    quantity: order.quantity,
+    bidPrice: !card
+      ? { amount: String(price), denom: order.askDenom }
+      : undefined,
+    price: card ? price : undefined,
+  };
+};

a4a58dbb39b27d4136bcab02167a6fe217545637
r41ph <ralph.galan@regen.network>
2024-09-26 15:56:13 +0100

    refactor: add AdminNavigation component to admin section

--- web-marketplace/src/pages/ProfileEdit/ProfileEdit.constants.tsx
+++ web-marketplace/src/pages/ProfileEdit/ProfileEdit.constants.tsx
@@ -1,16 +1,8 @@
 import { msg } from '@lingui/macro';
 
-import { CogIcon } from 'web-components/src/components/icons/CogIcon';
-import { EnvelopeIcon } from 'web-components/src/components/icons/EnvelopeIcon';
-import { PaymentInfoIcon } from 'web-components/src/components/icons/PaymentInfoIcon';
-import { PrefinanceIcon } from 'web-components/src/components/icons/PrefinanceIcon';
-import { ShoppingBagIcon } from 'web-components/src/components/icons/ShoppingBagIcon';
-import { UserMenuIcon } from 'web-components/src/components/icons/UserMenuIcon';
 import { ProfileVariant } from 'web-components/src/components/organisms/ProfileHeader/ProfileHeader.types';
 
 import { AccountType } from 'generated/graphql';
-
-import { DashboardNavSection } from './ProfileEdit.types';
 
 export const PROFILE = msg`Profile`;
 export const VIEW_PROFILE = msg`View Profile`;
@@ -34,46 +26,3 @@ export const profileVariantMapping: Record<AccountType, ProfileVariant> = {
   ORGANIZATION: 'organization',
   USER: 'individual',
 };
-
-export const dashboardNavSections: DashboardNavSection[] = [
-  {
-    heading: msg`Orders`,
-    items: [
-      {
-        name: msg`My Orders`,
-        icon: <ShoppingBagIcon linearGradient />,
-        href: '/profile/edit/orders',
-      },
-      {
-        name: msg`My prefinance projects`,
-        icon: <PrefinanceIcon linearGradient width="24" height="24" />,
-        href: '/profile/edit/prefinance-projects',
-      },
-      {
-        name: msg`Saved payment info`,
-        icon: <PaymentInfoIcon linearGradient />,
-        href: '/profile/edit/payment-info',
-      },
-    ],
-  },
-  {
-    heading: msg`Profile`,
-    items: [
-      {
-        name: msg`Edit Profile`,
-        icon: <UserMenuIcon linearGradient />,
-        href: '/profile/edit/profile',
-      },
-      {
-        name: msg`Settings`,
-        icon: <CogIcon linearGradient />,
-        href: '/profile/edit/settings',
-      },
-      {
-        name: msg`Email updates`,
-        icon: <EnvelopeIcon linearGradient />,
-        href: '/profile/email-updates',
-      },
-    ],
-  },
-];

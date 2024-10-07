1f6bb249f4d88d380615dd46abb277e8006dbc53
Will Gardiner <gardiner.w@gmail.com>
2024-10-07 10:47:22 -0700

    add collapse/expand function for project filters

--- web-components/src/components/organisms/ProjectFilters/ProjectFilters.tsx
+++ web-components/src/components/organisms/ProjectFilters/ProjectFilters.tsx
@@ -1,5 +1,13 @@
+import { useState } from 'react';
 import { Trans } from '@lingui/macro';
-import { Box, Divider } from '@mui/material';
+import {
+  Box,
+  ButtonBase,
+  Collapse,
+  Divider,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
 
 import { Subtitle, Title } from '../../typography';
 import TagFilter from './ProjectFilter.TagFilter';
@@ -15,6 +23,7 @@ export interface Filter {
   title: string;
   displayType: 'tag' | 'checkbox';
   options: FilterOptions[];
+  hasCollapse?: boolean;
 }
 
 export default function ProjectFilters({
@@ -28,6 +37,12 @@ export default function ProjectFilters({
   onFilterChange: (id: string) => void;
   onFilterReset: () => void;
 }) {
+  const theme = useTheme();
+  const isXs = useMediaQuery(theme.breakpoints.down('sm'));
+  const [isExpanded, setIsExpanded] = useState<Record<number, boolean>>({});
+  const handleExpand = (index: number) => {
+    setIsExpanded({ ...isExpanded, [index]: !isExpanded[index] });
+  };
   return (
     <>
       <Box display="flex" justifyContent="space-between" alignItems="baseline">
@@ -39,39 +54,61 @@ export default function ProjectFilters({
         </Subtitle>
       </Box>
       <Divider sx={{ my: 5 }} />
-      {filters.map(filter => {
+      {filters.map((filter, index) => {
         return (
           <Box sx={{ mb: 5 }} key={filter.title}>
             <Title variant="h5" sx={{ mb: 5 }}>
               {filter.title}
             </Title>
-            {filter.displayType === 'tag' && (
-              <Box display="flex" flexWrap="wrap" gap={2}>
-                {filter.options.map(({ name, icon, id }) => (
-                  <TagFilter
-                    name={name}
-                    icon={icon}
-                    key={id}
-                    isSelected={activeFilterIds.includes(id)}
-                    onClick={() => onFilterChange(id)}
-                  />
-                ))}
-              </Box>
+
+            <Collapse
+              in={
+                // always show expanded for filter without hasCollapse
+                !filter.hasCollapse || (filter.hasCollapse && isExpanded[index])
+              }
+              collapsedSize={isXs ? 110 : 130}
+            >
+              {filter.displayType === 'tag' && (
+                <Box display="flex" flexWrap="wrap" gap={2}>
+                  {filter.options.map(({ name, icon, id }) => (
+                    <TagFilter
+                      name={name}
+                      icon={icon}
+                      key={id}
+                      isSelected={activeFilterIds.includes(id)}
+                      onClick={() => onFilterChange(id)}
+                    />
+                  ))}
+                </Box>
+              )}
+              {filter.displayType === 'checkbox' && (
+                <Box display="flex" flexDirection="column">
+                  {filter.options.map(({ name, icon, id }) => (
+                    <CheckboxFilter
+                      isSelected={activeFilterIds.includes(id)}
+                      name={name}
+                      icon={icon}
+                      key={id}
+                      onChange={() => onFilterChange(id)}
+                    />
+                  ))}
+                </Box>
+              )}
+            </Collapse>
+            {filter.hasCollapse && (
+              <ButtonBase
+                disableRipple
+                onClick={() => handleExpand(index)}
+                sx={{ fontWeight: '700' }}
+              >
+                {isExpanded[index] ? (
+                  <Trans>- See Less</Trans>
+                ) : (
+                  <Trans>+ See More</Trans>
+                )}
+              </ButtonBase>
             )}
 
-            {filter.displayType === 'checkbox' && (
-              <Box display="flex" flexDirection="column">
-                {filter.options.map(({ name, icon, id }) => (
-                  <CheckboxFilter
-                    isSelected={activeFilterIds.includes(id)}
-                    name={name}
-                    icon={icon}
-                    key={id}
-                    onChange={() => onFilterChange(id)}
-                  />
-                ))}
-              </Box>
-            )}
             <Divider sx={{ my: 5 }} />
           </Box>
         );

3c1d0232dacaa37058be3600b0aeefae74fe8c2d
r41ph <ralph.galan@regen.network>
2024-10-10 15:23:26 +0100

    fix: CreditsAmount stories

--- web-marketplace/src/components/molecules/CreditsAmount/CurrencyInput.tsx
+++ web-marketplace/src/components/molecules/CreditsAmount/CurrencyInput.tsx
@@ -124,7 +124,7 @@ export const CurrencyInput = ({
         endAdornment={
           paymentOption === PAYMENT_OPTIONS.CARD ? (
             <DenomIconWithCurrency
-              baseDenom={currency.askBaseDenom}
+              baseDenom={currency?.askBaseDenom}
               displayDenom={displayDenom}
             />
           ) : (

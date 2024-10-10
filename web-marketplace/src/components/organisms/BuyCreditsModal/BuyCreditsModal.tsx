b20b7fb445bafab4267673554f98b15920691482
r41ph <ralph.galan@regen.network>
2024-10-10 14:42:11 +0100

    feat: add form error when user doesn't have enough balance to buy credits

--- web-marketplace/src/components/organisms/BuyCreditsModal/BuyCreditsModal.tsx
+++ web-marketplace/src/components/organisms/BuyCreditsModal/BuyCreditsModal.tsx
@@ -356,7 +356,7 @@ const BuyCreditsModal: React.FC<React.PropsWithChildren<BuyCreditsModalProps>> =
                                   bankDenom: selectedSellOrder?.askDenom ?? '',
                                   baseDenom: selectedSellOrder?.askBaseDenom,
                                 }),
-                                userBalance,
+                                userBalance: userBalance || 0,
                                 _,
                               })}
                             />

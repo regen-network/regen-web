610f16a24b02752ea637cbe13540d5dacee8acd7
r41ph <ralph.galan@regen.network>
2024-10-16 15:41:11 +0100

    fix: building error

--- web-marketplace/src/components/molecules/CreditsAmount/CurrencyInput.tsx
+++ web-marketplace/src/components/molecules/CreditsAmount/CurrencyInput.tsx
@@ -147,7 +147,7 @@ export const CurrencyInput = ({
               onSelect={onHandleCurrencyChange}
               placeholderAriaLabel={selectPlaceholderAriaLabel}
               selectAriaLabel={selectAriaLabel}
-              defaultOption={currency.askDenom}
+              defaultOption={currency?.askDenom}
             />
           )
         }

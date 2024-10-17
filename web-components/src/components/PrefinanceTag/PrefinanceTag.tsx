8fa493746ae9bddd5a1ca4b7948967bb086cc604
Florent <florent.lagrede@gmail.com>
2024-10-17 14:15:14 +0200

    fix: use gradient from config

--- web-components/src/components/PrefinanceTag/PrefinanceTag.tsx
+++ web-components/src/components/PrefinanceTag/PrefinanceTag.tsx
@@ -24,13 +24,9 @@ export const PrefinanceTag = ({
 }) => (
   <div
     className={cn(
-      'rounded-r-[5px] flex items-center justify-center px-10 py-[3px] absolute top-30 left-0 text-sc-button-text-icon-dark',
+      'rounded-r-[5px] flex items-center justify-center px-10 py-[3px] absolute top-30 left-0 text-sc-button-text-icon-dark bg-prefinance-tag',
       classNames.root,
     )}
-    style={{
-      background:
-        'linear-gradient(179deg, rgba(var(--sc-tag-prefinance-600) / 1) 19.77%, rgba(var(--sc-tag-prefinance-500) / 1) 114.05%, rgba(var(--sc-tag-prefinance-400) / 1) 200.67%)',
-    }}
   >
     <PrefinanceIcon width={iconSize.width} height={iconSize.height} />
     <Label

b2b7028abdc88ae049f592efb6fe2a68522cdab7
r41ph <ralph.galan@regen.network>
2024-10-17 10:49:10 +0100

    style: update heding and sides paddings

--- web-marketplace/src/components/organisms/EditProfileForm/EditProfileForm.tsx
+++ web-marketplace/src/components/organisms/EditProfileForm/EditProfileForm.tsx
@@ -130,7 +130,7 @@ const EditProfileForm: React.FC<React.PropsWithChildren<EditProfileFormProps>> =
 
     return (
       <Form
-        className="p-40"
+        className="p-10 md:p-40"
         form={form}
         onSubmit={async data => {
           const hasError = validateEditProfileForm({

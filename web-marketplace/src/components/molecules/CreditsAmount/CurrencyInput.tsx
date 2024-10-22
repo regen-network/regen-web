ddf9eaceb07ab72a8d5f3a53f8c664cb389d8945
r41ph <ralph.galan@regen.network>
2024-10-22 16:44:00 +0100

    style: update $ position on mobile

--- web-marketplace/src/components/molecules/CreditsAmount/CurrencyInput.tsx
+++ web-marketplace/src/components/molecules/CreditsAmount/CurrencyInput.tsx
@@ -77,7 +77,7 @@ export const CurrencyInput = ({
   return (
     <div className="grow sm:flex-1 w-full sm:w-auto relative">
       {paymentOption === PAYMENT_OPTIONS.CARD && (
-        <span className="text-sm absolute top-[13px] sm:top-[18px] left-10 z-50">
+        <span className="text-xs sm:text-sm absolute top-[13px] left-[9px] sm:top-[18px] sm:left-10 z-50">
           $
         </span>
       )}
@@ -87,7 +87,7 @@ export const CurrencyInput = ({
         type="number"
         className={`border border-solid border-grey-300 focus-within:border-grey-500 focus-within:border-2 ${
           paymentOption === PAYMENT_OPTIONS.CARD ? 'pl-5' : ''
-        } w-full sm:w-auto flex justify-start relative sm:h-60 rounded-sm items-center`}
+        } w-full sm:w-auto flex justify-start relative xs:h-60 rounded-sm items-center`}
         customInputProps={{
           max: maxCurrencyAmount,
           min: 0,

42b47e9e0a1dfee73af1285ee30b281af7cbf787
r41ph <ralph.galan@regen.network>
2024-10-23 21:17:57 +0100

    refactor: BuyFiatModal + web2 users message and check all sellOrders credits

--- web-marketplace/src/components/organisms/BuyFiatModal/BuyFiatModal.tsx
+++ web-marketplace/src/components/organisms/BuyFiatModal/BuyFiatModal.tsx
@@ -1,5 +1,6 @@
 import { Box } from '@mui/material';
 
+import ContainedButton from 'web-components/src/components/buttons/ContainedButton';
 import OutlinedButton from 'web-components/src/components/buttons/OutlinedButton';
 import Card from 'web-components/src/components/cards/Card';
 import SellOrderNotFoundIcon from 'web-components/src/components/icons/SellOrderNotFoundIcon';
@@ -8,29 +9,26 @@ import { Title } from 'web-components/src/components/typography';
 
 import { UseStateSetter } from 'types/react/use-state';
 
-import { UserCanPurchaseCreditsType } from './BuyFiatModal.types';
+import { BuyFiatModalContent, FiatModalStateType } from './BuyFiatModal.types';
 
-interface BuyFiatModalProps {
-  title: string;
-  content: React.ReactNode;
-  button: { text: string; href: string | null };
-  userCanPurchaseCredits: UserCanPurchaseCreditsType;
-  onClose: UseStateSetter<UserCanPurchaseCreditsType>;
-  handleClick: () => void;
+interface BuyFiatModalProps extends BuyFiatModalContent {
+  fiatModalState: FiatModalStateType;
+  onClose: UseStateSetter<FiatModalStateType>;
+  handleClick: (action: string | null) => void;
 }
 
 export const BuyFiatModal = ({
   title,
   content,
-  button,
-  userCanPurchaseCredits,
+  buttons,
+  fiatModalState,
   onClose,
   handleClick,
 }: BuyFiatModalProps) => {
   return (
     <Modal
-      open={userCanPurchaseCredits.openModal}
-      onClose={() => onClose({ ...userCanPurchaseCredits, openModal: false })}
+      open={fiatModalState.openModal}
+      onClose={() => onClose({ ...fiatModalState, openModal: false })}
       className="w-[560px] !py-40 !px-30"
     >
       <Box className="flex flex-col items-center">
@@ -39,7 +37,32 @@ export const BuyFiatModal = ({
           {title}
         </Title>
         <Card className="text-left w-full py-40 px-30 my-40">{content}</Card>
-        <OutlinedButton onClick={handleClick}>{button.text}</OutlinedButton>
+        {buttons?.map((button, index) => {
+          return button.type === 'outlined' ? (
+            <OutlinedButton
+              key={index}
+              onClick={() => handleClick(button.action)}
+              className={`h-[49px] ${
+                buttons.find(button => button.type === 'contained')
+                  ? 'w-[90%]'
+                  : ''
+              }`}
+            >
+              {button.text}
+            </OutlinedButton>
+          ) : (
+            <ContainedButton
+              size="small"
+              onClick={() => handleClick(button.action)}
+              key={index}
+              className={`text-lg w-[90%] h-[49px] ${
+                buttons.length > 1 ? 'mb-20' : ''
+              }`}
+            >
+              {button.text}
+            </ContainedButton>
+          );
+        })}
       </Box>
     </Modal>
   );

3d9991f6c1595e555a32acced1070542b393da00
Will Gardiner <gardiner.w@gmail.com>
2024-10-29 10:05:52 -0700

    add colors for iucn threat codes

--- web-components/src/components/icons/terrasos/IucnRedCodeIcon/IucnRedCodeIcon.tsx
+++ web-components/src/components/icons/terrasos/IucnRedCodeIcon/IucnRedCodeIcon.tsx
@@ -25,8 +25,8 @@ const codes = {
 };
 
 const colors: Record<string, string> = {
-  IucnExtinct: '--bc-neutral-400',
-  IucnExtinctInWild: '--bc-neutral-400',
+  IucnExtinct: '--bc-neutral-700',
+  IucnExtinctInWild: '--bc-red-400',
   IucnCriticallyEndangered: '--bc-red-400',
   IucnEndangered: '--bc-orange-400',
   IucnVulnerable: '--bc-yellow-500',
@@ -36,6 +36,18 @@ const colors: Record<string, string> = {
   IucnNotEvaluated: '--bc-neutral-400',
 };
 
+const fontColors: Record<string, string> = {
+  IucnExtinct: '--sc-text-over-image',
+  IucnExtinctInWild: '--sc-text-header',
+  IucnCriticallyEndangered: '--sc-text-header',
+  IucnEndangered: '--sc-text-header',
+  IucnVulnerable: '--sc-text-header',
+  IucnNearThreatened: '--sc-text-header',
+  IucnLeastConcern: '--sc-text-header',
+  IucnDataDeficient: '--sc-text-header',
+  IucnNotEvaluated: '--sc-text-header',
+};
+
 export interface IucnCodeIconProps {
   type: keyof typeof codes;
 }
@@ -43,9 +55,10 @@ export interface IucnCodeIconProps {
 const IucnCodeIcon: React.FC<IucnCodeIconProps> = ({ type }) => {
   return (
     <div
-      className="flex items-center justify-center w-[50px] h-[50px] rounded-full text-[18px] font-bold text-bc-neutral-700"
+      className="flex items-center justify-center w-[50px] h-[50px] rounded-full text-[18px] font-bold"
       style={{
         background: `rgba(var(${colors[type]}) / 1)`,
+        color: `rgba(var(${fontColors[type]}) / 1)`,
       }}
     >
       {codes[type]}

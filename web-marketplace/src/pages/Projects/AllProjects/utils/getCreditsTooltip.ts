2808f1206c759dc71a15d5e454cde6160ad7083e
blushi <marie.gauthier63@gmail.com>
2024-11-04 15:16:37 +0100

    fix: don't display credits tooltip if compliance credits

--- web-marketplace/src/pages/Projects/AllProjects/utils/getCreditsTooltip.ts
+++ web-marketplace/src/pages/Projects/AllProjects/utils/getCreditsTooltip.ts
@@ -1,10 +1,10 @@
 import { TranslatorType } from 'lib/i18n/i18n.types';
+import { NormalizeProject } from 'lib/normalizers/projects/normalizeProjectsWithMetadata';
 
 import { NO_CREDITS_TOOLTIP, SOLD_OUT_TOOLTIP } from '../AllProjects.constants';
-import { ProjectWithOrderData } from '../AllProjects.types';
 
 type Params = {
-  project?: ProjectWithOrderData;
+  project?: NormalizeProject;
   isSoldOut: boolean;
   _: TranslatorType;
 };
@@ -16,6 +16,7 @@ export const getCreditsTooltip = ({
 }: Params): string | undefined => {
   if (isSoldOut) return _(SOLD_OUT_TOOLTIP);
   if (
+    !project?.complianceCredits &&
     !project?.purchaseInfo?.sellInfo?.creditsAvailable &&
     !project?.projectPrefinancing?.isPrefinanceProject
   )

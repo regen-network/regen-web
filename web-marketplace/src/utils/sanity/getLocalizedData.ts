15d90c3316756481edc331f8bd5c0be02fe721af
Florent Lagrede <florent.lagrede@gmail.com>
2024-11-21 14:56:55 +0100

    fix: APP-487 sanity localized data fallback (#2552)

--- web-marketplace/src/utils/sanity/getLocalizedData.ts
+++ web-marketplace/src/utils/sanity/getLocalizedData.ts
@@ -5,7 +5,6 @@ type Params = {
 
 export const getLocalizedData = ({ data, languageCode }: Params) => {
   const localizedData = data.filter(data => data.language === languageCode);
-  const defaultData = data.filter(data => !data.language);
 
-  return localizedData.length > 0 ? localizedData : defaultData;
+  return localizedData.length > 0 ? localizedData : data;
 };

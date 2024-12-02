d9acb25351b4df30d9d6d9469a917ac04071c64f
r41ph <ralph.galan@regen.network>
2024-12-02 11:25:31 +0000

    refactor: move functions to a 'utils' file

--- web-marketplace/src/components/organisms/RetirementCertificatesTable/RetirementCertificatesTable.tsx
+++ web-marketplace/src/components/organisms/RetirementCertificatesTable/RetirementCertificatesTable.tsx
@@ -28,6 +28,7 @@ import { NoCredits } from 'components/molecules';
 
 import { VIEW_LESS, VIEW_MORE } from './RetirementCertificatesTable.constants';
 import { getRetirementCertificateHeaders } from './RetirementCertificatesTable.headers';
+import { getBatchDate, getBatchIds } from './RetirementCertificatesTable.utils';
 
 type RetirementCertificatesTableProps = {
   retirements?: NormalizedRetirement[];
@@ -65,20 +66,6 @@ export const RetirementCertificatesTable: React.FC<
   const [viewMoreState, setViewMoreState] = useState<{
     [key: number]: boolean;
   }>();
-
-  const getBatchIds = (batchIds: string[]) => {
-    return batchIds.map((batchId: string, index: number) => (
-      <WithLoader isLoading={!batchId} variant="skeleton" key={index}>
-        <Link href={`/credit-batches/${batchId}`}>{batchId}</Link>
-      </WithLoader>
-    ));
-  };
-
-  const getBatchDate = (dates: string[]) => {
-    return dates.map(date => (
-      <GreyText className="whitespace-nowrap">{formatDate(date)}</GreyText>
-    ));
-  };
 
   const viewMoreHandleToggle = useCallback((id: number) => {
     setViewMoreState(prevState => ({
---
+++ web-marketplace/src/components/organisms/RetirementCertificatesTable/RetirementCertificatesTable.utils.tsx
@@ -0,0 +1,18 @@
+import { formatDate } from 'web-components/src/utils/format';
+
+import { GreyText, Link } from 'components/atoms';
+import WithLoader from 'components/atoms/WithLoader';
+
+export const getBatchIds = (batchIds: string[]) => {
+  return batchIds.map((batchId: string, index: number) => (
+    <WithLoader isLoading={!batchId} variant="skeleton" key={index}>
+      <Link href={`/credit-batches/${batchId}`}>{batchId}</Link>
+    </WithLoader>
+  ));
+};
+
+export const getBatchDate = (dates: string[]) => {
+  return dates.map(date => (
+    <GreyText className="whitespace-nowrap">{formatDate(date)}</GreyText>
+  ));
+};

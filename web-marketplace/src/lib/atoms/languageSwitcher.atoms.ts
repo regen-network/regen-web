a0d6102b54f427d273718d2dda3dac480921121f
Marie Gauthier <marie.gauthier63@gmail.com>
2025-02-25 16:22:48 +0100

    chore: APP-614 hide language switcher temporarily and always default to english (#2612)

--- web-marketplace/src/lib/atoms/languageSwitcher.atoms.ts
+++ web-marketplace/src/lib/atoms/languageSwitcher.atoms.ts
@@ -1,16 +1,20 @@
 import { atom } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
 
-import { defaultLocale, locales } from 'lib/i18n/i18n';
+import {
+  defaultLocale,
+  // locales
+} from 'lib/i18n/i18n';
 
 export const bannerTextAtom = atom('');
 
 const getInitialLanguage = () => {
-  if (typeof navigator !== 'undefined') {
-    const browserLang = navigator.language.split('-')[0];
-    const supportedLocales = Object.keys(locales);
-    return supportedLocales.includes(browserLang) ? browserLang : defaultLocale;
-  }
+  // Commenting out temporarily to always default to english until content translations completed
+  // if (typeof navigator !== 'undefined') {
+  //   const browserLang = navigator.language.split('-')[0];
+  //   const supportedLocales = Object.keys(locales);
+  //   return supportedLocales.includes(browserLang) ? browserLang : defaultLocale;
+  // }
   return defaultLocale;
 };
 

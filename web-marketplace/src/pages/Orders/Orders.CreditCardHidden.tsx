bb1caab0c049b62b6617d73f8ef1d788ddd27a07
blushi <marie.gauthier63@gmail.com>
2025-02-26 17:36:06 +0100

    chore: use h2 for CreditCardHidden title

--- web-marketplace/src/pages/Orders/Orders.CreditCardHidden.tsx
+++ web-marketplace/src/pages/Orders/Orders.CreditCardHidden.tsx
@@ -4,7 +4,7 @@ import { Body, Title } from 'web-components/src/components/typography';
 
 export const CreditCardHidden = () => (
   <div className="mb-20 bg-orange-gradient p-20 pl-[12px] rounded-lg border-solid border border-l-8 border-bc-orange-300">
-    <Title variant="h6" className="text-base">
+    <Title variant="h2" className="text-base">
       <Trans>Credit card orders are hidden</Trans>
     </Title>
     <Body size="sm" mobileSize="sm">

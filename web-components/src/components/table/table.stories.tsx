fe09bd953be9ec36b3e01245cd0d5672ebf9364f
blushi <marie.gauthier63@gmail.com>
2025-03-20 13:38:00 +0100

    fix: storybook

--- web-components/src/components/table/table.stories.tsx
+++ web-components/src/components/table/table.stories.tsx
@@ -1,9 +1,15 @@
 import { LabelDisplayedRowsArgs } from '@mui/material';
-import { action } from '@storybook/addon-actions';
 import { Meta, StoryObj } from '@storybook/react';
 
 import { TableActionButtons } from '../buttons/TableActionButtons';
 import { ActionsTable } from './ActionsTable';
+
+export default {
+  title: 'ActionsTable',
+  component: ActionsTable,
+} as Meta<typeof ActionsTable>;
+
+type Story = StoryObj<typeof ActionsTable>;
 
 const isIgnoreOffset = false;
 const rows = [
@@ -11,28 +17,30 @@ const rows = [
   ['Row 21', 'Row 22', 'Row 23'],
 ];
 
-export const actionsTable = (): JSX.Element => (
-  <ActionsTable
-    tableLabel="actions table"
-    actionButtonsText="Actions"
-    isIgnoreOffset={isIgnoreOffset}
-    labelDisplayedRows={({ from, to, count }: LabelDisplayedRowsArgs) => {
-      const displayedTo = isIgnoreOffset ? from + rows.length - 1 : to;
-      return count !== -1
-        ? `${from}–${displayedTo} of ${count}`
-        : `${from}–${displayedTo} of more than ${to}`;
-    }}
-    renderActionButtons={i => (
-      <TableActionButtons
-        buttons={[
-          {
-            label: `Do something with row ${i + 1}`,
-            onClick: () => alert(`Action clicked for row ${i + 1}`),
-          },
-        ]}
-      />
-    )}
-    headerRows={['Column 1', 'Column 2', 'Column 3']}
-    rows={rows}
-  />
-);
+export const Default: Story = {
+  render: args => <ActionsTable {...args} />,
+};
+
+Default.args = {
+  tableLabel: 'actions table',
+  actionButtonsText: 'Actions',
+  isIgnoreOffset,
+  labelDisplayedRows: ({ from, to, count }: LabelDisplayedRowsArgs) => {
+    const displayedTo = isIgnoreOffset ? from + rows.length - 1 : to;
+    return count !== -1
+      ? `${from}–${displayedTo} of ${count}`
+      : `${from}–${displayedTo} of more than ${to}`;
+  },
+  renderActionButtons: i => (
+    <TableActionButtons
+      buttons={[
+        {
+          label: `Do something with row ${i + 1}`,
+          onClick: () => alert(`Action clicked for row ${i + 1}`),
+        },
+      ]}
+    />
+  ),
+  headerRows: ['Column 1', 'Column 2', 'Column 3'],
+  rows: rows,
+};

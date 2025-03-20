7cd512b6d550a8c978dc195e7438c0571be0626a
blushi <marie.gauthier63@gmail.com>
2025-03-20 14:46:57 +0100

    fix: ui issues

--- web-marketplace/src/pages/BuyCredits/BuyCredits.Form.tsx
+++ web-marketplace/src/pages/BuyCredits/BuyCredits.Form.tsx
@@ -388,7 +388,7 @@ export const BuyCreditsForm = ({
           : 'mt-20 sm:mt-40'
       }
     >
-      <div>
+      <div className="w-full flex justify-center">
         {activeStep === 0 && (
           <ChooseCreditsForm
             retiring={retiring}

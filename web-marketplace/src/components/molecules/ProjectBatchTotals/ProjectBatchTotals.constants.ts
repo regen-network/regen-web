bd50107df56e484431784eac212c4e9a9cc5319d
r41ph <ralph.galan@regen.network>
2025-03-31 17:46:26 +0100

    feat: round credits available to the closest whole number in project page

--- web-marketplace/src/components/molecules/ProjectBatchTotals/ProjectBatchTotals.constants.ts
+++ web-marketplace/src/components/molecules/ProjectBatchTotals/ProjectBatchTotals.constants.ts
@@ -5,4 +5,4 @@ export const REGISTERED_CREDITS_TOOLTIP = msg`The total amount of credits that w
 export const TRADEABLE_CREDITS_TOOLTIP = msg`Tradable credits have no yet been retired and can be held, sold, or sent to another party.`;
 export const RETIRED_CREDITS_TOOLTIP = msg`Retired credits have been taken out of circulation permanently and cannot be sold to anyone else.`;
 export const SOLD_OUT = msg`Sold Out`;
-export const MAX_FRACTION_DIGITS_PROJECT_CREDITS = 3;
+export const MAX_FRACTION_DIGITS_PROJECT_CREDITS = 0;

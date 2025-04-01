bf55b94a74ce9338ca5007dcc2a52b3430918d74
r41ph <ralph.galan@regen.network>
2025-04-01 09:41:40 +0100

    fix: import path for roundToClosestWholeNumber

--- web-components/src/components/cards/ProjectCard/ProjectCard.CreditPrice.tsx
+++ web-components/src/components/cards/ProjectCard/ProjectCard.CreditPrice.tsx
@@ -1,13 +1,14 @@
 import { ReactNode } from 'react';
 import { Box, SxProps } from '@mui/material';
 
-import { roundToClosestWholeNumber } from 'src/utils/format';
-
 import GradientBadge from '../../../components/gradient-badge';
 import InfoTooltipWithIcon from '../../../components/tooltip/InfoTooltipWithIcon';
 import { Body, Subtitle } from '../../../components/typography';
 import { Theme } from '../../../theme/muiTheme';
-import { getFormattedNumber } from '../../../utils/format';
+import {
+  getFormattedNumber,
+  roundToClosestWholeNumber,
+} from '../../../utils/format';
 import { sxToArray } from '../../../utils/mui/sxToArray';
 import {
   ProjectCardBodyTextsMapping,

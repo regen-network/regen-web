47c9912e9349caa193c05061c4d4e128c989cd1b
r41ph <ralph.galan@regen.network>
2025-04-24 15:26:23 +0100

    fix: update QUANTITY_MAX_DECIMALS to vary based on NODE_ENV

--- web-marketplace/src/config/decimals.ts
+++ web-marketplace/src/config/decimals.ts
@@ -1,5 +1,6 @@
 export const QUANTITY_MIN_DECIMALS = 0;
-export const QUANTITY_MAX_DECIMALS = 6;
+export const QUANTITY_MAX_DECIMALS =
+  import.meta.env.NODE_ENV === 'production' ? 2 : 6;
 
 export const quantityFormatNumberOptions = {
   minimumFractionDigits: QUANTITY_MIN_DECIMALS,

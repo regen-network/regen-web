5db1cdb86a2a9415a5f5a60eb7c6bb6c5502f527
r41ph <ralph.galan@regen.network>
2025-04-30 14:41:00 +0100

    refactor: migrate useQueryBalances.ts to react query

--- web-marketplace/src/hooks/useQueryBalances.ts
+++ web-marketplace/src/hooks/useQueryBalances.ts
@@ -1,53 +1,60 @@
-import { useCallback, useEffect, useState } from 'react';
 import {
   QueryBalancesRequest,
   QueryBalancesResponse,
 } from '@regen-network/api/regen/ecocredit/v1/query';
+import { useQuery } from '@tanstack/react-query';
 
 import { useLedger } from '../ledger';
+import { getBalancesQuery } from './../lib/queries/react-query/ecocredit/getBalancesQuery/getBalancesQuery';
 
 type FetchBalances = (
   request: QueryBalancesRequest,
-) => Promise<QueryBalancesResponse | undefined>;
+) => Promise<QueryBalancesResponse | undefined | void>;
 
 type Params = {
   address?: string;
 };
 
+/**
+ * A custom hook that returns and refetches balances for a given address.
+ *
+ * @param {Object} params - Object containing the address to query.
+ * @param {string} params.address - The wallet address for which to fetch balances.
+ *
+ * See {@link Params} for more details.
+ *
+ * @returns {Object} An object containing:
+ * - balancesResponse: The fetched balances data.
+ * - fetchBalances: A function to initiate a refetch of balances.
+ * - isLoadingBalances: A boolean indicating whether the fetch operation is in progress.
+ */
 export default function useQueryBalances({ address }: Params): {
-  balancesResponse: QueryBalancesResponse | undefined;
+  balancesResponse: QueryBalancesResponse | undefined | void;
   fetchBalances: FetchBalances;
+  isLoadingBalances: boolean;
 } {
   const { queryClient } = useLedger();
-  const [balancesResponse, setBalancesResponse] = useState<
-    QueryBalancesResponse | undefined
-  >();
 
-  const fetchBalances = useCallback(async () => {
-    if (!queryClient || !address) return;
+  const {
+    data: balancesResponse,
+    refetch,
+    isFetching: isLoadingBalances,
+  } = useQuery(
+    getBalancesQuery({
+      client: queryClient,
+      request: {
+        address: address ?? '',
+      },
+      enabled: !!queryClient && !!address,
+      keepPreviousData: true,
+    }),
+  );
 
-    try {
-      const balances = await queryClient.regen.ecocredit.v1.balances({
-        address,
-      });
-      return balances;
-    } catch (err) {
-      console.error(err); // eslint-disable-line no-console
-    }
+  const fetchBalances = async (): Promise<
+    QueryBalancesResponse | undefined | void
+  > => {
+    await refetch();
+  };
 
-    return;
-  }, [queryClient, address]);
-
-  useEffect(() => {
-    if (!queryClient) return;
-
-    if (address) {
-      fetchBalances()
-        .then(setBalancesResponse)
-        /* eslint-disable */
-        .catch(console.error);
-    }
-  }, [queryClient, address]);
-
-  return { balancesResponse, fetchBalances };
+  return { balancesResponse, fetchBalances, isLoadingBalances };
 }

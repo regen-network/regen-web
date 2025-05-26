09bffba8bfeed60baff4a89118d344d5a7fd6d1a
blushi <marie.gauthier63@gmail.com>
2025-05-26 16:53:34 +0200

    chore: rm fragment

--- web-marketplace/src/components/organisms/SellerSetupAccount/SellerSetupAccount.tsx
+++ web-marketplace/src/components/organisms/SellerSetupAccount/SellerSetupAccount.tsx
@@ -13,37 +13,35 @@ const SellerSetupAccount = () => {
   const { setupAccount, openLoginLink } = useStripeAccount();
 
   return (
-    <>
-      {privActiveAccount?.can_use_stripe_connect && (
-        <Card className="border-sc-card-standard-stroke shadow-none p-20 sm:p-30 relative">
-          <Title variant="h5" className="pb-20 relative z-1">
-            <Trans>Manage your Stripe account for fiat purchases</Trans>
-          </Title>
-          <div className="flex items-center">
-            {activeAccount?.stripeConnectedAccountId ? (
-              <ContainedButton onClick={openLoginLink} className="z-1">
-                <Trans>view transactions</Trans>
-              </ContainedButton>
-            ) : (
-              <ContainedButton onClick={setupAccount} className="z-1">
-                <Trans>set up account</Trans>
-              </ContainedButton>
-            )}
-            <img
-              className="hidden sm:block sm:pl-20"
-              src="/png/logo/stripe.png"
-              // eslint-disable-next-line lingui/no-unlocalized-strings
-              alt="stripe logo"
-            />
-          </div>
-          {/* eslint-disable-next-line jsx-a11y/alt-text */}
+    privActiveAccount?.can_use_stripe_connect && (
+      <Card className="border-sc-card-standard-stroke shadow-none p-20 sm:p-30 relative">
+        <Title variant="h5" className="pb-20 relative z-1">
+          <Trans>Manage your Stripe account for fiat purchases</Trans>
+        </Title>
+        <div className="flex items-center">
+          {activeAccount?.stripeConnectedAccountId ? (
+            <ContainedButton onClick={openLoginLink} className="z-1">
+              <Trans>view transactions</Trans>
+            </ContainedButton>
+          ) : (
+            <ContainedButton onClick={setupAccount} className="z-1">
+              <Trans>set up account</Trans>
+            </ContainedButton>
+          )}
           <img
-            className="sm:h-[224px] sm:w-[224px] w-[168px] h-[168px] absolute right-0 sm:top-[-20px] top-40"
-            src="/svg/tokenization.svg"
+            className="hidden sm:block sm:pl-20"
+            src="/png/logo/stripe.png"
+            // eslint-disable-next-line lingui/no-unlocalized-strings
+            alt="stripe logo"
           />
-        </Card>
-      )}
-    </>
+        </div>
+        {/* eslint-disable-next-line jsx-a11y/alt-text */}
+        <img
+          className="sm:h-[224px] sm:w-[224px] w-[168px] h-[168px] absolute right-0 sm:top-[-20px] top-40"
+          src="/svg/tokenization.svg"
+        />
+      </Card>
+    )
   );
 };
 export { SellerSetupAccount };

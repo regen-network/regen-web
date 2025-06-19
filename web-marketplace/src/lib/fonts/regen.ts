9a73c26a95798e980656a37b063fd1d74d825bbb
r41ph <ralph.galan@regen.network>
2025-06-20 00:15:39 +0200

    refactor: inline fallback fonts in regen.ts

--- web-marketplace/src/lib/fonts/regen.ts
+++ web-marketplace/src/lib/fonts/regen.ts
@@ -1,26 +1,24 @@
 /* eslint-disable lingui/no-unlocalized-strings */
 import { Lato, Mulish } from 'next/font/google';
 
-const fallbackFonts = [
-  '-apple-system', // macOS, iOS
-  'BlinkMacSystemFont', // Chrome on macOS
-  'Segoe UI', // Windows
-  'Roboto', // Android, ChromeOS
-  'Oxygen', // KDE
-  'Ubuntu', // Ubuntu
-  'Cantarell', // GNOME
-  'Fira Sans', // Firefox OS
-  'Droid Sans', // Older Android
-  'Helvetica Neue', // Older macOS
-  'sans-serif', // Generic fallback
-];
-
 export const lato = Lato({
   subsets: ['latin'],
   display: 'swap',
   weight: ['100', '300', '400', '700'],
   variable: '--font-sans',
-  fallback: fallbackFonts,
+  fallback: [
+    '-apple-system', // macOS, iOS
+    'BlinkMacSystemFont', // Chrome on macOS
+    'Segoe UI', // Windows
+    'Roboto', // Android, ChromeOS
+    'Oxygen', // KDE
+    'Ubuntu', // Ubuntu
+    'Cantarell', // GNOME
+    'Fira Sans', // Firefox OS
+    'Droid Sans', // Older Android
+    'Helvetica Neue', // Older macOS
+    'sans-serif', // Generic fallback
+  ],
 });
 
 export const muli = Mulish({
@@ -28,5 +26,17 @@ export const muli = Mulish({
   display: 'swap',
   weight: ['500', '800', '900'],
   variable: '--font-muli',
-  fallback: fallbackFonts,
+  fallback: [
+    '-apple-system', // macOS, iOS
+    'BlinkMacSystemFont', // Chrome on macOS
+    'Segoe UI', // Windows
+    'Roboto', // Android, ChromeOS
+    'Oxygen', // KDE
+    'Ubuntu', // Ubuntu
+    'Cantarell', // GNOME
+    'Fira Sans', // Firefox OS
+    'Droid Sans', // Older Android
+    'Helvetica Neue', // Older macOS
+    'sans-serif', // Generic fallback
+  ],
 });

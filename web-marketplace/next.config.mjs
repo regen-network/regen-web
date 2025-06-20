8727e36f4699daf18b89bcfde748dbc266f4a64c
r41ph <ralph.galan@regen.network>
2025-06-20 09:46:53 +0200

    feat: handle memory built pages in development to improve performance

--- web-marketplace/next.config.mjs
+++ web-marketplace/next.config.mjs
@@ -4,12 +4,20 @@ const nextConfig = {
   distDir: './dist', // Changes the build output directory to `./dist/`.
   experimental: {
     swcPlugins: [['@lingui/swc-plugin', {}]],
+    // Optimize imports to reduce bundle size in development and production
     optimizePackageImports: [
       '@mui/material',
       '@mui/lab',
       '@lingui/macro',
       'lodash',
     ],
+  },
+  // Handle how the server will dispose or keep in memory built pages in development
+  onDemandEntries: {
+    // pages will stay in memory for 15 minutes after their last access before being disposed to improve dev performance
+    maxInactiveAge: 15 * 60 * 1000,
+    // number of pages that should be kept simultaneously in memory to avoid re-compilation
+    pagesBufferLength: 5,
   },
   serverExternalPackages: ['electron', 'pino-pretty', 'lokijs', 'encoding'],
   turbopack: {
@@ -30,6 +38,10 @@ const nextConfig = {
     resolveAlias: {
       'rdf-canonize-native': './empty-shim.js',
     },
+  },
+  // Move the dev indicators to the bottom right corner to avoid blocking the view of ReactQueryDevtools
+  devIndicators: {
+    position: 'bottom-right',
   },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // https://github.com/sindresorhus/got/issues/345

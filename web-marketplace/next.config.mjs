964c99359441747338a697e523f0f33e8ebb17a3
r41ph <ralph.galan@regen.network>
2025-06-23 14:53:00 +0200

    chore: add next/bundle-analyzer

--- web-marketplace/next.config.mjs
+++ web-marketplace/next.config.mjs
@@ -1,3 +1,4 @@
+import withBundleAnalyzer from '@next/bundle-analyzer';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: 'export', // Outputs a Single-Page Application (SPA).
@@ -64,4 +65,6 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withBundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+})(nextConfig);

af3e7a23a79e47da384fcb223cfe02d3381d6a57
blushi <marie.gauthier63@gmail.com>
2025-07-03 13:31:01 +0200

    chore: rm Buyers page

--- web-marketplace/src/legacy-pages/CreditClassDetails/CreditClassDetailsSimple/CreditClassDetailsSimple.constants.ts
+++ web-marketplace/src/legacy-pages/CreditClassDetails/CreditClassDetailsSimple/CreditClassDetailsSimple.constants.ts
@@ -2,3 +2,4 @@ import { msg } from '@lingui/core/macro';
 
 export const ELIGIBLE_ACTIVITIES = msg`eligible activities`;
 export const CREDIT_CLASS_TOOLTIP = msg`The structure, procedures and requirements for project registration, the quantification, monitoring, reporting and verification (MRV), and issuance of credits related to a certain credit type.`;
+export const OFFSET_GENERATION_METHOD = msg`offset generation method`;

4fe11bbb6b3d569f860bf3b13453cba01ad0f6a6
blushi <marie.gauthier63@gmail.com>
2025-07-16 17:33:09 +0200

    fix: imports and server issue with withHoverColor

--- web-components/src/components/icons/social/TwitterIcon.tsx
+++ web-components/src/components/icons/social/TwitterIcon.tsx
@@ -1,28 +1,12 @@
-import withHoverColor, { Props } from '../withHoverColor';
-
-function TwitterIcon({
-  className,
-  color,
-  onMouseEnter,
-  onMouseLeave,
-}: Props): JSX.Element {
+function TwitterIcon(props: React.SVGProps<SVGSVGElement>): JSX.Element {
   return (
-    <svg
-      className={className}
-      width="23"
-      height="22"
-      viewBox="0 0 23 22"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      onMouseEnter={onMouseEnter}
-      onMouseLeave={onMouseLeave}
-    >
+    <svg width="23" height="22" viewBox="0 0 23 22" fill="none" {...props}>
       <path
         d="M0.0560763 0L8.93612 12.1343L0 22H2.01116L9.83472 13.3624L16.1559 22H23L13.6203 9.18317L21.938 0H19.9268L12.7217 7.95503L6.90014 0H0.0560763ZM3.01364 1.51397H6.15782L20.042 20.4858H16.8978L3.01364 1.51397Z"
-        fill={color}
+        fill="currentColor"
       />
     </svg>
   );
 }
 
-export default withHoverColor(TwitterIcon);
+export default TwitterIcon;

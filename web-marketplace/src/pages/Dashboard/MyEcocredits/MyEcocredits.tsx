aa8cd3d6b04b49ffd2a1dc588fe092b08b4ddc03
blushi <marie.gauthier63@gmail.com>
2025-07-24 14:58:38 +0200

    chore: rm mentions of storefront

--- web-marketplace/src/pages/Dashboard/MyEcocredits/MyEcocredits.tsx
+++ web-marketplace/src/pages/Dashboard/MyEcocredits/MyEcocredits.tsx
@@ -165,7 +165,7 @@ export const MyEcocredits = (): JSX.Element => {
   const onButtonClick = (): void => {
     handleTxModalClose();
     if (txButtonTitle === _(CREATE_SELL_ORDER_BUTTON) && !error) {
-      navigate('/storefront');
+      navigate('/dashboard/sell');
     }
     if (txButtonTitle === _(RETIRE_SUCCESS_BUTTON) && !error) {
       setActivePortfolioTab(1);

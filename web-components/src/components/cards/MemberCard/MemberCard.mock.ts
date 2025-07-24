ae97761aa0c3ea0b4667809394b451f6db07af06
Marie Gauthier <marie.gauthier63@gmail.com>
2025-07-24 15:41:32 +0200

    feat: APP-673 add member card UI (#2693)

---
+++ web-components/src/components/cards/MemberCard/MemberCard.mock.ts
@@ -0,0 +1,11 @@
+/* eslint-disable lingui/no-unlocalized-strings */
+import { Link } from '@mui/material';
+
+export const member = {
+  imageSrc: '/member-image.png',
+  name: 'Mary Smith',
+  description: 'Chief Financial Officer, Ecometric',
+  linkComponent: Link,
+  editText: 'edit',
+  editLink: '/',
+};

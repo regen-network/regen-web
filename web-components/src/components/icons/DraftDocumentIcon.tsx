8ed155d942a6596a3bc7258841f7573ee5a83943
Alexander Åstrand <alexander.astrand@gmail.com>
2025-07-28 13:18:12 +0200

    feat: APP-671 project banner new branch and stories setup

--- web-components/src/components/icons/DraftDocumentIcon.tsx
+++ web-components/src/components/icons/DraftDocumentIcon.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
 
-export const DraftDocumentIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg width="14" height="14" viewBox="0 0 14 14" fill="none" {...props}>
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M11.25 4.99991V12.4166H2.75V1.58325H7H7.83338L7.83337 4.24991C7.83337 4.66412 8.16916 4.99991 8.58337 4.99991H11.25ZM10.8782 3.99991L8.83338 1.82229L8.83338 3.99991H10.8782ZM1.75 0.833252C1.75 0.695181 1.86193 0.583252 2 0.583252H7H8.93348C9.00251 0.583252 9.06847 0.611796 9.11572 0.662119L12.1822 3.92775C12.2258 3.97411 12.25 4.0353 12.25 4.09889V13.1666C12.25 13.3047 12.1381 13.4166 12 13.4166H2C1.86193 13.4166 1.75 13.3047 1.75 13.1666V0.833252Z"
-      fill="currentColor"
-    />
-  </svg>
-);
+interface DraftDocumentIconProps extends React.SVGProps<SVGSVGElement> {
+  useGradient?: boolean;
+}
+
+export const DraftDocumentIcon = ({
+  useGradient = false,
+  ...props
+}: DraftDocumentIconProps) => {
+  const gradientId = `draft-gradient-${Math.random()
+    .toString(36)
+    .substr(2, 9)}`;
+  const fillColor = useGradient ? `url(#${gradientId})` : 'currentColor';
+
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" {...props}>
+      {useGradient && (
+        <defs>
+          <linearGradient
+            id={gradientId}
+            x1="12"
+            y1="2.86102e-07"
+            x2="0.440139"
+            y2="21.6747"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop offset="0.00458717" stopColor="#7BC796" />
+            <stop offset="1" stopColor="#C5E6D1" />
+          </linearGradient>
+        </defs>
+      )}
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M19.2857 8.57129V21.2857H4.71429V2.71429H12H13.4286L13.4286 7.28571C13.4286 8.13571 14.1371 8.57129 14.7143 8.57129H19.2857ZM18.5017 6.85714L14.7143 3.11829L14.7143 6.85714H18.5017ZM3 1.42857C3 1.19143 3.19143 1 3.42857 1H12H15.2906C15.4328 1 15.5695 1.05651 15.6697 1.15651L20.6648 6.73107C20.7366 6.8057 20.7857 6.90594 20.7857 7.01308V22.7143C20.7857 22.9514 20.5943 23.1429 20.3571 23.1429H3.42857C3.19143 23.1429 3 22.9514 3 22.7143V1.42857Z"
+        fill={fillColor}
+      />
+    </svg>
+  );
+};
---
+++ web-marketplace/src/components/organisms/ProjectDashboardBanner/ProjectDashboardBanner.tsx
@@ -0,0 +1,159 @@
+import React, { useMemo, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { msg } from '@lingui/macro';
+import { useLingui } from '@lingui/react';
+
+import ContainedButton from 'web-components/src/components/buttons/ContainedButton';
+import OutlinedButton from 'web-components/src/components/buttons/OutlinedButton';
+import ArrowDownIcon from 'web-components/src/components/icons/ArrowDownIcon';
+import ClipboardIcon from 'web-components/src/components/icons/ClipboardIcon';
+import { DraftDocumentIcon } from 'web-components/src/components/icons/DraftDocumentIcon';
+import EditIcon from 'web-components/src/components/icons/EditIcon';
+import EyeIcon from 'web-components/src/components/icons/EyeIcon';
+import { HorizontalDotsIcon } from 'web-components/src/components/icons/HorizontalDotsIcon';
+import TrashIcon from 'web-components/src/components/icons/TrashIcon';
+import ProjectPlaceInfo from 'web-components/src/components/place/ProjectPlaceInfo';
+import { Title } from 'web-components/src/components/typography';
+
+interface Project {
+  id: string;
+  name?: string;
+  place?: string;
+  area?: number;
+  areaUnit?: string;
+  imgSrc?: string;
+  slug?: string;
+}
+
+export interface ProjectBannerProps {
+  project: Project;
+  canEdit?: boolean;
+}
+
+const ProjectDashboardBanner: React.FC<ProjectBannerProps> = ({
+  project,
+  canEdit,
+}) => {
+  const { _ } = useLingui();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const menuItems = useMemo(
+    () => [
+      {
+        label: _(msg`Register with a crediting protocol`),
+        color: 'text-gray-700',
+        icon: <ClipboardIcon />,
+        action: () => {
+          setIsMenuOpen(false);
+        },
+      },
+      {
+        label: _(msg`Convert to draft`),
+        color: 'text-gray-700',
+        icon: <DraftDocumentIcon className="w-6 h-6" useGradient />,
+        action: () => setIsMenuOpen(false),
+      },
+      {
+        label: _(msg`Migrate project`),
+        color: 'text-gray-700',
+        icon: <ArrowDownIcon direction="next" fontSize="medium" useGradient />,
+        action: () => setIsMenuOpen(false),
+      },
+      {
+        label: _(msg`Delete project`),
+        color: 'text-red-600',
+        icon: <TrashIcon className="w-6 h-6" style={{ color: '#f87171' }} />,
+        action: () => setIsMenuOpen(false),
+      },
+    ],
+    [_],
+  );
+
+  return (
+    <div className="relative w-full mt-20">
+      {/* Background Image Container */}
+      <div
+        className="relative w-full min-h-[187px] bg-cover bg-center bg-no-repeat rounded-lg"
+        style={{
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${
+            project.imgSrc || '/default-project-image.jpg'
+          })`,
+        }}
+      >
+        {/* Content Container */}
+        <div className="relative z-10 p-20 pb-30 flex flex-col">
+          {/* Top right menu */}
+          <div className="flex justify-end">
+            <div className="absolute top-4 right-4">
+              <button
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+                className="flex items-center justify-center w-[39px] h-[39px] cursor-pointer rounded-full bg-bc-neutral-700 p-5 transition-colors"
+              >
+                <HorizontalDotsIcon sx={{ color: 'white' }} />
+              </button>
+
+              {isMenuOpen && (
+                <div className="absolute top-12 right-0 w-[199px] bg-bc-neutral-100 rounded-md shadow-lg z-10 p-15 pr-10 flex flex-col gap-10">
+                  {menuItems.map((item, idx) => (
+                    <button
+                      key={idx}
+                      className={`flex items-center justify-start p-0 gap-10 w-full bg-transparent border-none text-left text-[16px] ${item.color} hover:bg-gray-100 cursor-pointer`}
+                      onClick={item.action}
+                    >
+                      {item.icon}
+                      {item.label}
+                    </button>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+
+          {/* Project info and buttons */}
+          <div className="flex flex-col justify-end">
+            <Title sx={{ color: 'white', marginBottom: '4px' }} variant="h3">
+              {project.name || _(msg`Untitled Project`)}
+            </Title>
+
+            <div className="mb-20">
+              <ProjectPlaceInfo
+                place={project.place ?? ''}
+                area={project.area}
+                areaUnit={project.areaUnit}
+                smFontSize="0.8125rem"
+                fontSize="14px"
+                color="white"
+                iconClassName="text-bc-neutral-0"
+              />
+            </div>
+
+            <div className="flex flex-col sm:flex-row gap-15">
+              <OutlinedButton
+                startIcon={<EyeIcon />}
+                onClick={() => {
+                  navigate(`/project/${project.slug || project.id}`);
+                }}
+              >
+                {_(msg`View`)}
+              </OutlinedButton>
+
+              {canEdit && (
+                <ContainedButton
+                  startIcon={<EditIcon sx={{ color: 'white' }} />}
+                  onClick={() => {
+                    navigate(`/project-pages/${project.id}/edit/basic-info`);
+                  }}
+                >
+                  {_(msg`Edit`)}
+                </ContainedButton>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectDashboardBanner;

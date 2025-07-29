1f0c3c2eb91ab6af11a39fefd3496674a78ff555
Alexander Åstrand <alexander.astrand@gmail.com>
2025-07-29 15:16:36 +0200

    fix: APP-671 rename useGradient, close dropdown on click outside, useMediaQuery from MUI, remove useMemo, code cleanup

--- web-marketplace/src/components/organisms/ProjectDashboardBanner/ProjectDashboardBanner.constants.tsx
+++ web-marketplace/src/components/organisms/ProjectDashboardBanner/ProjectDashboardBanner.constants.tsx
@@ -1,2 +1,12 @@
+import { msg } from '@lingui/macro';
+
 export const MAX_ADDRESS_LENGTH_DESKTOP = 160;
 export const MAX_ADDRESS_LENGTH_MOBILE = 55;
+
+export const REGISTER_WITH_PROTOCOL = msg`Register with a crediting protocol`;
+export const CONVERT_TO_DRAFT = msg`Convert to draft`;
+export const MIGRATE_PROJECT = msg`Migrate project`;
+export const DELETE_PROJECT = msg`Delete project`;
+export const UNTITLED_PROJECT = msg`Untitled Project`;
+export const VIEW = msg`View`;
+export const EDIT = msg`Edit`;

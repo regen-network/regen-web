6df6f6edbb2a27b996ef7eef3f8e3ec8bdfd0397
r41ph <ralph.galan@regen.network>
2025-08-06 13:27:30 +0100

    ci: add file URL handling in next.config.mjs

--- web-marketplace/next.config.mjs
+++ web-marketplace/next.config.mjs
@@ -1,5 +1,9 @@
 import withBundleAnalyzer from '@next/bundle-analyzer';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {

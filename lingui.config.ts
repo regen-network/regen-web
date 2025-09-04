817b2dcbb3007513f4b9e163b5f977848914c82b
r41ph <ralph.galan@regen.network>
2025-09-04 16:52:20 +0100

    fix: add newline in lingui.config.ts

--- lingui.config.ts
+++ lingui.config.ts
@@ -1,6 +1,7 @@
 import type { LinguiConfig } from '@lingui/conf';
 
 export const localesConst = ['en', 'es'] as const;
+
 export type Locale = (typeof localesConst)[number];
 
 const config: LinguiConfig = {

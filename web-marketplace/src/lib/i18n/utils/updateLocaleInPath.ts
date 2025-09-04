b7b6627b19180c248fe5bc737262065222d437dd
r41ph <ralph.galan@regen.network>
2025-09-04 15:34:22 +0100

    fix: double locale in url

--- web-marketplace/src/lib/i18n/utils/updateLocaleInPath.ts
+++ web-marketplace/src/lib/i18n/utils/updateLocaleInPath.ts
@@ -2,5 +2,10 @@ import { LOCALE_PREFIX_REGEX } from '../locales';
 
 export function updateLocaleInPath(pathname: string, locale: string): string {
   const stripped = pathname.replace(LOCALE_PREFIX_REGEX, '');
-  return `/${locale}${stripped || '/'}`;
+  const suffix = stripped
+    ? stripped.startsWith('/')
+      ? stripped
+      : `/${stripped}`
+    : '/';
+  return `/${locale}${suffix}`;
 }

0730d40b467b7880fc1b3481a0797f4750f5ec2f
blushi <marie.gauthier63@gmail.com>
2025-09-04 10:39:51 +0200

    feat: predict more contract addresses

---
+++ web-marketplace/src/utils/hooks/useCreateDao/useCreateDao.constants.ts
@@ -0,0 +1,9 @@
+// TODO: replace with env variables
+// these are the code IDs on regen-upgrade testnet
+// https://github.com/DA0-DA0/dao-dao-ui/blob/development/packages/utils/constants/codeIds.json#L1777
+export const daoDaoCoreCodeId = 6;
+export const filterCodeId = 14;
+export const protobufRegistryCodeId = 15;
+export const rbamCodeId = 16;
+export const proposalSingleCodeId = 13;
+export const preProposeSingleCodeId = 11;

460b67a0076f92b3789cac2373e7fd7f0b8b64ff
r41ph <ralph.galan@regen.network>
2025-09-04 16:31:12 +0100

    refactor: Lingui configuration  constant for locales

--- lingui.config.ts
+++ lingui.config.ts
@@ -1,7 +1,10 @@
 import type { LinguiConfig } from '@lingui/conf';
 
+export const localesConst = ['en', 'es'] as const;
+export type Locale = (typeof localesConst)[number];
+
 const config: LinguiConfig = {
-  locales: ['en', 'es'],
-} as const;
+  locales: [...localesConst],
+};
 
 export default config;

6fd61981a7a34ef0c5b40a7e47288b166ffb4c05
Alexander Åstrand <alexander.astrand@gmail.com>
2025-09-22 11:34:15 +0200

    chore: APP-766 update table header name, add translations

--- web-marketplace/src/components/organisms/UserSellOrders/UserSellOrders.Toolbar.tsx
+++ web-marketplace/src/components/organisms/UserSellOrders/UserSellOrders.Toolbar.tsx
@@ -45,7 +45,7 @@ export const UserSellOrdersToolbar = ({
         )}
       >
         <Subtitle size="xl" className="text-base sm:text-[22px] pt-3">
-          <Trans>Open orders</Trans>
+          <Trans>Open sell orders</Trans>
         </Subtitle>
         {hasTradableCredits ? (
           <CreateButton

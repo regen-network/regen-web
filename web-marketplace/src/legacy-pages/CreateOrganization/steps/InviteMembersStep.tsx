71999bc3f1f30cf4c7c960af6b299c9ce3033d26
blushi <marie.gauthier63@gmail.com>
2025-10-07 09:58:19 +0200

    feat: get members and refactor modals

--- web-marketplace/src/legacy-pages/CreateOrganization/steps/InviteMembersStep.tsx
+++ web-marketplace/src/legacy-pages/CreateOrganization/steps/InviteMembersStep.tsx
@@ -2,38 +2,74 @@ import { Trans } from '@lingui/macro';
 import { useLingui } from '@lingui/react';
 import { Title, Body } from 'web-components/src/components/typography';
 import { INVITE_MEMBERS } from '../CreateOrganization.constants';
-import { OrganizationMembers } from 'components/organisms/OrganizationMembers/OrganizationMembers';
 import { BaseMemberRole } from 'components/organisms/BaseMembersTable/BaseMembersTable.types';
+import { OrganizationMembersInviteTable } from 'components/organisms/OrganizationMembers/InviteMembers/InviteMembers.Table';
+import { useAuth } from 'lib/auth/auth';
+import { useMemo } from 'react';
+import { DEFAULT_NAME } from 'pages/Dashboard/Dashboard.constants';
+import { Member } from 'components/organisms/OrganizationMembers/OrganizationMembers.types';
 
-export const InviteMembersStep = () => {
+const InviteMembersStep = () => {
   const { _ } = useLingui();
+  const { activeAccount } = useAuth();
+
+  const members = useMemo(() => {
+    const dao =
+      activeAccount?.daosByAssignmentAccountIdAndDaoAddress?.nodes?.find(
+        dao => !!dao?.organizationByDaoAddress,
+      );
+    return (dao?.assignmentsByDaoAddress?.nodes?.map(assignment => {
+      const acc = assignment?.accountByAccountId;
+      return assignment
+        ? {
+            id: acc?.id,
+            name: acc?.name || _(DEFAULT_NAME),
+            title: acc?.title,
+            avatar: acc?.image,
+            role: assignment?.roleName,
+            visible: assignment?.visible,
+            hasWalletAddress: !!acc?.addr,
+            isCurrentUser: acc?.id === activeAccount?.id,
+            organization:
+              dao?.organizationByDaoAddress?.name || _(DEFAULT_NAME),
+            email:
+              acc?.privateAccountById?.email ||
+              acc?.privateAccountById?.googleEmail,
+          }
+        : null;
+    }) ?? []) as Member[];
+  }, [activeAccount?.daosByAssignmentAccountIdAndDaoAddress?.nodes]);
+
   return (
     <div className="text-center">
-      <Title>{_(INVITE_MEMBERS)}</Title>
-      <Body>
+      <Title variant="h3">{_(INVITE_MEMBERS)}</Title>
+      <Body className="pt-20 pb-40">
         <Trans>
           Organization members have permissions for all projects associated with
           an organization. You can also skip this step and invite members later.
         </Trans>
       </Body>
-      <OrganizationMembers
-        members={[]}
-        onInvite={function (): void {
-          throw new Error('Function not implemented.');
-        }}
-        onToggleSort={function (): void {
-          throw new Error('Function not implemented.');
-        }}
-        onUpdateRole={function (id: string, role: BaseMemberRole): void {
-          throw new Error('Function not implemented.');
-        }}
-        onUpdateVisibility={function (id: string, visible: boolean): void {
-          throw new Error('Function not implemented.');
-        }}
-        onRemove={function (id: string): void {
-          throw new Error('Function not implemented.');
-        }}
-      />
+      {members && (
+        <OrganizationMembersInviteTable
+          members={members}
+          onInvite={function (): void {
+            throw new Error('Function not implemented.');
+          }}
+          onToggleSort={function (): void {
+            throw new Error('Function not implemented.');
+          }}
+          onUpdateRole={function (id: string, role: BaseMemberRole): void {
+            throw new Error('Function not implemented.');
+          }}
+          onUpdateVisibility={function (id: string, visible: boolean): void {
+            throw new Error('Function not implemented.');
+          }}
+          onRemove={function (id: string): void {
+            throw new Error('Function not implemented.');
+          }}
+        />
+      )}
     </div>
   );
 };
+export default InviteMembersStep;

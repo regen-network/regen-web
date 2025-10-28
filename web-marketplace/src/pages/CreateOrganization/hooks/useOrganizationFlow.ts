998f5b8d4bcd06b77d323b9722fb6a64d508aa96
blushi <marie.gauthier63@gmail.com>
2025-10-28 13:50:33 +0100

    chore: rm \n

--- web-marketplace/src/pages/CreateOrganization/hooks/useOrganizationFlow.ts
+++ web-marketplace/src/pages/CreateOrganization/hooks/useOrganizationFlow.ts
@@ -13,7 +13,6 @@ import { getCreateOrgSteps } from '../CreateOrganization.utils';
 export type OrganizationMultiStepData = Partial<EditProfileFormSchemaType> & {
   dao?: {
     daoAddress?: string;
-
     organizationId?: string;
     walletAddress?: string;
   };

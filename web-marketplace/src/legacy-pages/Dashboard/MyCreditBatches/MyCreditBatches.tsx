5d4f1e50e1605b9d12165b654586ac1eb2edebcc
Alexander Åstrand <alexander.astrand@gmail.com>
2025-11-03 19:32:40 +0100

    feat: APP-789 sell order and credit batch adjusted for org dashboard

--- web-marketplace/src/legacy-pages/Dashboard/MyCreditBatches/MyCreditBatches.tsx
+++ web-marketplace/src/legacy-pages/Dashboard/MyCreditBatches/MyCreditBatches.tsx
@@ -13,7 +13,6 @@ import { useWallet } from 'lib/wallet/wallet';
 import { useFetchPaginatedBatches } from 'hooks/batches/useFetchPaginatedBatches';
 
 import { useDashboardContext } from '../Dashboard.context';
-
 import { NO_CREDIT_BATCHES_MESSAGE } from './MyCreditBatches.constants';
 import { MyCreditBatchesTable } from './MyCreditBatches.Table';
 
@@ -21,13 +20,20 @@ export const MyCreditBatches = (): JSX.Element => {
   const { _ } = useLingui();
   const theme = useTheme();
   const { wallet } = useWallet();
-  const { selectedAccountAddress } = useDashboardContext();
+  const {
+    selectedAccountAddress,
+    isOrganizationDashboard,
+    isOrganizationOwner,
+    isOrganizationAdmin,
+  } = useDashboardContext();
   const accountAddress = selectedAccountAddress ?? wallet?.address;
   const { batchesWithSupply, setPaginationParams, paginationParams } =
     useFetchPaginatedBatches({
       address: accountAddress,
     });
   const hasNoBatches = batchesWithSupply && batchesWithSupply?.length === 0;
+  const canManageCreditBatches =
+    !isOrganizationDashboard || isOrganizationOwner || isOrganizationAdmin;
 
   return (
     <div className="w-[100%]">
@@ -36,9 +42,9 @@ export const MyCreditBatches = (): JSX.Element => {
         paginationParams={paginationParams}
         setPaginationParams={setPaginationParams}
         batchesWithSupply={batchesWithSupply}
-        useCreate
+        useCreate={canManageCreditBatches}
       />
-      {hasNoBatches && (
+      {hasNoBatches && canManageCreditBatches && (
         <EmptyState
           message={_(NO_CREDIT_BATCHES_MESSAGE)}
           icon={<NoEcocreditsIcon sx={{ width: 100, height: 100 }} />}

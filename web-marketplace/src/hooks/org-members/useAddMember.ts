6bb37ea08ba584ae3fb86e05d088f320674e318a
blushi <marie.gauthier63@gmail.com>
2025-11-04 17:03:42 +0100

    refactor: useAddMember

--- web-marketplace/src/hooks/org-members/useAddMember.ts
+++ web-marketplace/src/hooks/org-members/useAddMember.ts
@@ -48,10 +48,13 @@ export function useAddMember(params: MembersHookParams) {
     checkProjectsErrors,
   } = useMembersContext(params);
 
-  const addMember = useCallback(
-    async (data: MemberData) => {
+  const addMemberWithWalletAddress = useCallback(
+    async (
+      data: MemberData,
+      roleIdToAdd: number,
+      projectRoleIdToAdd: number,
+    ) => {
       const { role, addressOrEmail, visible } = data;
-      const isWalletAddr = isValidAddress(addressOrEmail);
 
       if (
         !wallet?.address ||
@@ -70,140 +73,65 @@ export function useAddMember(params: MembersHookParams) {
         return;
       }
 
-      const roleIdToAdd = getNewRoleId({ type: 'organization', role });
-      const projectRoleIdToAdd = getNewRoleId({ type: 'project', role });
-
-      if (isWalletAddr) {
-        const orgExecuteInstruction = {
-          contractAddress: daoRbamAddress,
-          msg: {
-            execute_actions: {
-              actions: addMemberActions({
-                daoRbamAddress,
-                cw4GroupAddress,
-                authorizationId,
-                roleId,
-                memberAddress: addressOrEmail,
-                roleIdToAdd,
-              }),
-            },
+      const orgExecuteInstruction = {
+        contractAddress: daoRbamAddress,
+        msg: {
+          execute_actions: {
+            actions: addMemberActions({
+              daoRbamAddress,
+              cw4GroupAddress,
+              authorizationId,
+              roleId,
+              memberAddress: addressOrEmail,
+              roleIdToAdd,
+            }),
           },
-        };
+        },
+      };
 
-        const projectExecuteInstructions = (projectsCurrentUserCanManageMembers
-          ?.map(project => {
-            const projectDao =
-              project?.projectByProjectId?.daoByAdminDaoAddress;
-            if (!projectDao) return null;
+      const projectExecuteInstructions = (projectsCurrentUserCanManageMembers
+        ?.map(project => {
+          const projectDao = project?.projectByProjectId?.daoByAdminDaoAddress;
+          if (!projectDao) return null;
 
-            // If user is already member of the project,
-            const isMemberOfProject =
-              projectDao?.assignmentsByDaoAddress?.nodes.some(
-                assignment =>
-                  assignment?.accountByAccountId?.addr === addressOrEmail,
-              );
-            // then we keep his/her role there unchanged
-            if (isMemberOfProject) return null;
+          // If user is already member of the project,
+          const isMemberOfProject =
+            projectDao?.assignmentsByDaoAddress?.nodes.some(
+              assignment =>
+                assignment?.accountByAccountId?.addr === addressOrEmail,
+            );
+          // then we keep his/her role there unchanged
+          if (isMemberOfProject) return null;
 
-            return {
-              contractAddress: projectDao.daoRbamAddress,
-              msg: {
-                execute_actions: {
-                  actions: addMemberActions({
-                    daoRbamAddress: projectDao.daoRbamAddress,
-                    cw4GroupAddress: projectDao.cw4GroupAddress,
-                    authorizationId: projectAuthorizationId,
-                    roleId: projectRoleId,
-                    memberAddress: addressOrEmail,
-                    roleIdToAdd: projectRoleIdToAdd,
-                  }),
-                },
+          return {
+            contractAddress: projectDao.daoRbamAddress,
+            msg: {
+              execute_actions: {
+                actions: addMemberActions({
+                  daoRbamAddress: projectDao.daoRbamAddress,
+                  cw4GroupAddress: projectDao.cw4GroupAddress,
+                  authorizationId: projectAuthorizationId,
+                  roleId: projectRoleId,
+                  memberAddress: addressOrEmail,
+                  roleIdToAdd: projectRoleIdToAdd,
+                }),
               },
-            };
-          })
-          .filter(Boolean) || []) as ExecuteInstruction[];
+            },
+          };
+        })
+        .filter(Boolean) || []) as ExecuteInstruction[];
 
-        try {
-          setProcessingModal(atom => void (atom.open = true));
-          await signingCosmWasmClient.executeMultiple(
-            wallet.address,
-            [orgExecuteInstruction, ...projectExecuteInstructions],
-            2,
-          );
-          await refetchMembers({ address: addressOrEmail, role, visible });
-        } catch (e) {
-          setProcessingModal(atom => void (atom.open = false));
-          setErrorBannerText(String(e));
-        }
-      } else if (token) {
-        try {
-          await postData({
-            url: `${apiServerUrl}/marketplace/v1/assignments/add-by-email`,
-            parseTextResponse: true,
-            data: {
-              email: addressOrEmail,
-              roleName: role,
-              daoAddress,
-              visible,
-              onChainRoleId: roleIdToAdd,
-            },
-            token,
-            retryCsrfRequest,
-            onSuccess: async () => {
-              await reactQueryClient.invalidateQueries({
-                queryKey: getAccountByIdQueryKey({
-                  id: activeAccountId,
-                  daoAccountsOrderBy: params.daoAccountsOrderBy,
-                }),
-              });
-            },
-          });
-        } catch (e) {
-          setErrorBannerText(String(e));
-        }
-
-        if (projectsCurrentUserCanManageMembers) {
-          const projectsFiltered = projectsCurrentUserCanManageMembers.filter(
-            project => {
-              if (!project) return false;
-              // If user is already member of the project,
-              const isMemberOfProject =
-                project?.projectByProjectId?.daoByAdminDaoAddress?.assignmentsByDaoAddress?.nodes.some(
-                  assignment =>
-                    assignment?.accountByAccountId?.privateAccountById
-                      ?.email === addressOrEmail,
-                );
-              // then we keep his/her role there unchanged
-              return !isMemberOfProject;
-            },
-          );
-          const projectsRes = await Promise.allSettled(
-            projectsFiltered.map(
-              async project =>
-                await postData({
-                  url: `${apiServerUrl}/marketplace/v1/assignments/add-by-email`,
-                  parseTextResponse: true,
-                  data: {
-                    email: addressOrEmail,
-                    roleName: role,
-                    daoAddress: project?.projectByProjectId?.adminDaoAddress,
-                    visible,
-                    onChainRoleId: projectRoleIdToAdd,
-                  },
-                  token,
-                  retryCsrfRequest,
-                  onSuccess: async () => {
-                    await reactQueryClient.invalidateQueries({
-                      queryKey: getOrganizationByDaoAddressQueryKey({
-                        daoAddress,
-                      }),
-                    });
-                  },
-                }),
-            ),
-          );
-          checkProjectsErrors(projectsRes, projectsFiltered);
-        }
+      try {
+        setProcessingModal(atom => void (atom.open = true));
+        await signingCosmWasmClient.executeMultiple(
+          wallet.address,
+          [orgExecuteInstruction, ...projectExecuteInstructions],
+          2,
+        );
+        await refetchMembers({ address: addressOrEmail, role, visible });
+      } catch (e) {
+        setProcessingModal(atom => void (atom.open = false));
+        setErrorBannerText(String(e));
       }
     },
     [
@@ -220,13 +148,131 @@ export function useAddMember(params: MembersHookParams) {
       projectsCurrentUserCanManageMembers,
       setErrorBannerText,
       _,
+      refetchMembers,
+      setProcessingModal,
+    ],
+  );
+
+  const addMemberOffChain = useCallback(
+    async (
+      data: MemberData,
+      roleIdToAdd: number,
+      projectRoleIdToAdd: number,
+    ) => {
+      if (!daoAddress || !token) {
+        setErrorBannerText(_(MISSING_REQUIRED_PARAMS));
+        return;
+      }
+
+      const { role, addressOrEmail, visible } = data;
+      try {
+        await postData({
+          url: `${apiServerUrl}/marketplace/v1/assignments/add-by-email`,
+          parseTextResponse: true,
+          data: {
+            email: addressOrEmail,
+            roleName: role,
+            daoAddress,
+            visible,
+            onChainRoleId: roleIdToAdd,
+          },
+          token,
+          retryCsrfRequest,
+          onSuccess: async () => {
+            await reactQueryClient.invalidateQueries({
+              queryKey: getAccountByIdQueryKey({
+                id: activeAccountId,
+                daoAccountsOrderBy: params.daoAccountsOrderBy,
+              }),
+            });
+          },
+        });
+      } catch (e) {
+        setErrorBannerText(String(e));
+      }
+
+      if (projectsCurrentUserCanManageMembers) {
+        const projectsFiltered = projectsCurrentUserCanManageMembers.filter(
+          project => {
+            if (!project) return false;
+            // If user is already member of the project,
+            const isMemberOfProject =
+              project?.projectByProjectId?.daoByAdminDaoAddress?.assignmentsByDaoAddress?.nodes.some(
+                assignment =>
+                  assignment?.accountByAccountId?.privateAccountById?.email ===
+                  addressOrEmail,
+              );
+            // then we keep his/her role there unchanged
+            return !isMemberOfProject;
+          },
+        );
+        const projectsRes = await Promise.allSettled(
+          projectsFiltered.map(
+            async project =>
+              await postData({
+                url: `${apiServerUrl}/marketplace/v1/assignments/add-by-email`,
+                parseTextResponse: true,
+                data: {
+                  email: addressOrEmail,
+                  roleName: role,
+                  daoAddress: project?.projectByProjectId?.adminDaoAddress,
+                  visible,
+                  onChainRoleId: projectRoleIdToAdd,
+                },
+                token,
+                retryCsrfRequest,
+                onSuccess: async () => {
+                  await reactQueryClient.invalidateQueries({
+                    queryKey: getOrganizationByDaoAddressQueryKey({
+                      daoAddress,
+                    }),
+                  });
+                },
+              }),
+          ),
+        );
+        checkProjectsErrors(projectsRes, projectsFiltered);
+      }
+    },
+    [
+      daoAddress,
       token,
-      retryCsrfRequest,
+      setErrorBannerText,
+      _,
       reactQueryClient,
       activeAccountId,
-      refetchMembers,
       params.daoAccountsOrderBy,
-      setProcessingModal,
+      projectsCurrentUserCanManageMembers,
+      retryCsrfRequest,
+      checkProjectsErrors,
+    ],
+  );
+
+  const addMember = useCallback(
+    async (data: MemberData) => {
+      const { role, addressOrEmail } = data;
+      const isWalletAddr = isValidAddress(addressOrEmail);
+
+      if (!role) {
+        setErrorBannerText(_(MISSING_REQUIRED_PARAMS));
+        return;
+      }
+
+      const roleIdToAdd = getNewRoleId({ type: 'organization', role });
+      const projectRoleIdToAdd = getNewRoleId({ type: 'project', role });
+
+      if (isWalletAddr) {
+        await addMemberWithWalletAddress(data, roleIdToAdd, projectRoleIdToAdd);
+      } else if (token) {
+        await addMemberOffChain(data, roleIdToAdd, projectRoleIdToAdd);
+      }
+    },
+    [
+      addMemberWithWalletAddress,
+      addMemberOffChain,
+      setErrorBannerText,
+      _,
+      token,
     ],
   );
 

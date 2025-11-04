c945f97b45d9719b9ee83f679f2044f4065ddb06
blushi <marie.gauthier63@gmail.com>
2025-11-04 16:32:34 +0100

    refactor: useRemoveMember

--- web-marketplace/src/hooks/org-members/useRemoveMember.ts
+++ web-marketplace/src/hooks/org-members/useRemoveMember.ts
@@ -14,6 +14,8 @@ import { getAccountByIdQueryKey } from 'lib/queries/react-query/registry-server/
 import { getFromCacheOrFetch } from 'lib/queries/react-query/utils/getFromCacheOrFetch';
 import { useWallet } from 'lib/wallet/wallet';
 
+import { BaseMemberRole } from 'components/organisms/BaseMembersTable/BaseMembersTable.types';
+
 import { MEMBER_NOT_FOUND, MISSING_REQUIRED_PARAMS } from './constants';
 import { AssignmentToDelete, MembersHookParams } from './types';
 import { useMembersContext } from './useMembersContext';
@@ -47,8 +49,13 @@ export function useRemoveMember(params: MembersHookParams) {
     checkErrors,
   } = useMembersContext(params);
 
-  const removeMember = useCallback(
-    async (id: string) => {
+  const removeMemberWithWalletAddress = useCallback(
+    async (
+      id: string,
+      role: BaseMemberRole,
+      memberRoleId: number,
+      memberAddress: string,
+    ) => {
       if (
         !wallet?.address ||
         !signingCosmWasmClient ||
@@ -64,192 +71,133 @@ export function useRemoveMember(params: MembersHookParams) {
         setErrorBannerText(_(MISSING_REQUIRED_PARAMS));
         return;
       }
+      // Check if role is assigned
+      // If member was initially added with email address and added a wallet address later on,
+      // then role was not assigned on chain (only off chain)
+      const assignedRes = await getFromCacheOrFetch({
+        reactQueryClient,
+        query: getAssignedQuery({
+          client: signingCosmWasmClient,
+          addr: memberAddress as string,
+          roleId: memberRoleId,
+          daoRbamAddress,
+          enabled: !!memberAddress,
+        }),
+      });
+      const assigned = assignedRes?.assigned;
 
-      const member = members.find(m => m.id === id);
-      if (!member) {
-        setErrorBannerText(_(MEMBER_NOT_FOUND));
-        return;
-      }
-      const {
-        onChainRoleId: memberRoleId,
-        address: memberAddress,
-        role,
-      } = member;
+      const orgExecuteInstruction = assigned
+        ? {
+            contractAddress: daoRbamAddress,
+            msg: {
+              execute_actions: {
+                actions: removeMemberActions({
+                  daoRbamAddress,
+                  cw4GroupAddress,
+                  authorizationId,
+                  roleId,
+                  memberAddress,
+                  memberRoleId,
+                }),
+              },
+            },
+          }
+        : undefined;
 
-      if (memberAddress) {
-        // Check if role is assigned
-        // If member was initially added with email address and added a wallet address later on,
-        // then role was not assigned on chain (only off chain)
-        const assignedRes = await getFromCacheOrFetch({
-          reactQueryClient,
-          query: getAssignedQuery({
-            client: signingCosmWasmClient,
-            addr: memberAddress as string,
-            roleId: memberRoleId,
-            daoRbamAddress,
-            enabled: !!memberAddress,
-          }),
-        });
-        const assigned = assignedRes?.assigned;
+      const offchainProjectAssignmentsToDelete: AssignmentToDelete[] = [];
+      const projectExecuteInstructions = (await Promise.all(
+        projectsCurrentUserCanManageMembers
+          ?.map(async project => {
+            const projectDao =
+              project?.projectByProjectId?.daoByAdminDaoAddress;
+            if (!projectDao) return null;
+            const assignment = projectDao.assignmentsByDaoAddress.nodes.find(
+              a => a?.accountId === id,
+            );
+            if (!assignment) return null;
 
-        const orgExecuteInstruction = assigned
-          ? {
-              contractAddress: daoRbamAddress,
+            const assignedRes = await getFromCacheOrFetch({
+              reactQueryClient,
+              query: getAssignedQuery({
+                client: signingCosmWasmClient,
+                addr: memberAddress,
+                roleId: assignment.onChainRoleId,
+                daoRbamAddress,
+              }),
+            });
+            const assigned = assignedRes?.assigned;
+            if (!assigned && project.projectByProjectId?.adminDaoAddress) {
+              offchainProjectAssignmentsToDelete.push({
+                daoAddress: project.projectByProjectId?.adminDaoAddress,
+                roleName: assignment.roleName,
+              });
+              return null;
+            }
+
+            return {
+              contractAddress: projectDao.daoRbamAddress,
               msg: {
                 execute_actions: {
                   actions: removeMemberActions({
-                    daoRbamAddress,
-                    cw4GroupAddress,
-                    authorizationId,
-                    roleId,
+                    daoRbamAddress: projectDao.daoRbamAddress,
+                    cw4GroupAddress: projectDao.cw4GroupAddress,
+                    authorizationId: projectAuthorizationId,
+                    roleId: projectRoleId,
                     memberAddress,
-                    memberRoleId,
+                    memberRoleId: parseInt(assignment.onChainRoleId),
                   }),
                 },
               },
-            }
-          : undefined;
+            };
+          })
+          .filter(Boolean) || [],
+      )) as ExecuteInstruction[];
 
-        const offchainProjectAssignmentsToDelete: AssignmentToDelete[] = [];
-        const projectExecuteInstructions = (await Promise.all(
-          projectsCurrentUserCanManageMembers
-            ?.map(async project => {
-              const projectDao =
-                project?.projectByProjectId?.daoByAdminDaoAddress;
-              if (!projectDao) return null;
-              const assignment = projectDao.assignmentsByDaoAddress.nodes.find(
-                a => a?.accountId === id,
-              );
-              if (!assignment) return null;
+      try {
+        setProcessingModal(atom => void (atom.open = true));
+        const executions = [
+          orgExecuteInstruction,
+          ...projectExecuteInstructions,
+        ].filter(Boolean) as ExecuteInstruction[];
+        if (executions.length > 0) {
+          await signingCosmWasmClient.executeMultiple(
+            wallet.address,
+            executions,
+            2,
+          );
+        }
+        if (!assigned) {
+          await deleteAssignment({
+            variables: {
+              input: { daoAddress, roleName: role, accountId: id },
+            },
+          });
+        }
 
-              const assignedRes = await getFromCacheOrFetch({
-                reactQueryClient,
-                query: getAssignedQuery({
-                  client: signingCosmWasmClient,
-                  addr: memberAddress,
-                  roleId: assignment.onChainRoleId,
-                  daoRbamAddress,
-                }),
-              });
-              const assigned = assignedRes?.assigned;
-              if (!assigned && project.projectByProjectId?.adminDaoAddress) {
-                offchainProjectAssignmentsToDelete.push({
-                  daoAddress: project.projectByProjectId?.adminDaoAddress,
-                  roleName: assignment.roleName,
-                });
-                return null;
-              }
-
-              return {
-                contractAddress: projectDao.daoRbamAddress,
-                msg: {
-                  execute_actions: {
-                    actions: removeMemberActions({
-                      daoRbamAddress: projectDao.daoRbamAddress,
-                      cw4GroupAddress: projectDao.cw4GroupAddress,
-                      authorizationId: projectAuthorizationId,
-                      roleId: projectRoleId,
-                      memberAddress,
-                      memberRoleId: parseInt(assignment.onChainRoleId),
-                    }),
-                  },
-                },
-              };
-            })
-            .filter(Boolean) || [],
-        )) as ExecuteInstruction[];
-
-        try {
-          setProcessingModal(atom => void (atom.open = true));
-          const executions = [
-            orgExecuteInstruction,
-            ...projectExecuteInstructions,
-          ].filter(Boolean) as ExecuteInstruction[];
-          if (executions.length > 0) {
-            await signingCosmWasmClient.executeMultiple(
-              wallet.address,
-              executions,
-              2,
-            );
-          }
-          if (!assigned) {
+        const delAssignmentsRes = await Promise.allSettled(
+          offchainProjectAssignmentsToDelete.map(async assignment => {
             await deleteAssignment({
               variables: {
-                input: { daoAddress, roleName: role, accountId: id },
+                input: {
+                  daoAddress: assignment.daoAddress,
+                  roleName: assignment.roleName,
+                  accountId: id,
+                },
               },
             });
-          }
+          }),
+        );
+        checkErrors(delAssignmentsRes, offchainProjectAssignmentsToDelete);
 
-          const delAssignmentsRes = await Promise.allSettled(
-            offchainProjectAssignmentsToDelete.map(async assignment => {
-              await deleteAssignment({
-                variables: {
-                  input: {
-                    daoAddress: assignment.daoAddress,
-                    roleName: assignment.roleName,
-                    accountId: id,
-                  },
-                },
-              });
-            }),
-          );
-          checkErrors(delAssignmentsRes, offchainProjectAssignmentsToDelete);
-
-          await refetchMembers({
-            address: memberAddress,
-            role,
-            accountId: id,
-            shouldFindAssignment: false,
-          });
-        } catch (e) {
-          setProcessingModal(atom => void (atom.open = false));
-          setErrorBannerText(String(e));
-        }
-      } else {
-        try {
-          await deleteAssignment({
-            variables: { input: { daoAddress, roleName: role, accountId: id } },
-          });
-          await reactQueryClient.invalidateQueries({
-            queryKey: getAccountByIdQueryKey({
-              id: activeAccountId,
-              daoAccountsOrderBy: params.daoAccountsOrderBy,
-            }),
-          });
-        } catch (e) {
-          setErrorBannerText(String(e));
-        }
-
-        if (projectsCurrentUserCanManageMembers) {
-          const projectsRes = await Promise.allSettled(
-            projectsCurrentUserCanManageMembers.map(async project => {
-              if (!project) return;
-              const projectDaoAddress =
-                project?.projectByProjectId?.adminDaoAddress;
-              const projectRoleName =
-                project?.projectByProjectId?.daoByAdminDaoAddress?.assignmentsByDaoAddress.nodes?.find(
-                  a => a?.accountId === id,
-                )?.roleName;
-              if (!projectDaoAddress || !projectRoleName) return;
-              await deleteAssignment({
-                variables: {
-                  input: {
-                    daoAddress: projectDaoAddress,
-                    roleName: projectRoleName,
-                    accountId: id,
-                  },
-                },
-              });
-            }),
-          );
-          await reactQueryClient.invalidateQueries({
-            queryKey: getAccountByIdQueryKey({
-              id: activeAccountId,
-              daoAccountsOrderBy: params.daoAccountsOrderBy,
-            }),
-          });
-          checkProjectsErrors(projectsRes, projectsCurrentUserCanManageMembers);
-        }
+        await refetchMembers({
+          address: memberAddress,
+          role,
+          accountId: id,
+          shouldFindAssignment: false,
+        });
+      } catch (e) {
+        setProcessingModal(atom => void (atom.open = false));
+        setErrorBannerText(String(e));
       }
     },
     [
@@ -263,16 +211,111 @@ export function useRemoveMember(params: MembersHookParams) {
       roleId,
       projectAuthorizationId,
       projectRoleId,
-      members,
+      projectsCurrentUserCanManageMembers,
+      setErrorBannerText,
+      _,
+      deleteAssignment,
+      reactQueryClient,
+      refetchMembers,
+      checkErrors,
+      setProcessingModal,
+    ],
+  );
+
+  const removeMemberOffChain = useCallback(
+    async (id: string, role: BaseMemberRole) => {
+      if (!daoAddress) {
+        setErrorBannerText(_(MISSING_REQUIRED_PARAMS));
+        return;
+      }
+      try {
+        await deleteAssignment({
+          variables: { input: { daoAddress, roleName: role, accountId: id } },
+        });
+        await reactQueryClient.invalidateQueries({
+          queryKey: getAccountByIdQueryKey({
+            id: activeAccountId,
+            daoAccountsOrderBy: params.daoAccountsOrderBy,
+          }),
+        });
+      } catch (e) {
+        setErrorBannerText(String(e));
+      }
+
+      if (projectsCurrentUserCanManageMembers) {
+        const projectsRes = await Promise.allSettled(
+          projectsCurrentUserCanManageMembers.map(async project => {
+            if (!project) return;
+            const projectDaoAddress =
+              project?.projectByProjectId?.adminDaoAddress;
+            const projectRoleName =
+              project?.projectByProjectId?.daoByAdminDaoAddress?.assignmentsByDaoAddress.nodes?.find(
+                a => a?.accountId === id,
+              )?.roleName;
+            if (!projectDaoAddress || !projectRoleName) return;
+            await deleteAssignment({
+              variables: {
+                input: {
+                  daoAddress: projectDaoAddress,
+                  roleName: projectRoleName,
+                  accountId: id,
+                },
+              },
+            });
+          }),
+        );
+        await reactQueryClient.invalidateQueries({
+          queryKey: getAccountByIdQueryKey({
+            id: activeAccountId,
+            daoAccountsOrderBy: params.daoAccountsOrderBy,
+          }),
+        });
+        checkProjectsErrors(projectsRes, projectsCurrentUserCanManageMembers);
+      }
+    },
+    [
+      daoAddress,
       projectsCurrentUserCanManageMembers,
       setErrorBannerText,
       _,
       deleteAssignment,
       reactQueryClient,
       activeAccountId,
-      setProcessingModal,
-      refetchMembers,
       params.daoAccountsOrderBy,
+      checkProjectsErrors,
+    ],
+  );
+
+  const removeMember = useCallback(
+    async (id: string) => {
+      const member = members.find(m => m.id === id);
+      if (!member) {
+        setErrorBannerText(_(MEMBER_NOT_FOUND));
+        return;
+      }
+      const {
+        onChainRoleId: memberRoleId,
+        address: memberAddress,
+        role,
+      } = member;
+
+      if (memberAddress) {
+        await removeMemberWithWalletAddress(
+          id,
+          role,
+          memberRoleId,
+          memberAddress,
+        );
+      } else {
+        await removeMemberOffChain(id, role);
+      }
+    },
+    [
+      members,
+      removeMemberWithWalletAddress,
+      removeMemberOffChain,
+      setErrorBannerText,
+      _,
     ],
   );
 

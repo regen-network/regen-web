68739a695d33c5824d22dc18ede1bd2f3afcfa47
blushi <marie.gauthier63@gmail.com>
2025-11-18 14:55:17 +0100

    feat: canEditProject / canCreatePost

--- web-marketplace/src/legacy-pages/Dashboard/MyProjects/ManageProject.tsx
+++ web-marketplace/src/legacy-pages/Dashboard/MyProjects/ManageProject.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useState } from 'react';
+import { useMemo, useState } from 'react';
 import { Outlet, useLocation, useParams } from 'react-router-dom';
 import { msg } from '@lingui/core/macro';
 import { useLingui } from '@lingui/react';

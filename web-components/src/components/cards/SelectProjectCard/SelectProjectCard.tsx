878b7bb363db01a8a2cdcb9927833841181f9c0e
Alexander Åstrand <alexander.astrand@gmail.com>
2026-01-13 21:56:39 +1100

    fix: APP-860 opacity location fix

--- web-components/src/components/cards/SelectProjectCard/SelectProjectCard.tsx
+++ web-components/src/components/cards/SelectProjectCard/SelectProjectCard.tsx
@@ -21,8 +21,8 @@ export const SelectProjectCard = ({
       htmlFor={cardId}
       className={`cursor-pointer rounded-[10px] overflow-hidden shadow-sm outline outline-solid relative z-50 ${
         selected
-          ? 'rounded-xl outline-brand-400 outline-[3px]'
-          : 'outline-1 outline-grey-300 focus-within:shadow-xs focus-within:shadow-brand-400/50'
+          ? 'rounded-xl outline-brand-400 outline-[3px] opacity-100'
+          : 'outline-1 outline-grey-300 focus-within:shadow-xs focus-within:shadow-brand-400/50 opacity-70'
       }`}
     >
       <input

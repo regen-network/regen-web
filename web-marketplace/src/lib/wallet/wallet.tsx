84e616d57c8045197995191a43375db81625adc3
blushi <marie.gauthier63@gmail.com>
2026-02-09 13:21:49 +0100

    chore: revert

--- web-marketplace/src/lib/wallet/wallet.tsx
+++ web-marketplace/src/lib/wallet/wallet.tsx
@@ -223,7 +223,7 @@ export const WalletProvider: React.FC<React.PropsWithChildren<unknown>> = ({
         wallet,
         walletConfig: walletConfigRef.current,
         activeWalletAddr,
-        loginDisabled: true,
+        loginDisabled,
         loaded: loaded && !isFetching && (loginDisabled || !authLoading),
         connect,
         connectWallet,

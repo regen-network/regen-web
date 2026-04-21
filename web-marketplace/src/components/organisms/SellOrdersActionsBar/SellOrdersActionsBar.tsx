bc51de3536a5c3c80670efcd4e41bb19eb16f495
blushi <marie.gauthier63@gmail.com>
2026-04-21 17:09:14 +0200

    chore: add isPrefinanceProject check

--- web-marketplace/src/components/organisms/SellOrdersActionsBar/SellOrdersActionsBar.tsx
+++ web-marketplace/src/components/organisms/SellOrdersActionsBar/SellOrdersActionsBar.tsx
@@ -143,7 +143,7 @@ export const SellOrdersActionsBar = ({
         ) : (
           <>
             {((!isPrefinanceProject && !isBuyButtonDisabled) || !isSoldOut) &&
-              (prefinancePrice ||
+              ((isPrefinanceProject && prefinancePrice) ||
                 (avgPricePerTonLabel && !!onChainProjectId)) && (
                 <Box
                   sx={{
